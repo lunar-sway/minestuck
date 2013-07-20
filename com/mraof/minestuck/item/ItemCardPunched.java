2d9f57f5c7c238e187e3f2a1ede64a6c15bfd681
Iconmaster <crazybonesj@yahoo.com>
2013-07-20 12:55:40 -0400

    Punched cards, etc. now use NBT to store item ID
    Only actually displays ID rather than name. Also, there's no way to
    write the data until I finish the machines...

--- com/mraof/minestuck/item/ItemCardPunched.java
+++ com/mraof/minestuck/item/ItemCardPunched.java
@@ -1,10 +1,17 @@
 package com.mraof.minestuck.item;
 
+import java.util.List;
+
 import com.mraof.minestuck.Minestuck;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.Icon;
 
 public class ItemCardPunched extends Item {
@@ -19,4 +26,34 @@ public class ItemCardPunched extends Item {
 	public void registerIcons(IconRegister par1IconRegister) {
 	   itemIcon = par1IconRegister.registerIcon("Minestuck:CardPunched");
 	}
+	
+//    public String getItemDisplayName(ItemStack par1ItemStack)
+//    {
+//        if (par1ItemStack.hasTagCompound())
+//        {
+//            NBTTagCompound nbttagcompound = par1ItemStack.getTagCompound();
+//            NBTTagString nbttagstring = (NBTTagString)nbttagcompound.getTag("contents");
+//
+//            if (nbttagstring != null)
+//            {
+//                return "Captchalogue Card\n\f" + EnumChatFormatting.GRAY + "(" + nbttagstring.toString() + ")";
+//            }
+//        }
+//		return "Captchalogue Card\n\f" + EnumChatFormatting.GRAY + "(invalid data!)";
+//    }
+    
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+        if (par1ItemStack.hasTagCompound())
+        {
+	        NBTTagCompound nbttagcompound = par1ItemStack.getTagCompound();
+	        NBTTagString nbttagstring = (NBTTagString)nbttagcompound.getTag("contents");
+	
+	        if (nbttagstring != null)
+	        {
+	        	par3List.add("(" + nbttagstring.toString() + ")");
+	        }
+        }
+        par3List.add("(invalid data)");
+    }
 }
--- com/mraof/minestuck/item/ItemDowelCarved.java
+++ com/mraof/minestuck/item/ItemDowelCarved.java
@@ -1,10 +1,16 @@
 package com.mraof.minestuck.item;
 
+import java.util.List;
+
 import com.mraof.minestuck.Minestuck;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagString;
 import net.minecraft.util.Icon;
 
 public class ItemDowelCarved extends Item {
@@ -19,4 +25,19 @@ public class ItemDowelCarved extends Item {
 	public void registerIcons(IconRegister par1IconRegister) {
 	   itemIcon = par1IconRegister.registerIcon("Minestuck:CruxiteCarved");
 	}
+	
+    @Override
+    public void addInformation(ItemStack par1ItemStack, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
+        if (par1ItemStack.hasTagCompound())
+        {
+	        NBTTagCompound nbttagcompound = par1ItemStack.getTagCompound();
+	        NBTTagString nbttagstring = (NBTTagString)nbttagcompound.getTag("contents");
+	
+	        if (nbttagstring != null)
+	        {
+	        	par3List.add("(" + nbttagstring.toString() + ")");
+	        }
+        }
+        par3List.add("(invalid data)");
+    }
 }
