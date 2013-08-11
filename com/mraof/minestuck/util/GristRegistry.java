198eb8424b7364ada897ff5d3267c323f31045a0
Iconmaster <crazybonesj@yahoo.com>
2013-08-11 09:38:05 -0400

    GristConvs now use optional damage ignore mode

--- com/mraof/minestuck/util/GristRegistry.java
+++ com/mraof/minestuck/util/GristRegistry.java
@@ -12,13 +12,21 @@ public class GristRegistry {
 	 * Creates a item-grist conversion ratio for an ItemStack. Used in the Alchemiter and GristWidget.
 	 */
 	public static void addGristConversion(ItemStack item,GristSet grist) {
-		gristRecipes.put(Arrays.asList(item.itemID,item.getItemDamage()), grist);
+		addGristConversion(item,true,grist);
+	}
+
+	public static void addGristConversion(ItemStack item,boolean useDamage,GristSet grist) {
+		gristRecipes.put(Arrays.asList(item.itemID,useDamage ? item.getItemDamage() : 0,useDamage), grist);
 	}
 	
 	/**
 	 * Returns a item-grist conversion ratio, given an ItemStack. Used in the Alchemiter and GristWidget.
 	 */
 	public static GristSet getGristConversion(ItemStack item) {
-		return (GristSet) gristRecipes.get(Arrays.asList(item.itemID,item.getItemDamage()));
+		if (gristRecipes.get(Arrays.asList(item.itemID,item.getItemDamage(),true)) == null) {
+			return (GristSet) gristRecipes.get(Arrays.asList(item.itemID,0,false));
+		} else {
+			return (GristSet) gristRecipes.get(Arrays.asList(item.itemID,item.getItemDamage(),true));
+		}
 	}
 }
