6f3c2557f1ad068d3f49571209b39f39708977b3
Iconmaster <crazybonesj@yahoo.com>
2013-08-24 13:14:42 -0400

    Some more tries at fixes

--- com/mraof/minestuck/tileentity/TileEntityComputer.java
+++ com/mraof/minestuck/tileentity/TileEntityComputer.java
@@ -1,5 +1,8 @@
 package com.mraof.minestuck.tileentity;
 
+import com.mraof.minestuck.util.Debug;
+import com.mraof.minestuck.util.SburbConnection;
+
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet;
@@ -9,10 +12,12 @@ import net.minecraft.tileentity.TileEntity;
 public class TileEntityComputer extends TileEntity {
 
 	public int program;
-	public boolean connected;
-	public String connectedTo;
+	public String connectedTo = "";
 	public boolean givenItems = false;
-	public boolean waiting = false;
+	//public boolean waiting = false;
+	public String owner = "";
+	public boolean initialized = false;
+	public SburbConnection conn;
 	
     public TileEntityComputer() {
             
@@ -23,8 +28,9 @@ public class TileEntityComputer extends TileEntity {
     	super.readFromNBT(par1NBTTagCompound);
     	 this.program = par1NBTTagCompound.getInteger("program");
     	 this.connectedTo = par1NBTTagCompound.getString("connectedTo");
+    	 this.owner = par1NBTTagCompound.getString("owner");
     	 this.givenItems = par1NBTTagCompound.getBoolean("givenItems");
-    	 this.connected = connectedTo != "";
+    	//this.connected = connectedTo != "";
     }
     
     @Override
@@ -32,8 +38,11 @@ public class TileEntityComputer extends TileEntity {
     	super.writeToNBT(par1NBTTagCompound);
     	par1NBTTagCompound.setInteger("program",this.program);
     	par1NBTTagCompound.setBoolean("givenItems",this.givenItems);
-    	if (this.connectedTo != null && !this.connectedTo.equals("")) {
+    	if (!this.connectedTo.equals("")) {
     		par1NBTTagCompound.setString("connectedTo",this.connectedTo);
+    	}
+    	if (!this.owner.equals("")) {
+    		par1NBTTagCompound.setString("owner",this.owner);
     	}
 
     }
@@ -49,7 +58,37 @@ public class TileEntityComputer extends TileEntity {
     @Override
     public void onDataPacket(INetworkManager net, Packet132TileEntityData pkt) 
     {
+    	Debug.print("Data packet gotten "+net.getClass());
     	this.readFromNBT(pkt.customParam1);
+    	if (conn == null) {
+	   		if (connectedTo.equals("") && !owner.equals("") && program == 1) {
+				conn = new SburbConnection(owner,false);
+				SburbConnection.addServer(conn);
+			} else if (connectedTo.equals("") && !owner.equals("") && program == 0) {
+	   			conn = new SburbConnection(owner,true);
+			} else if (!connectedTo.equals("") && !owner.equals("")) {
+				conn = new SburbConnection(owner,program == 0);
+				conn.connect(connectedTo);
+			} 
+    	}
+    }
+    
+    @Override
+    public void updateEntity() {
+    	if (!initialized) {
+    		if (connectedTo.equals("") && !owner.equals("") && program == 1) {
+    			conn = new SburbConnection(owner,false);
+    			SburbConnection.addServer(conn);
+    			initialized = true;
+    		} else if (connectedTo.equals("") && !owner.equals("") && program == 0) {
+       			conn = new SburbConnection(owner,true);
+       			initialized = true;
+    		} else if (!connectedTo.equals("") && !owner.equals("")) {
+    			conn = new SburbConnection(owner,program == 0);
+    			conn.connect(connectedTo);
+    			initialized = true;
+    		} 
+    	}
     }
     
 }
