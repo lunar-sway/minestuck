9ae28309aeb32cf0d5ba52b4895d7eb4303463c0
kirderf1 <kirderf4@gmail.com>
2013-09-14 19:14:02 +0200

    Made the title enums more robust.

--- com/mraof/minestuck/util/EnumAspect.java
+++ com/mraof/minestuck/util/EnumAspect.java
@@ -1,12 +1,12 @@
 package com.mraof.minestuck.util;
 
-import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.Random;
 
 /**
  * An aspect version of <code>EnumClass</code> that works pretty much the same way as the <code>EnumClass</code> exept
  * that it doesn't have any special types.
-	 * The <code>toString()</code> method is overridden and returns a better cased version of the aspect name.
+ * The <code>toString()</code> method is overridden and returns a better cased version of the aspect name.
  * @author kirderf1
  */
 public enum EnumAspect {
@@ -15,14 +15,14 @@ public enum EnumAspect {
 	/**
 	 * This method generates one of the 12 aspects that is not specified in the
 	 * <code>unavailableAspects</code> array. Beware that this method is not compatible with duplicates in the array.
-	 * @param unavailableAspects An <code>ArrayList&#60;EnumAspect&#62;</code> that includes the aspects that it won't choose from.
+	 * @param unavailableAspects An <code>EnumSet&#60;EnumAspect&#62;</code> that includes the aspects that it won't choose from.
 	 * Compatible with the value null.
 	 * @return null if <code>unavailableAspects</code> contains 12 or more aspects or
 	 * an <code>EnumAspect</code> of the chosen aspect.
 	 */
-	public static EnumAspect getRandomAspect(ArrayList<EnumAspect> unavailableAspects){
+	public static EnumAspect getRandomAspect(EnumSet<EnumAspect> unavailableAspects){
 		if(unavailableAspects == null)
-			unavailableAspects = new ArrayList<EnumAspect>();
+			unavailableAspects = EnumSet.noneOf(EnumAspect.class);
 		if(!(unavailableAspects.size() < 12))
 			return null;	//No aspect available to generate
 		int aspectInt = new Random().nextInt(12-unavailableAspects.size());
--- com/mraof/minestuck/util/EnumClass.java
+++ com/mraof/minestuck/util/EnumClass.java
@@ -1,11 +1,11 @@
 package com.mraof.minestuck.util;
 
-import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.Random;
 
 /**
  * This class represents the 14 classes that exists in sburb,
- * (including lord and muse, but they are not by default generated in the <code>getRandomClass()</code> method,
+ * (including lord and muse, but they are by default not generated in the <code>getRandomClass()</code> method,
  * thought they can be included by setting the parameter <code>includeSpecial</code> to <code>true</code>)
  * The <code>toString()</code> method is overridden and returns a better cased version of the class name.
  * @author kirderf1
@@ -14,31 +14,31 @@ public enum EnumClass {
 	BARD,HEIR,KNIGHT,MAGE,MAID,PAGE,PRINCE,ROGUE,SEER,SYLPH,THIEF,WITCH,
 	LORD,MUSE;	//Non-randomized classes
 	
-	public static EnumClass getRandomClass(ArrayList<EnumClass> unavailableClasses){
+	public static EnumClass getRandomClass(EnumSet<EnumClass> unavailableClasses){
 		return getRandomClass(unavailableClasses, false);
 	}
 	
 	/**
 	 * This method generates one of the 12 default classes that is not specified in the <code>unavailableClasses</code> array.
 	 * If you want to enable special classes, use <code>true</code> as the <code>includeSpecial</code> parameter.
-	 * Beware that this method is not compatible with duplicates in the array.
-	 * @param unavailableClasses An <code>ArrayList&#60;EnumClass&#62;</code> that includes the classes that it won't choose from.
-	 * Compatible with the value null. Do not include special classes if <code>includeSpecial</code> is <code>false</code>.
+	 * @param unavailableClasses An <code>EnumSet&#60;EnumClass&#62;</code> that includes the classes that it won't choose from.
+	 * Compatible with the value null.
 	 * @param includeSpecial If it should include the two special classes.
 	 * @return  null if <code>unavailableClasses</code> contains 12 or more classes (14 or more is <code>includeSpecial</code> is set to <code>true</code>.)
 	 * or an <code>EnumClass</code> of the chosen class.
 	 */
-	public static EnumClass getRandomClass(ArrayList<EnumClass> unavailableClasses, boolean includeSpecial){
+	public static EnumClass getRandomClass(EnumSet<EnumClass> unavailableClasses, boolean includeSpecial){
 		if(unavailableClasses == null)
-			unavailableClasses = new ArrayList<EnumClass>();
+			unavailableClasses = EnumSet.noneOf(EnumClass.class);
+		if(!includeSpecial){
+			unavailableClasses.add(LORD);
+			unavailableClasses.add(MUSE);
+		}
 		if(!(unavailableClasses.size() < 12) || includeSpecial && !(unavailableClasses.size() < 14))
 			return null;	//No class available to generate
 		int classInt = new Random().nextInt(12-unavailableClasses.size());
 		EnumClass[] list = values();
-		int e = list.length-2;
-		if(includeSpecial)
-			e+=2;
-		for(int i = 0; i < e; i++)
+		for(int i = 0; i < list.length; i++)
 			if(!unavailableClasses.contains(list[i])){
 				classInt--;
 				if(classInt == -1)
