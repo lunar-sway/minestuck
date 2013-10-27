13403c5d06ad8c1f4648daea693233cf0e677d82
kirderf1 <kirderf4@gmail.com>
2013-10-27 20:23:28 +0100

    Method I forgot to add.

--- com/mraof/minestuck/util/SburbServerController.java
+++ com/mraof/minestuck/util/SburbServerController.java
@@ -2,6 +2,7 @@ package com.mraof.minestuck.util;
 
 import com.mraof.minestuck.network.MinestuckPacket;
 import com.mraof.minestuck.network.MinestuckPacket.Type;
+import com.mraof.minestuck.tracker.MinestuckPlayerTracker;
 
 import cpw.mods.fml.common.network.PacketDispatcher;
 import net.minecraft.client.Minecraft;
@@ -10,9 +11,12 @@ import net.minecraft.client.multiplayer.PlayerControllerMP;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemInWorldManager;
+import net.minecraft.item.ItemStack;
 import net.minecraft.network.packet.Packet250CustomPayload;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.Vec3;
 import net.minecraft.world.EnumGameType;
 import net.minecraft.world.World;
 
@@ -21,6 +25,15 @@ public class SburbServerController extends PlayerControllerMP {
 	public SburbServerController(Minecraft mc, NetClientHandler netHandler) {
 		super(mc, netHandler);
 		this.setGameType(EnumGameType.CREATIVE);
+	}
+	
+	@Override
+	public boolean onPlayerRightClick(EntityPlayer entityPlayer, World world, ItemStack stack, int par4, int par5, int par6, int par7, Vec3 par8Vec3) {
+		if(stack.getItem() instanceof ItemBlock)
+			if(!GristHelper.canAfford(entityPlayer, stack) || !super.onPlayerRightClick(entityPlayer, world, stack, par4, par5, par6, par7, par8Vec3))
+				return false;
+			else return true;
+		return false;
 	}
 	
 	@Override
