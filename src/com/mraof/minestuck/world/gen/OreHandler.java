da5ecc0e0d9a6dc0c0ec73076600e874e59fbaf3
Kirderf1 <Kirderf1@AllaAlbers.lan>
2014-11-08 10:40:20 +0100

    Cruxite can now always generate in the land

--- src/com/mraof/minestuck/world/gen/OreHandler.java
+++ src/com/mraof/minestuck/world/gen/OreHandler.java
@@ -3,11 +3,15 @@ package com.mraof.minestuck.world.gen;
 import java.util.Random;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 
 import com.mraof.minestuck.Minestuck;
+import com.mraof.minestuck.block.OreCruxite;
+import com.mraof.minestuck.world.WorldProviderLands;
+import com.mraof.minestuck.world.gen.lands.BlockWithMetadata;
 
 import cpw.mods.fml.common.IWorldGenerator;
 
@@ -24,15 +28,18 @@ public class OreHandler implements IWorldGenerator {
 	
 	public void addOreSpawn(Block block, World world, Random random, int blockXPos, int blockZPos, int maxX, int maxZ, int maxVeinSize, int chancesToSpawn, int minY, int maxY)
 	{
-	      //int maxPossY = minY + (maxY - 1);
-	      int diffBtwnMinMaxY = maxY - minY;
-	       for(int x = 0; x < chancesToSpawn; x++)
-	       {
-	             int posX = blockXPos + random.nextInt(maxX);
-	             int posY = minY + random.nextInt(diffBtwnMinMaxY);
-	             int posZ = blockZPos + random.nextInt(maxZ);
-	             (new WorldGenMinable(block, maxVeinSize)).generate(world, random, posX, posY, posZ);	//TODO Fix so it generates in the land's underground material instead of stone.
-	       }
+		//int maxPossY = minY + (maxY - 1);
+		int diffBtwnMinMaxY = maxY - minY;
+		BlockWithMetadata groundType = new BlockWithMetadata(Blocks.stone);
+		if(world.provider instanceof WorldProviderLands)
+			groundType = ((ChunkProviderLands) world.provider.createChunkGenerator()).upperBlock;
+		for(int x = 0; x < chancesToSpawn; x++)
+		{
+			int posX = blockXPos + random.nextInt(maxX);
+			int posY = minY + random.nextInt(diffBtwnMinMaxY);
+			int posZ = blockZPos + random.nextInt(maxZ);
+			(new WorldGenMinable(block, OreCruxite.getMetadata(groundType), maxVeinSize, groundType.block)).generate(world, random, posX, posY, posZ);
+		}
 	}
 
 }
