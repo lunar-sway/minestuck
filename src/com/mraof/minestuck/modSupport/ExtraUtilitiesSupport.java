096a931a5bdf809fe0f8168215fe1b0607344ae3
kirderf1 <kirderf4@gmail.com>
2014-11-26 13:34:08 +0100

    Some more mod recipes from the spreadsheets

--- src/com/mraof/minestuck/modSupport/ExtraUtilitiesSupport.java
+++ src/com/mraof/minestuck/modSupport/ExtraUtilitiesSupport.java
@@ -27,6 +27,13 @@ public class ExtraUtilitiesSupport extends ModSupport
 			Block enderLily = ((Block) Class.forName("com.rwtema.extrautils.ExtraUtils").getField("enderLily").get(null));
 			GristRegistry.addGristConversion(new ItemStack(enderLily), new GristSet(new GristType[]{GristType.Uranium, GristType.Iodine}, new int[]{24, 6}));
 			CombinationRegistry.addCombination(new ItemStack(Items.wheat_seeds), new ItemStack(Items.ender_pearl), CombinationRegistry.MODE_OR,  new ItemStack(enderLily));
+			CombinationRegistry.addCombination(new ItemStack(Items.wheat_seeds), new ItemStack(Items.ender_eye), CombinationRegistry.MODE_OR,  new ItemStack(enderLily));	//Might as well do this too
+		}
+		
+		if(Class.forName("com.rwtema.extrautils.ExtraUtils").getField("transferPipeEnabled").getBoolean(null))
+		{
+			Block transferPipe = ((Block) Class.forName("com.rwtema.extrautils.ExtraUtils").getField("transferPipe").get(null));
+			GristRegistry.addGristConversion(new ItemStack(transferPipe), new GristSet(new GristType[]{GristType.Build, GristType.Garnet}, new int[]{1, 1}));
 		}
 	}
 	
--- src/com/mraof/minestuck/modSupport/TinkersConstructSupport.java
+++ src/com/mraof/minestuck/modSupport/TinkersConstructSupport.java
@@ -1,12 +1,15 @@
 package com.mraof.minestuck.modSupport;
 
+import com.mraof.minestuck.util.CombinationRegistry;
 import com.mraof.minestuck.util.Debug;
 import com.mraof.minestuck.util.GristRegistry;
 import com.mraof.minestuck.util.GristSet;
 import com.mraof.minestuck.util.GristType;
 
 import net.minecraft.block.Block;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class TinkersConstructSupport extends ModSupport
 {
@@ -17,6 +20,9 @@ public class TinkersConstructSupport extends ModSupport
 		Block oreBush1 = ((Block) Class.forName("tconstruct.world.TinkerWorld").getField("oreBerry").get(null));
 		Block oreBush2 = ((Block) Class.forName("tconstruct.world.TinkerWorld").getField("oreBerrySecond").get(null));
 		
+		Object[] items1 = {"ingotIron", "ingotGold", "ingotCopper", "ingotTin"};
+		Object[] items2 = {"ingotAluminium", Items.experience_bottle};
+		
 		GristRegistry.addGristConversion(new ItemStack(oreBush1, 1, 0), new GristSet(new GristType[]{GristType.Rust, GristType.Build, GristType.Amber}, new int[]{16, 1, 1}));
 		GristRegistry.addGristConversion(new ItemStack(oreBush1, 1, 1), new GristSet(new GristType[]{GristType.Gold, GristType.Build, GristType.Amber}, new int[]{16, 1, 1}));
 		GristRegistry.addGristConversion(new ItemStack(oreBush1, 1, 2), new GristSet(new GristType[]{GristType.Rust, GristType.Cobalt, GristType.Build, GristType.Amber}, new int[]{16, 3, 1, 1}));
@@ -24,6 +30,11 @@ public class TinkersConstructSupport extends ModSupport
 		GristRegistry.addGristConversion(new ItemStack(oreBush2, 1, 0), new GristSet(new GristType[]{GristType.Rust, GristType.Chalk, GristType.Build, GristType.Amber}, new int[]{16, 6, 1, 1}));
 		GristRegistry.addGristConversion(new ItemStack(oreBush2, 1, 1), new GristSet(new GristType[]{GristType.Uranium, GristType.Quartz, GristType.Diamond, GristType.Ruby, GristType.Build, GristType.Amber}, new int[]{8, 1, 4, 4, 1, 1}));
 		
+		for(int i = 0; i < items1.length; i++)
+			CombinationRegistry.addCombination(items1[i], OreDictionary.WILDCARD_VALUE, "treeLeaves", OreDictionary.WILDCARD_VALUE, CombinationRegistry.MODE_AND, new ItemStack(oreBush1, 1, i));
+		
+		for(int i = 0; i < items2.length; i++)
+			CombinationRegistry.addCombination(items2[i], OreDictionary.WILDCARD_VALUE, "treeLeaves", OreDictionary.WILDCARD_VALUE, CombinationRegistry.MODE_AND, new ItemStack(oreBush2, 1, i));
 	}
 	
 }
