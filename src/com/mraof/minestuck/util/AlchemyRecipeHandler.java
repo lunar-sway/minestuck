de9b41314a6c82a07334e80664c332f3a56bf241
trielt1 <danielbragada@gmail.com>
2015-08-31 22:08:06 -0300

    Added Skaia War Fork Grist Cost and Combination Recipes

--- src/com/mraof/minestuck/util/AlchemyRecipeHandler.java
+++ src/com/mraof/minestuck/util/AlchemyRecipeHandler.java
@@ -538,6 +538,8 @@ public class AlchemyRecipeHandler
 		GristRegistry.addGristConversion(new ItemStack(Minestuck.prismarine_leggings), new GristSet(new GristType[] {GristType.Build, GristType.Cobalt, GristType.Marble}, new int[] {70, 35, 14}));
 		GristRegistry.addGristConversion(new ItemStack(Minestuck.prismarine_boots), new GristSet(new GristType[] {GristType.Build, GristType.Cobalt, GristType.Marble}, new int[] {40, 20, 8}));
 		
+		GristRegistry.addGristConversion(new ItemStack(Minestuck.skaiaFork), new GristSet(new GristType[] {GristType.Build, GristType.Quartz, GristType.Diamond}, new int[]{25,30,11}));
+		
 		//add Designix combinations
 		CombinationRegistry.addCombination(new ItemStack(Items.iron_sword), new ItemStack(Items.rotten_flesh), MODE_AND, false, true, new ItemStack(Minestuck.ninjaSword));
 		CombinationRegistry.addCombination(new ItemStack(Items.iron_sword), new ItemStack(Minestuck.component, 1, 2), MODE_AND, false, true, new ItemStack(Minestuck.regisword));
@@ -596,6 +598,8 @@ public class AlchemyRecipeHandler
 		CombinationRegistry.addCombination(new ItemStack(Items.iron_leggings), new ItemStack(Blocks.prismarine), MODE_OR, false, false, new ItemStack(Minestuck.prismarine_leggings));
 		CombinationRegistry.addCombination(new ItemStack(Items.iron_boots), new ItemStack(Items.prismarine_shard), MODE_OR, false, false, new ItemStack(Minestuck.prismarine_boots));
 		CombinationRegistry.addCombination(new ItemStack(Items.iron_boots), new ItemStack(Blocks.prismarine), MODE_OR, false, false, new ItemStack(Minestuck.prismarine_boots));
+		
+		CombinationRegistry.addCombination(new ItemStack(Minestuck.fork), new ItemStack(Minestuck.component, 1, 2), MODE_AND, false, true, new ItemStack(Minestuck.skaiaFork));
 		
 		
 		//Register chest loot
