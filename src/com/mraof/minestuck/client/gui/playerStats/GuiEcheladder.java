11ff6d7b1b59fc3b3636bd4dac8ed1e14be9d3e7
kirderf1 <kirderf4@gmail.com>
2016-01-09 16:24:23 +0100

    Worked with rungs

--- src/com/mraof/minestuck/client/gui/playerStats/GuiEcheladder.java
+++ src/com/mraof/minestuck/client/gui/playerStats/GuiEcheladder.java
@@ -19,8 +19,8 @@ public class GuiEcheladder extends GuiPlayerStats
 	private static final ResourceLocation guiEcheladder = new ResourceLocation("minestuck", "textures/gui/echeladder.png");
 	
 	private static final int MAX_SCROLL = Echeladder.RUNG_COUNT*14 - 154;
-	private static final int[] backgrounds = new int[] {0xFF4FD400, 0xFFFDFF2B, 0xFFFF2B8F, 0xFF24D9D7, 0xFFA120AC, 0xFFFFAE00, 0xFFA8FFA8, 0xFF345AFF, 0xFFDFBB6C, 0xFFB1FFFF, 0xFFF12B26, 0xFFD885D9, 0xFFE4FF00, 0xFF3DFF17};
-	private static final int[] textColors = new int[] {0xFDFF2B, 0xFF942B, 0x000000, 0x008EA0, 0x8AFF33, 0xF80000, 0xFFB8AF, 0x00FFF0, 0xFF0000, 0x595959, 0xFFFFFF, 0x6C00FF, 0x6D9A00, 0x00970B};
+	private static final int[] backgrounds = new int[] {0xFF4FD400, 0xFFFF0000, 0xFF956C4C, 0xFF7DB037, 0xFFD8A600, 0xFF7F0000, 0xFF007F0E, 0xFF808080, 0xFF00FF21, 0xFF4800FF, 0xFF404040};
+	private static final int[] textColors  = new int[] {  0xFDFF2B,   0x404040,   0xB6FF00,   0x8D7647,   0xFFFFFF,   0xFF6A00,   0x0094FF,   0x000000,   0x007F7F,   0xB200FF,   0x7B9CB5};
 	
 	private static final int ladderXOffset = 163, ladderYOffset = 25;
 	private static final int rows = 12;
