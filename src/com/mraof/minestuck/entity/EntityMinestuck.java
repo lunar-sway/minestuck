cb515040d63f69e1f152e760699bb21a327c5ea3
tgarr0 <tgarr0@users.noreply.github.com>
2016-01-11 17:48:19 -0500

    Leftover Import Removal
    I forgot to remove a few imports that I ended up not needing.

--- src/com/mraof/minestuck/entity/EntityMinestuck.java
+++ src/com/mraof/minestuck/entity/EntityMinestuck.java
@@ -1,10 +1,8 @@
 package com.mraof.minestuck.entity;
 
 import net.minecraft.entity.EntityCreature;
-import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 public abstract class EntityMinestuck extends EntityCreature 
