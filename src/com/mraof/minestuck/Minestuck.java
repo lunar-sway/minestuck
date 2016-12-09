f3bc411f1edd04995f97d4a6e34d80109e14c58d
kirderf1 <kirderf4@gmail.com>
2016-12-09 14:38:43 +0100

    Stopped using deprecated bus, changed to post config event

--- src/com/mraof/minestuck/Minestuck.java
+++ src/com/mraof/minestuck/Minestuck.java
@@ -9,7 +9,6 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.world.gen.structure.MapGenStructureIO;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -168,19 +167,13 @@ public class Minestuck
 		MinecraftForge.EVENT_BUS.register(MinestuckAchievementHandler.instance);
 		MinecraftForge.EVENT_BUS.register(MinestuckPlayerTracker.instance);
 		MinecraftForge.EVENT_BUS.register(ServerEventHandler.instance);
-		
-		FMLCommonHandler.instance().bus().register(MinestuckPlayerTracker.instance);
-		FMLCommonHandler.instance().bus().register(ServerEditHandler.instance);
-		FMLCommonHandler.instance().bus().register(MinestuckChannelHandler.instance);
-		FMLCommonHandler.instance().bus().register(new ConnectionListener());
-		FMLCommonHandler.instance().bus().register(ServerEventHandler.instance);
+		MinecraftForge.EVENT_BUS.register(MinestuckChannelHandler.instance);
+		MinecraftForge.EVENT_BUS.register(new ConnectionListener());
 		
 		if(event.getSide().isClient())
 		{
-		//register channel handler
 			MinecraftForge.EVENT_BUS.register(ClientEditHandler.instance);
-			FMLCommonHandler.instance().bus().register(ClientEditHandler.instance);
-			FMLCommonHandler.instance().bus().register(new MinestuckConfig());
+			MinecraftForge.EVENT_BUS.register(new MinestuckConfig());
 		}
 		
 		//register channel handler
