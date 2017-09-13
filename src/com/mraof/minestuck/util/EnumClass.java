3feb0eb411c08b32f2796f1b6a9cea57c9129204
kirderf1 <kirderf4@gmail.com>
2017-09-13 21:22:12 +0200

    More comments and javadoc for the EnumClass class

--- src/com/mraof/minestuck/util/EnumClass.java
+++ src/com/mraof/minestuck/util/EnumClass.java
@@ -48,7 +48,7 @@ public enum EnumClass
 	{
 		if(unavailableClasses == null)
 			unavailableClasses = EnumSet.noneOf(EnumClass.class);
-		if(!includeSpecial)
+		if(!includeSpecial)	//Prevent generation of the special "master" classes unless includeSpecial is true
 		{
 			unavailableClasses.add(LORD);
 			unavailableClasses.add(MUSE);
@@ -56,9 +56,9 @@ public enum EnumClass
 		
 		if(unavailableClasses.size() == 14)
 			return null;	//No class available to generate
-		int classInt = rand.nextInt(14 - unavailableClasses.size());
+		int classInt = rand.nextInt(14 - unavailableClasses.size());	//TODO Change to a set with all available classes to avoid iterating through the enum values
 		EnumClass[] list = values();
-		for(EnumClass c : list)
+		for(EnumClass c : list)	//Go through each title-class until the index generated is reached
 			if(!unavailableClasses.contains(c))
 			{
 				if(classInt == 0)
@@ -73,30 +73,53 @@ public enum EnumClass
 	 * Used to get a title-class based on index. Used when reading a title-class from nbt.
 	 * The index value is matched with the return of <code>getIntFromClass</code>
 	 * @param i The index number.
-	 * @return The title-class
+	 * @return the title-class
 	 */
 	public static EnumClass getClassFromInt(int i)
 	{
 		return EnumClass.values()[i];
 	}
 	
+	/**
+	 * Gives the index for the class. Used when writing a title-class to nbt.
+	 * When reading a title-class from nbt, <code>getClassFromInt</code> should be used to return the index to a title-class.
+	 * @param e The title-class to convert.
+	 * @return an index number that matches this title-class
+	 */
 	public static int getIntFromClass(EnumClass e)
 	{
 		return e.ordinal();
 	}
 	
+	/**
+	 * Takes the enum name for this title-class and returns a lowercase version.
+	 * Aside from regular use of the method, it is useful for producing
+	 * the unlocalized name of the title-class using <code>"title." + titleClass.toString()</code>
+	 * @return the name of this title-class
+	 */
 	@Override
 	public String toString()
 	{
 		return this.name().toLowerCase();
 	}
 	
+	/**
+	 * Translates and returns the proper name of this title-class. Should only be used client-side.
+	 * For usage in messages sent to a player from a server, use <code>asTextComponent()</code>.
+	 * For debugging purposes, use <code>toString()</code> instead.
+	 * @return a translated string of the name.
+	 */
 	@SideOnly(Side.CLIENT)
 	public String getDisplayName()
 	{
 		return I18n.format("title." + this.toString());
 	}
 	
+	/**
+	 * Creates a text component for this title-class that will be translated client-side.
+	 * Used for messages from the mod that for example will be sent trough chat.
+	 * @return a text component that will translate into the name of the title-class
+	 */
 	public ITextComponent asTextComponent()
 	{
 		return new TextComponentTranslation("title." + this.toString());
