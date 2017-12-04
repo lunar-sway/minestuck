8d5c8e629216f8c7c1a59a4350f954da25aa0c83
Ben-Kirksey <falconprince3@yahoo.com>
2017-12-04 14:23:28 -0600

    Quantum Saber withering

--- src/com/mraof/minestuck/item/weapon/ItemUraniumWeapon.java
+++ src/com/mraof/minestuck/item/weapon/ItemUraniumWeapon.java
@@ -8,23 +8,18 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.common.util.EnumHelper;
 
-public class ItemUraniumWeapon extends ItemWeapon
+public class ItemUraniumWeapon extends ItemPotionWeapon
 {
 	public static Item.ToolMaterial toolUranium = EnumHelper.addToolMaterial("URANIUM", 3, 1220, 12.0F, 6.0F, 15).setRepairItem(new ItemStack(MinestuckItems.rawUranium));
 	
 	public ItemUraniumWeapon(int maxUses, int damageVsEntity, double weaponSpeed, int enchantability, String name)
 	{
-		super(toolUranium, maxUses, damageVsEntity, weaponSpeed, enchantability, name);
+		super(toolUranium, maxUses, damageVsEntity, weaponSpeed, enchantability, name, new PotionEffect(MobEffects.WITHER, 100, 1));
 	}
 	
 	public ItemUraniumWeapon(int maxUses, int damageVsEntity, double weaponSpeed, int enchantability, String name, int radius, int terminus)
 	{
-		super(toolUranium, maxUses, damageVsEntity, weaponSpeed, enchantability, name);
+		super(toolUranium, maxUses, damageVsEntity, weaponSpeed, enchantability, name, new PotionEffect(MobEffects.WITHER, 100, 1));
 		setTerminus(radius, terminus);
-	}
-	
-	public PotionEffect getEffect()
-	{
-		return new PotionEffect(MobEffects.WITHER, 100, 1);
 	}
 }
