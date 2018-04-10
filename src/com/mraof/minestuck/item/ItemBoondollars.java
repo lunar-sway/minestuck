440c05e18ae0119bdf8ed57c02a3253062bf578d
kirderf1 <kirderf4@gmail.com>
2018-04-10 16:39:36 +0200

    Added boondollar subtypes to creative tab

--- src/com/mraof/minestuck/item/ItemBoondollars.java
+++ src/com/mraof/minestuck/item/ItemBoondollars.java
@@ -2,6 +2,7 @@ package com.mraof.minestuck.item;
 
 import com.mraof.minestuck.util.MinestuckPlayerData;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,6 +10,7 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.text.translation.I18n;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
@@ -19,10 +21,10 @@ import java.util.List;
 
 public class ItemBoondollars extends Item
 {
-	public ItemBoondollars()
+	public ItemBoondollars()	//TODO Add custom crafting recipe that merges boondollar stacks
 	{
 		setUnlocalizedName("boondollars");
-		setCreativeTab(MinestuckItems.tabMinestuck);
+		setCreativeTab(TabMinestuck.instance);
 		setMaxStackSize(1);
 	}
 	
@@ -34,6 +36,19 @@ public class ItemBoondollars extends Item
 			MinestuckPlayerData.addBoondollars(playerIn, getCount(playerIn.getHeldItem(handIn)));
 		}
 		return new ActionResult<>(EnumActionResult.SUCCESS, ItemStack.EMPTY);
+	}
+	
+	@Override
+	public void getSubItems(CreativeTabs tab, NonNullList<ItemStack> items)
+	{
+		if(isInCreativeTab(tab))
+		{
+			items.add(new ItemStack(this));
+			items.add(setCount(new ItemStack(this), 10));
+			items.add(setCount(new ItemStack(this), 100));
+			items.add(setCount(new ItemStack(this), 1000));
+			items.add(setCount(new ItemStack(this), 10000));
+		}
 	}
 	
 	@SideOnly(Side.CLIENT)
