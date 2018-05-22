8441c82a04821bfc9c90c21547c079b4045bf7be
Ben-Kirksey <falconprince3@yahoo.com>
2018-05-22 12:47:27 -0500

    Respawn dimension changes
    The respawn dimension is no longer the dimension the death was in.
    Players are sent to their Lands if they have one, and are sent to their personal respawn dimensions if they don't.

--- src/com/mraof/minestuck/world/WorldProviderSkaia.java
+++ src/com/mraof/minestuck/world/WorldProviderSkaia.java
@@ -1,10 +1,17 @@
 package com.mraof.minestuck.world;
 
+import com.mraof.minestuck.network.skaianet.SburbConnection;
+import com.mraof.minestuck.network.skaianet.SkaianetHandler;
+import com.mraof.minestuck.util.IdentifierHandler;
 import com.mraof.minestuck.world.gen.ChunkProviderSkaia;
+
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Biomes;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DimensionType;
 import net.minecraft.world.WorldProvider;
+import net.minecraft.world.WorldProvider.WorldSleepResult;
 import net.minecraft.world.biome.BiomeProviderSingle;
 import net.minecraft.world.gen.IChunkGenerator;
 
@@ -34,6 +41,33 @@ public class WorldProviderSkaia extends WorldProvider
 	public float calculateCelestialAngle(long par1, float par3)
 	{
 		return 1.0F;
+	}
+	
+	@Override
+	public boolean canRespawnHere()
+	{
+		return false;
+	}
+	
+	@Override
+	public int getRespawnDimension(EntityPlayerMP player)
+	{
+		int dimOut = 0;
+		SburbConnection c = SkaianetHandler.getMainConnection(IdentifierHandler.encode(player), true);
+		if(c == null || !c.enteredGame())
+			dimOut = player.getSpawnDimension();	//Method outputs 0 when no spawn dimension is set, sending players to the overworld.
+		else
+		{
+			dimOut = c.getClientDimension();
+		}
+		
+		return dimOut;
+	}
+	
+	@Override
+	public WorldSleepResult canSleepAt(EntityPlayer player, BlockPos pos)
+	{
+		return WorldSleepResult.ALLOW;
 	}
 	
 	@Override
