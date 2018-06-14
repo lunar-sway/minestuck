ca2d78ad48ee0966de43592d923f470ba359031d
kirderf1 <kirderf4@gmail.com>
2018-06-14 23:08:59 +0200

    Top middle model

--- src/com/mraof/minestuck/block/BlockTotemLathe.java
+++ src/com/mraof/minestuck/block/BlockTotemLathe.java
@@ -229,12 +229,12 @@ public class BlockTotemLathe extends BlockLargeMachine
 		MID_RIGHT(		new AxisAlignedBB(0.0D, 0.0D,  3/16D, 14/16D, 10/16D, 9/16D)),
 		
 		TOP_MIDRIGHT(	new AxisAlignedBB(8/16D, 0/16D, 6/16D, 16/16D,  12/16D, 14/16D)),
-		TOP_MIDLEFT(	new AxisAlignedBB(0/16D, 0/16D, 6/16D, 16/16D,  12/16D, 14/16D)),
+		TOP_MIDLEFT(	new AxisAlignedBB(0.0D, 3/16D, 1/16D, 1.0D,  1.0D, 11/16D)),
 		TOP_LEFT(		new AxisAlignedBB(0.0D, 0.0D, 1/16D, 1.0D,  1.0D, 11/16D)),
 		
 		BOTTOM_LEFT_CARD_1(new AxisAlignedBB(0.0D, 0.0D, 1/16D, 1.0D, 1.0D, 13/16D)),
 		BOTTOM_LEFT_CARD_2(new AxisAlignedBB(0.0D, 0.0D, 1/16D, 1.0D, 1.0D, 13/16D)),
-		ROD_LEFT_ACTIVE(new AxisAlignedBB(0.0D, 0.0D,  3/16D, 1.0D,  1.0D, 9/16D)),
+		ROD_LEFT_ACTIVE(new AxisAlignedBB(0.0D, 4/16D,  3/16D, 1.0D,  10/16D, 9/16D)),
 		ROD_RIGHT_CARVED(new AxisAlignedBB(0.0D, 4/16D, 3/16D, 1.0D,  10/16D, 9/16D)),
 		MID_RIGHT_ACTIVE(new AxisAlignedBB(0.0D, 0.0D,  3/16D, 14/16D, 10/16D, 9/16D));
 		
