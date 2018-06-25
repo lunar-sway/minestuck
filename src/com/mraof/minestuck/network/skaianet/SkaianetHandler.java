9de42f543930ade6e41193554d710c11e87c72cf
BenjaminKirksey <benjamindkirksey@gmail.com>
2018-06-25 14:37:51 -0500

    Quick solution to editmode location after Entry.

--- src/com/mraof/minestuck/network/skaianet/SkaianetHandler.java
+++ src/com/mraof/minestuck/network/skaianet/SkaianetHandler.java
@@ -694,6 +694,8 @@ public class SkaianetHandler {
 			
 			c.centerX = 0;
 			c.centerZ = 0;
+			//A similar solution to this line of code will appear upstream in the near future. In any way it may be important, defer to that version.
+			c.useCoordinates = false;	//This is a different set of coordinates from the ones defined immediately above.
 			updateAll();
 		} else
 			Debug.errorf("Couldn't move %s to their Land. Stopping entry.", player.getName());
