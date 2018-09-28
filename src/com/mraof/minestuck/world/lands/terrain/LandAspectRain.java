e0b5f67b6084d53718f0d52c1a8c5301cb16c8f1
BenjaminKirksey <benjamindkirksey@gmail.com>
2018-09-27 19:40:34 -0500

    Beginning of Rain

--- src/com/mraof/minestuck/world/lands/terrain/LandAspectRain.java
+++ src/com/mraof/minestuck/world/lands/terrain/LandAspectRain.java
@@ -1,44 +1,81 @@
 package com.mraof.minestuck.world.lands.terrain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import com.mraof.minestuck.block.BlockMinestuckStone;
 import com.mraof.minestuck.block.MinestuckBlocks;
+import com.mraof.minestuck.block.MinestuckBlocks.EnumSlabStairMaterial;
 import com.mraof.minestuck.entity.consort.EnumConsort;
 import com.mraof.minestuck.world.biome.BiomeMinestuck;
-import com.mraof.minestuck.world.lands.decorator.*;
+import com.mraof.minestuck.world.lands.decorator.ILandDecorator;
+import com.mraof.minestuck.world.lands.decorator.LeaflessTreeDecorator;
+import com.mraof.minestuck.world.lands.decorator.SurfaceDecoratorVein;
+import com.mraof.minestuck.world.lands.decorator.UndergroundDecoratorVein;
 import com.mraof.minestuck.world.lands.gen.ChunkProviderLands;
+import com.mraof.minestuck.world.lands.gen.DefaultTerrainGen;
+import com.mraof.minestuck.world.lands.gen.ILandTerrainGen;
+import com.mraof.minestuck.world.lands.gen.LandTerrainGenBase;
 import com.mraof.minestuck.world.lands.structure.blocks.StructureBlockRegistry;
-import net.minecraft.block.*;
+
+import net.minecraft.block.BlockColored;
+import net.minecraft.block.BlockDirt;
+import net.minecraft.block.BlockPlanks;
+import net.minecraft.block.BlockPrismarine;
+import net.minecraft.block.BlockTallGrass;
+import net.minecraft.block.BlockWoodSlab;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.util.math.Vec3d;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LandAspectRain extends TerrainLandAspect 
 {
-	static Vec3d skyColor = new Vec3d(0.98D, 0.5D, 0.2D);
-
+	static Vec3d skyColor = new Vec3d(0.2D, 0.5D, 0.98D);
+	
 	//TODO:
-	//Literally all of it
+	//Craftable terrain materials
+	//Pink stone brick temples
+	//Light water bucket textures, light water for lands with Light?
+	//Light Clouds (Dungeons?)
+	//Custom dungeon loot
+	//Custom consort dialogue
+	//Custom names and descriptions
+	//Probably something underwater
+	
+	//Unrelated, there should be a way to use events to make leaves update their appearance when the client's settings change.
+	//This would be good to make them synchronize with fast/fancy
+	
+	//Don't forget the weapons that Winter made. There should be two in particular that stand out.
 	@Override
 	public void registerBlocks(StructureBlockRegistry registry)
 	{
-		registry.setBlockState("surface", Blocks.GRASS.getDefaultState());
-		registry.setBlockState("upper", Blocks.DIRT.getDefaultState());
-		registry.setBlockState("structure_primary", Blocks.PRISMARINE.getDefaultState());
-		registry.setBlockState("structure_primary_decorative", Blocks.PRISMARINE.getDefaultState().withProperty(BlockPrismarine.VARIANT, BlockPrismarine.EnumType.BRICKS));
-		registry.setBlockState("structure_secondary", MinestuckBlocks.stone.getDefaultState().withProperty(BlockMinestuckStone.VARIANT, BlockMinestuckStone.BlockType.FROST_BRICK));
-		registry.setBlockState("structure_secondary_stairs", MinestuckBlocks.frostBrickStairs.getDefaultState());
-		registry.setBlockState("structure_secondary_decorative", MinestuckBlocks.stone.getDefaultState().withProperty(BlockMinestuckStone.VARIANT, BlockMinestuckStone.BlockType.FROST_CHISELED));
-		registry.setBlockState("structure_planks", Blocks.PLANKS.getDefaultState().withProperty(BlockPlanks.VARIANT, BlockPlanks.EnumType.SPRUCE));
-		registry.setBlockState("structure_planks_slab", Blocks.WOODEN_SLAB.getDefaultState().withProperty(BlockWoodSlab.VARIANT, BlockPlanks.EnumType.SPRUCE));
-		registry.setBlockState("river", Blocks.ICE.getDefaultState());
-		registry.setBlockState("light_block", Blocks.SEA_LANTERN.getDefaultState());
-		registry.setBlockState("bucket1", Blocks.SNOW.getDefaultState());
-		registry.setBlockState("bush", Blocks.TALLGRASS.getDefaultState().withProperty(BlockTallGrass.TYPE, BlockTallGrass.EnumType.FERN));
-		registry.setBlockState("structure_wool_1", Blocks.WOOL.getDefaultState().withProperty(BlockColored.COLOR, EnumDyeColor.WHITE));
-		registry.setBlockState("structure_wool_3", Blocks.WOOL.getDefaultState().withProperty(BlockColored.COLOR, EnumDyeColor.CYAN));
+		registry.setBlockState("surface", MinestuckBlocks.chalk.getDefaultState());
+		registry.setBlockState("upper", MinestuckBlocks.chalk.getDefaultState());
+		registry.setBlockState("ground", MinestuckBlocks.pinkStoneSmooth.getDefaultState());
+		registry.setBlockState("ocean", Blocks.WATER.getDefaultState());
+		registry.setBlockState("structure_primary", MinestuckBlocks.pinkStoneBricks.getDefaultState());
+		registry.setBlockState("structure_primary_stairs", EnumSlabStairMaterial.PINK_BRICK.getStair().getDefaultState());
+		registry.setBlockState("structure_primary_decorative", MinestuckBlocks.pinkStoneChisel.getDefaultState());
+		registry.setBlockState("structure_secondary", MinestuckBlocks.pinkStonePolish.getDefaultState());
+		registry.setBlockState("structure_secondary_stairs", EnumSlabStairMaterial.CHALK_BRICK.getStair().getDefaultState());
+		registry.setBlockState("structure_secondary_decorative", MinestuckBlocks.pinkStoneChisel.getDefaultState());
+		registry.setBlockState("structure_planks", MinestuckBlocks.deadPlanks.getDefaultState());
+		registry.setBlockState("structure_planks_slab", EnumSlabStairMaterial.DEAD.getSlab().getDefaultState());
+		registry.setBlockState("bush", Blocks.DEADBUSH.getDefaultState());
+		registry.setBlockState("structure_wool_1", Blocks.WOOL.getDefaultState().withProperty(BlockColored.COLOR, EnumDyeColor.YELLOW));
+		registry.setBlockState("structure_wool_3", Blocks.WOOL.getDefaultState().withProperty(BlockColored.COLOR, EnumDyeColor.MAGENTA));
+	}
+	
+	@Override
+	public ILandTerrainGen createTerrainGenerator(ChunkProviderLands chunkProvider, Random rand)
+	{
+		DefaultTerrainGen out = new DefaultTerrainGen(chunkProvider, rand);
+		out.setSeaHeight(64);		//The ocean is two blocks higher than any other Lands
+		out.oceanVariation += 0.2F;	//The ground under the ocean is a bit more varied
+		out.roughHeight -= 0.1F;	//Rough biomes average a bit lower than normal
+		out.roughVariation += 0.1F;	//But they are more hilly overall
+		return out;
 	}
 	
 	@Override
@@ -56,17 +93,14 @@ public class LandAspectRain extends TerrainLandAspect
 	public List<ILandDecorator> getDecorators()
 	{
 		ArrayList<ILandDecorator> list = new ArrayList<ILandDecorator>();
+		list.add(new LeaflessTreeDecorator(MinestuckBlocks.deadLog.getDefaultState(), 0.5F, BiomeMinestuck.mediumNormal));
+		list.add(new LeaflessTreeDecorator(MinestuckBlocks.deadLog.getDefaultState(), 0.25F, BiomeMinestuck.mediumRough));
 		
-		list.add(new SurfaceDecoratorVein(Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.COARSE_DIRT), 10, 32, BiomeMinestuck.mediumRough, BiomeMinestuck.mediumOcean));
-		list.add(new SurfaceDecoratorVein(Blocks.ICE.getDefaultState(), 5, 8, BiomeMinestuck.mediumRough));
-		list.add(new SurfaceDecoratorVein(Blocks.SNOW.getDefaultState(), 8, 16, BiomeMinestuck.mediumRough));
-		list.add(new SurfaceDecoratorVein(Blocks.SNOW.getDefaultState(), 15, 16, BiomeMinestuck.mediumNormal));
-		
-		list.add(new UndergroundDecoratorVein(Blocks.PACKED_ICE.getDefaultState(), 2, 8, 64));
-		list.add(new UndergroundDecoratorVein(Blocks.SNOW.getDefaultState(), 3, 16, 64));
-		list.add(new UndergroundDecoratorVein(Blocks.DIRT.getDefaultState(), 3, 28, 64));
-		list.add(new UndergroundDecoratorVein(Blocks.COAL_ORE.getDefaultState(), 13, 17, 64));
-		list.add(new UndergroundDecoratorVein(Blocks.DIAMOND_ORE.getDefaultState(), 3, 6, 24));
+		list.add(new UndergroundDecoratorVein(MinestuckBlocks.pinkStonePolish.getDefaultState(), 2, 8, 64));
+		list.add(new UndergroundDecoratorVein(MinestuckBlocks.coalOrePinkStone.getDefaultState(), 13, 17, 64));
+		list.add(new UndergroundDecoratorVein(MinestuckBlocks.lapisOrePinkStone.getDefaultState(), 4, 7, 24));
+		list.add(new UndergroundDecoratorVein(MinestuckBlocks.goldOrePinkStone.getDefaultState(), 4, 9, 32));
+		list.add(new UndergroundDecoratorVein(MinestuckBlocks.diamondOrePinkStone.getDefaultState(), 3, 6, 24));
 		return list;
 	}
 	
@@ -91,6 +125,12 @@ public class LandAspectRain extends TerrainLandAspect
 	public float getOceanChance()
 	{
 		return 3/4F;
+	}
+	
+	@Override
+	public float getRainfall()
+	{
+		return 0.6F;
 	}
 	
 	@Override
