64fc0dd9fb41049873dbeccbb579b21b59f79c08
Cibernet <cyberneticGuardian@gmail.com>
2018-11-13 19:11:57 -0500

    added in a few things so that the ShatterBeacon and the Vrillyhoo Hammer work properly

--- src/com/mraof/minestuck/item/weapon/ItemWeapon.java
+++ src/com/mraof/minestuck/item/weapon/ItemWeapon.java
@@ -97,6 +97,7 @@ public class ItemWeapon extends ItemSword //To allow enchantments such as sharpn
 	{
 		if(!unbreakable)
 			stack.damageItem(1, player);
+		System.out.println("boop");
 		return true;
 	}
 
