e3c4ca626d58826c31e6e8e88918df85e075bb00
Cibernet <cyberneticGuardian@gmail.com>
2018-11-17 20:37:55 -0500

    fixed a bug with the shop signs where the first sign you break will always drop a fraymotif sign

--- src/com/mraof/minestuck/entity/item/EntityShopPoster.java
+++ src/com/mraof/minestuck/entity/item/EntityShopPoster.java
@@ -5,9 +5,14 @@ import java.util.List;
 import java.util.Set;
 
 import com.google.common.collect.Lists;
+import com.mraof.minestuck.entity.EntityFrog;
 import com.mraof.minestuck.item.MinestuckItems;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -15,15 +20,24 @@ import net.minecraft.world.World;
 public class EntityShopPoster extends EntityHangingArt<EntityShopPoster.ShopArt>
 {
 	protected int dmg = 0;
+	private static final DataParameter<Integer> TYPE = EntityDataManager.<Integer>createKey(EntityFrog.class, DataSerializers.VARINT);
+	
 	public EntityShopPoster(World worldIn)
 	{
 		super(worldIn);
 	}
 	
+	protected void entityInit()
+    {
+        super.entityInit();
+        this.dataManager.register(TYPE, 0);
+        
+    }
+	
 	public EntityShopPoster(World worldIn, BlockPos pos, EnumFacing facing, ItemStack stack, int meta)
 	{
 		super(worldIn, pos, facing, stack, meta, false);
-		dmg = meta;
+		setType(meta);
 	}
 	
 	@Override
@@ -41,8 +55,33 @@ public class EntityShopPoster extends EntityHangingArt<EntityShopPoster.ShopArt>
 	@Override
 	public ItemStack getStackDropped()
 	{
-		return new ItemStack(MinestuckItems.shopPoster, 1, dmg);
+		return new ItemStack(MinestuckItems.shopPoster, 1, getType());
 	}
+	
+	private void setType(int i) 
+    {
+    	this.dataManager.set(TYPE, i);
+	}
+
+	public int getType() 
+	{
+		return this.dataManager.get(TYPE);
+	}
+	
+	//NBT
+		public void writeEntityToNBT(NBTTagCompound compound)
+	    {
+	        super.writeEntityToNBT(compound);
+	        compound.setInteger("Type", this.getType());
+	    }
+
+	    public void readEntityFromNBT(NBTTagCompound compound)
+	    {
+	        super.readEntityFromNBT(compound);
+	        
+	        if(compound.hasKey("Type")) setType(compound.getInteger("Type"));
+	        else setType(0);
+	    }
 	
 	public static enum ShopArt implements EntityHangingArt.IArt
 	{
