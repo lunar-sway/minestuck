efd0b23ca54c48809ae7d255d1d427a938b313bd
Cibernet <Cibernet83@users.noreply.github.com>
2018-11-20 12:04:01 -0500

    Changed Susan's eye animation to be more accurate to the webcomic
    let's just hope that it doesn't give anyone seizures

--- src/com/mraof/minestuck/client/renderer/entity/frog/LayerFrogEyes.java
+++ src/com/mraof/minestuck/client/renderer/entity/frog/LayerFrogEyes.java
@@ -15,8 +15,6 @@ public class LayerFrogEyes implements LayerRenderer<EntityFrog>
 	private final ModelFrog frogModel = new ModelFrog();
 	private final RenderFrog frogRender;
 	private float colorMin = 0;
-	private int type = 0;
-	private boolean isSusan = false;
 	private String name;
 	
 	public LayerFrogEyes(RenderFrog renderIn)
@@ -34,7 +32,7 @@ public class LayerFrogEyes implements LayerRenderer<EntityFrog>
 			this.frogRender.bindTexture(this.getTexture(frog));
 			if (frog.getType() == 6)
 	        {
-				this.isSusan = true;
+	            /*
 	            int i1 = 25;
 	            int i = frog.ticksExisted / 25 + frog.getEntityId();
 	            int j = EnumDyeColor.values().length;
@@ -43,13 +41,54 @@ public class LayerFrogEyes implements LayerRenderer<EntityFrog>
 	            float f = ((float)(frog.ticksExisted % 25) + partialTicks) / 25.0F;
 	            float[] afloat1 = EntitySheep.getDyeRgb(EnumDyeColor.byMetadata(k));
 	            float[] afloat2 = EntitySheep.getDyeRgb(EnumDyeColor.byMetadata(l));
-	            
+	           
 	            GlStateManager.color(afloat1[0] * (1.0F - f) + afloat2[0] * f, afloat1[1] * (1.0F - f) + afloat2[1] * f, afloat1[2] * (1.0F - f) + afloat2[2] * f);
+	             */
+				
+				float r, g, b;
+				switch(frog.ticksExisted % 4)
+				{
+					default:
+					{
+						r = 1f;
+						g = 0f;
+						b = 0f;
+					} break;
+
+					case 1:
+					{
+						r = 0f;
+						g = 1f;
+						b = 0f;
+					} break;
+
+					case 2:
+					{
+						r = 0f;
+						g = 0.25f;
+						b = 1f;
+					} break;
+					
+					case 3:
+					{
+						r = 0.5f;
+						g = 0f;
+						b = 1f;
+					} break;
+				}
+				
+				GlStateManager.color(r, g, b);
+				
+				this.frogModel.setModelAttributes(this.frogRender.getMainModel());
+	            this.frogModel.setLivingAnimations(frog, limbSwing, limbSwingAmount, partialTicks);
+	            this.frogModel.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, 1f, frog);
+		        this.frogModel.render(frog, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+		        
+				GlStateManager.disableBlend();
 	        }
-			else if (frog.getType() > frog.maxTypes() || frog.getType() < 1 || frog.getType() == 6)
+			else if (frog.getType() > frog.maxTypes() || frog.getType() < 1)
 	        {
 				int eyeColor = frog.getEyeColor();
-				type = frog.getEyeType();
 				
 				float r = (float) ((eyeColor & 16711680) >> 16) / 255f;
 				float g = (float) ((eyeColor & 65280) >> 8) / 255f;
@@ -60,14 +99,15 @@ public class LayerFrogEyes implements LayerRenderer<EntityFrog>
 				if(b < this.colorMin) b = this.colorMin;
 				
 				GlStateManager.color(r, g, b, 1f);
+
+				this.frogModel.setModelAttributes(this.frogRender.getMainModel());
+	            this.frogModel.setLivingAnimations(frog, limbSwing, limbSwingAmount, partialTicks);
+	            this.frogModel.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, 1f, frog);
+		        this.frogModel.render(frog, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+		        
+				GlStateManager.disableBlend();
 	        }
 			
-			this.frogModel.setModelAttributes(this.frogRender.getMainModel());
-            this.frogModel.setLivingAnimations(frog, limbSwing, limbSwingAmount, partialTicks);
-            this.frogModel.setRotationAngles(limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, 1f, frog);
-	        this.frogModel.render(frog, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
-	        
-			GlStateManager.disableBlend();
 		}
 	}
 
