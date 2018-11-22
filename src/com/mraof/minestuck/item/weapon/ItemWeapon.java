4039732b44fb44e4862eba6d4cc5eafaa7abb2e1
Cibernet <Cibernet83@users.noreply.github.com>
2018-11-22 10:07:09 -0500

    cleaned up some code in ItemWeapon

--- src/com/mraof/minestuck/item/weapon/ItemWeapon.java
+++ src/com/mraof/minestuck/item/weapon/ItemWeapon.java
@@ -97,7 +97,6 @@ public class ItemWeapon extends ItemSword //To allow enchantments such as sharpn
 	{
 		if(!unbreakable)
 			stack.damageItem(1, player);
-		System.out.println("boop");
 		return true;
 	}
 
