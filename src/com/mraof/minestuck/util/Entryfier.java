f20873c675f0913eb50b4286a72a22a1634c07e2
SarahKirksey <benjamindkirksey@gmail.com>
2019-04-21 15:11:19 -0700

    RS Support?

---
+++ src/com/mraof/minestuck/util/Entryfier.java
@@ -0,0 +1,500 @@
+package com.mraof.minestuck.util;
+
+import static com.mraof.minestuck.MinestuckConfig.artifactRange;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.mraof.minestuck.MinestuckConfig;
+import com.mraof.minestuck.block.BlockGate;
+import com.mraof.minestuck.block.MinestuckBlocks;
+import com.mraof.minestuck.editmode.ServerEditHandler;
+import com.mraof.minestuck.event.ServerEventHandler;
+import com.mraof.minestuck.modSupport.RefinedStorageSupport;
+import com.mraof.minestuck.network.skaianet.SburbConnection;
+import com.mraof.minestuck.network.skaianet.SkaianetHandler;
+import com.mraof.minestuck.tileentity.TileEntityComputer;
+import com.mraof.minestuck.tileentity.TileEntityGate;
+import com.mraof.minestuck.tileentity.TileEntityTransportalizer;
+import com.mraof.minestuck.world.GateHandler;
+import com.mraof.minestuck.world.MinestuckDimensionHandler;
+import com.raoulvdberge.refinedstorage.api.network.node.INetworkNode;
+import com.raoulvdberge.refinedstorage.api.network.node.INetworkNodeManager;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityList;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.Chunk.EnumCreateEntityType;
+import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
+import net.minecraftforge.fml.common.Loader;
+
+public class Entryfier
+{
+	private int xDiff;
+	private int yDiff;
+	private int zDiff;
+	private int topY;
+	private BlockPos origin;
+	private boolean creative;
+	private HashSet<BlockMove> blockMoves;
+	
+	private static boolean isRefinedStorageInstalled = Loader.isModLoaded("refinedstorage");
+	
+	public enum EntryPrepResult
+	{
+		success,
+		notInCreative,
+		stealingIsWrong,
+		noComputer,
+		notAPlayer,
+		stealingModStuffIsAlsoWrong
+	}
+	
+	public EntryPrepResult prepareDestination(BlockPos origin, Entity player, WorldServer worldserver0)
+	{
+		if(!(player instanceof EntityPlayerMP))
+		{
+			return EntryPrepResult.notAPlayer;
+		}
+		
+		blockMoves = new HashSet<BlockMove>();
+		
+		Debug.infof("Starting entry for player %s", player.getName());
+		int x = origin.getX();
+		int y = origin.getY();
+		int z = origin.getZ();
+		this.origin = origin;
+		
+		creative = ((EntityPlayerMP) player).interactionManager.isCreative();
+		SburbConnection conn = SkaianetHandler.getMainConnection(IdentifierHandler.encode((EntityPlayer) player), true);
+		
+		topY = MinestuckConfig.adaptEntryBlockHeight ? getTopHeight(worldserver0, x, y, z) : y + artifactRange;
+		yDiff = 127 - topY;
+		xDiff = 0 - x;
+		zDiff = 0 - z;
+		
+		Debug.debug("Loading block movements...");
+		long time = System.currentTimeMillis();
+		int bl = 0;
+		boolean foundComputer = false;
+		for(int blockX = x - artifactRange; blockX <= x + artifactRange; blockX++)
+		{
+			int zWidth = (int) Math.sqrt((artifactRange+0.5) * (artifactRange+0.5) - (blockX - x) * (blockX - x));
+			for(int blockZ = z - zWidth; blockZ <= z + zWidth; blockZ++)
+			{
+				Chunk c = worldserver0.getChunkFromChunkCoords(blockX >> 4, blockZ >> 4);
+				
+				int height = (int) Math.sqrt(artifactRange * artifactRange - (((blockX - x) * (blockX - x) + (blockZ - z) * (blockZ - z)) / 2));
+				
+				int blockY;
+				for(blockY = Math.max(0, y - height); blockY <= Math.min(topY, y + height); blockY++)
+				{
+					BlockPos pos = new BlockPos(blockX, blockY, blockZ);
+					BlockPos pos1 = pos.add(xDiff, yDiff, zDiff);
+					IBlockState block = worldserver0.getBlockState(pos);
+					TileEntity te = worldserver0.getTileEntity(pos);
+					
+					Block gotBlock = block.getBlock();
+					
+					if(gotBlock == Blocks.BEDROCK || gotBlock == Blocks.PORTAL)
+					{
+						blockMoves.add(new BlockMove(c, pos, pos1, Blocks.AIR.getDefaultState(), true));
+						continue;
+					}
+					else if(!creative && (gotBlock == Blocks.COMMAND_BLOCK || gotBlock == Blocks.CHAIN_COMMAND_BLOCK || gotBlock == Blocks.REPEATING_COMMAND_BLOCK))
+					{
+						return EntryPrepResult.notInCreative;
+					} else if(te instanceof TileEntityComputer)		//If the block is a computer
+					{
+						if(!((TileEntityComputer)te).owner.equals(IdentifierHandler.encode((EntityPlayer) player)))	//You can't Enter with someone else's computer
+						{
+							return EntryPrepResult.stealingIsWrong;
+						}
+						
+						foundComputer = true;	//You have a computer in range. That means you're taking your computer with you when you Enter. Smart move.
+					}
+					
+					//Shouldn't this line check if the block is an edge block?
+					blockMoves.add(new BlockMove(c, pos, pos1, block, false, getExtraData(worldserver0, pos)));
+				}
+				
+				//What does this code accomplish?
+				for(blockY += yDiff; blockY <= 255; blockY++)
+				{
+					//The first BlockPos isn't used for this operation.
+					blockMoves.add(new BlockMove(c, BlockPos.ORIGIN, new BlockPos(blockX + xDiff, blockY, blockZ + zDiff), Blocks.AIR.getDefaultState(), false));
+				}
+			}
+		}
+		
+		if(foundComputer == false && MinestuckConfig.needComputer)
+		{
+			return EntryPrepResult.noComputer;
+		}
+		
+		return EntryPrepResult.success;
+	}
+	
+	private NBTTagCompound getExtraData(WorldServer world, BlockPos pos)
+	{
+		if(isRefinedStorageInstalled)
+		{
+			INetworkNodeManager manager = RefinedStorageSupport.API.getNetworkNodeManager(world);
+			INetworkNode node = manager.getNode(pos);
+			
+			if(node != null)
+			{
+				NBTTagCompound tag = new NBTTagCompound();
+				
+				tag.setTag(RefinedStorageSupport.NBT_NODE, node.write(new NBTTagCompound()));
+				tag.setString(RefinedStorageSupport.NBT_NODE_ID, node.getId());
+				tag.setString("responsibleMod", "refinedstorage");
+				
+				manager.markForSaving();
+				return tag;
+			}
+			
+		}
+		return null;
+	}
+
+	public void finalizeDestination(Entity player, WorldServer worldserver0, WorldServer worldserver1)
+	{
+		if(player instanceof EntityPlayerMP)
+		{
+			int x = origin.getX();
+			int y = origin.getY();
+			int z = origin.getZ();
+			
+			Debug.debug("Loading spawn chunks...");
+			for(int chunkX = ((x + xDiff - artifactRange) >> 4) - 1; chunkX <= ((x + xDiff + artifactRange) >> 4) + 2; chunkX++)		//Prevent anything generating on the piece that we move
+				for(int chunkZ = ((z + zDiff - artifactRange) >> 4) - 1; chunkZ <= ((z + zDiff + artifactRange) >> 4) + 2; chunkZ++)	//from the overworld.
+					worldserver1.getChunkProvider().provideChunk(chunkX, chunkZ);
+			
+			//Set again, but with a more precise value now that the y-coordinate is properly decided.
+			MinestuckDimensionHandler.setSpawn(worldserver1.provider.getDimension(), new BlockPos(x + xDiff, y + yDiff, z + zDiff));
+			
+			//This is split into two sections because moves that require block updates should happen after the ones that don't.
+			//This helps to ensure that "anchored" blocks like torches still have the blocks they are anchored to when they update.
+			//Some blocks like this (confirmed for torches, rails, and glowystone) will break themselves if they update without their anchor.
+			Debug.debug("Moving blocks...");
+			HashSet<BlockMove> blockMoves2 = new HashSet<BlockMove>();
+			for(BlockMove move : blockMoves)
+			{
+				if(!move.update)
+					move.copy(worldserver1.getChunkFromBlockCoords(move.dest));
+				else
+					blockMoves2.add(move);
+			}
+			for(BlockMove move : blockMoves2)
+			{
+				move.copy(worldserver1.getChunkFromBlockCoords(move.dest));
+			}
+			blockMoves2.clear();
+			
+			Debug.debug("Teleporting entities...");
+			//The fudge here is to ensure that the AABB will always contain every entity meant to be moved.
+			// As entities outside the radius will be excluded from transport anyway, this is fine.
+			AxisAlignedBB entityTeleportBB = player.getEntityBoundingBox().grow(artifactRange + 0.5);
+			List<Entity> list = worldserver0.getEntitiesWithinAABBExcludingEntity(player, entityTeleportBB);
+			Iterator<Entity> iterator = list.iterator();
+			while (iterator.hasNext())
+			{
+				Entity e = iterator.next();
+				if(origin.distanceSqToCenter(e.posX, e.posY, e.posZ) <= artifactRange*artifactRange)
+				{
+					if(MinestuckConfig.entryCrater || e instanceof EntityPlayer || !creative && e instanceof EntityItem)
+					{
+						if(e instanceof EntityPlayer && ServerEditHandler.getData((EntityPlayer) e) != null)
+							ServerEditHandler.reset(ServerEditHandler.getData((EntityPlayer) e));
+						else
+						{
+							Teleport.teleportEntity(e, worldserver1.provider.getDimension(), null, e.posX + xDiff, e.posY + yDiff, e.posZ + zDiff);
+						}
+						//These entities should no longer be in the world, and this list is later used for entities that *should* remain.
+						iterator.remove();
+					}
+					else	//Copy instead of teleport
+					{
+						Entity newEntity = EntityList.newEntity(e.getClass(), worldserver1);
+						if (newEntity != null)
+						{
+							NBTTagCompound nbttagcompound = new NBTTagCompound();
+							e.writeToNBT(nbttagcompound);
+							nbttagcompound.removeTag("Dimension");
+							newEntity.readFromNBT(nbttagcompound);
+							newEntity.dimension = worldserver1.provider.getDimension();
+							newEntity.setPosition(newEntity.posX + xDiff, newEntity.posY + yDiff, newEntity.posZ + zDiff);
+							worldserver1.spawnEntity(newEntity);
+						}
+					}
+				}
+			}
+			
+			for(BlockMove move : blockMoves)
+			{
+				removeTileEntity(worldserver0, move.source, creative);	//Tile entities need special treatment
+				
+				if(MinestuckConfig.entryCrater && worldserver0.getBlockState(move.source).getBlock() != Blocks.BEDROCK)
+				{
+					if(move.update)
+						worldserver0.setBlockState(move.source, Blocks.AIR.getDefaultState(), 3);
+					else
+						worldserver0.setBlockState(move.source, Blocks.AIR.getDefaultState(), 2);
+				}
+			}
+			blockMoves.clear();
+			
+			player.setPositionAndUpdate(player.posX + xDiff, player.posY + yDiff, player.posZ + zDiff);
+			
+			SkaianetHandler.clearMovingList();
+			
+			//Remove entities that were generated in the process of teleporting entities and removing blocks.
+			// This is usually caused by "anchored" blocks being updated between the removal of their anchor and their own removal.
+			if(!creative || MinestuckConfig.entryCrater)
+			{
+				Debug.debug("Removing entities left in the crater...");
+				List<Entity> removalList = worldserver0.getEntitiesWithinAABBExcludingEntity(player, entityTeleportBB);
+				
+				//We check if the old list contains the entity, because that means it was there before the entities were teleported and blocks removed.
+				// This can be caused by them being outside the Entry radius but still within the AABB,
+				// Or by the player being in creative mode, or having entryCrater disabled, etc.
+				// Ultimately, this means that the entity has already been taken care of as much as it needs to be, and it is inappropriate to remove the entity.
+				removalList.removeAll(list);
+				
+				iterator = removalList.iterator();
+				if(MinestuckConfig.entryCrater)
+				{
+					while (iterator.hasNext())
+					{
+						iterator.next().setDead();
+					}
+				} else
+				{
+					while (iterator.hasNext())
+					{
+						Entity e = iterator.next();
+						if(e instanceof EntityItem)
+							e.setDead();
+					}
+				}
+			}
+			
+			Debug.debug("Placing gates...");
+			
+			GateHandler.findGatePlacement(worldserver1);
+			placeGate(1, new BlockPos(x + xDiff, GateHandler.gateHeight1, z + zDiff), worldserver1);
+			placeGate(2, new BlockPos(x + xDiff, GateHandler.gateHeight2, z + zDiff), worldserver1);
+			
+			ServerEventHandler.tickTasks.add(new PostEntryTask(worldserver1.provider.getDimension(), x + xDiff, y + yDiff, z + zDiff, artifactRange, (byte) 0));
+			
+			Debug.info("Entry finished");
+		}
+	}
+	
+	private static void placeGate(int gateCount, BlockPos pos, WorldServer world)
+	{
+		for(int i = 0; i < 9; i++)
+			if(i == 4)
+			{
+				world.setBlockState(pos, MinestuckBlocks.gate.getDefaultState().cycleProperty(BlockGate.isMainComponent), 0);
+				TileEntityGate tileEntity = (TileEntityGate) world.getTileEntity(pos);
+				tileEntity.gateCount = gateCount;
+			}
+			else world.setBlockState(pos.add((i % 3) - 1, 0, i/3 - 1), MinestuckBlocks.gate.getDefaultState(), 0);
+	}
+	
+	/**
+	 * Determines if it is appropriate to remove the tile entity in the specified location,
+	 * and removes both the tile entity and its corresponding block if so.
+	 * This method is expressly designed to prevent drops from appearing when the block is removed.
+	 * It will also deliberately trigger block updates based on the removal of the tile entity's block.
+	 * @param worldserver0 The world where the tile entity is located
+	 * @param pos The position at which the tile entity is located
+	 * @param creative Whether or not creative-mode rules should be employed
+	 */
+	private static void removeTileEntity(WorldServer worldserver0, BlockPos pos, boolean creative)
+	{
+		TileEntity tileEntity = worldserver0.getTileEntity(pos);
+		if(tileEntity != null)
+		{
+			if(MinestuckConfig.entryCrater || !creative)
+			{
+				String name = worldserver0.getBlockState(pos).getBlock().getRegistryName().toString();
+				try {
+					if(isRefinedStorageInstalled)
+					{
+						INetworkNodeManager manager = RefinedStorageSupport.API.getNetworkNodeManager((World)worldserver0);
+						if(manager.getNode(pos) != null)
+						{
+							manager.removeNode(pos);
+							manager.markForSaving();
+						}
+					}
+					worldserver0.removeTileEntity(pos);
+					worldserver0.setBlockToAir(pos);
+				} catch (NullPointerException e) {
+					Logger.getGlobal().warning("Null Pointer Exception encountered when removing " + name + ". "
+							+ "Notify the mod author that the block should make a null check on its tile entity when broken.");
+				} catch (Exception e) {
+					Logger.getGlobal().warning("Unknown Exception encountered when removing " + name + ". "
+							+ "Notify a Minestuck dev of this error.");
+				}
+			} else
+			{
+				if(tileEntity instanceof TileEntityComputer)	//Avoid duplicating computer data when a computer is kept in the overworld
+					((TileEntityComputer) tileEntity).programData = new NBTTagCompound();
+				else if(tileEntity instanceof TileEntityTransportalizer)
+					worldserver0.removeTileEntity(pos);
+			}
+		}
+	}
+	
+	/**
+	 * Gives the Y-value of the highest non-air block within artifact range of the coordinates provided in the given world.
+	 */
+	private static int getTopHeight(WorldServer world, int x, int y, int z)
+	{
+		Debug.debug("Getting maxY..");
+		int maxY = y;
+		for(int blockX = x - artifactRange; blockX <= x + artifactRange; blockX++)
+		{
+			int zWidth = (int) Math.sqrt(artifactRange * artifactRange - (blockX - x) * (blockX - x));
+			for(int blockZ = z - zWidth; blockZ <= z + zWidth; blockZ++)
+			{
+				int height = (int) (Math.sqrt(artifactRange * artifactRange - (((blockX - x) * (blockX - x) + (blockZ - z) * (blockZ - z)) / 2)));
+				for(int blockY = Math.min(255, y + height); blockY > maxY; blockY--)
+					if(!world.isAirBlock(new BlockPos(blockX, blockY, blockZ)))
+					{
+						maxY = blockY;
+						break;
+					}
+			}
+		}
+		
+		Debug.debug("maxY: "+ maxY);
+		return maxY;
+	}
+
+	private class BlockMove
+	{
+		protected Chunk chunkFrom;
+		BlockPos source;
+		BlockPos dest;
+		private IBlockState block = null;
+		private boolean update;
+		private NBTTagCompound extraData;
+		
+		BlockMove(Chunk c, BlockPos src, BlockPos dst, IBlockState b, boolean u)
+		{
+			chunkFrom = c;
+			source = src;
+			dest = dst;
+			block = b;
+			update = u;
+		}
+		
+		BlockMove(Chunk c, BlockPos src, BlockPos dst, IBlockState b, boolean u, NBTTagCompound eD)
+		{
+			chunkFrom = c;
+			source = src;
+			dest = dst;
+			block = b;
+			update = u;
+			extraData = eD;
+		}
+		
+		void copy(Chunk chunkTo)
+		{
+			if(chunkTo.getBlockState(dest).getBlock() == Blocks.BEDROCK)
+			{
+				return;
+			}
+			
+			if(update)
+			{
+				chunkTo.setBlockState(dest, block);
+			} else if(block == Blocks.AIR.getDefaultState())
+			{
+				chunkTo.getWorld().setBlockState(dest, block, 0);
+			} else
+			{
+				copyBlockDirect(chunkFrom, chunkTo, source.getX(), source.getY(), source.getZ(), dest.getX(), dest.getY(), dest.getZ());
+			}
+			
+			TileEntity tileEntity = chunkFrom.getTileEntity(source, EnumCreateEntityType.CHECK);
+			if(tileEntity != null)
+			{
+				NBTTagCompound nbt = new NBTTagCompound();
+				tileEntity.writeToNBT(nbt);
+				nbt.setInteger("x", dest.getX());
+				nbt.setInteger("y", dest.getY());
+				nbt.setInteger("z", dest.getZ());
+				
+				handleExtraData(chunkTo);
+				
+				TileEntity te1 = TileEntity.create(chunkTo.getWorld(), nbt);
+				chunkTo.addTileEntity(dest, te1);
+				
+				if(tileEntity instanceof TileEntityComputer)
+					SkaianetHandler.movingComputer((TileEntityComputer) tileEntity, (TileEntityComputer) te1);
+				
+				te1.markDirty();
+			}
+		}
+		
+		private void handleExtraData(Chunk chunkTo)
+		{
+			if(extraData != null)
+			{
+				switch(extraData.getString("responsibleMod"))
+				{
+				case "refinedstorage":
+					INetworkNode node = RefinedStorageSupport.API.getNetworkNodeRegistry().
+						get(extraData.getString(RefinedStorageSupport.NBT_NODE_ID)).
+						create(extraData.getCompoundTag(RefinedStorageSupport.NBT_NODE), chunkTo.getWorld(), dest);
+					//node.setThrottlingDisabled();		//Might not be necessary due to new instances defaulting to this?
+					
+					INetworkNodeManager manager = RefinedStorageSupport.API.getNetworkNodeManager(chunkTo.getWorld());
+					manager.setNode(dest, node);
+					manager.markForSaving();
+					break;
+				}
+			}
+		}
+		
+		private void copyBlockDirect(Chunk cSrc, Chunk cDst, int xSrc, int ySrc, int zSrc, int xDst, int yDst, int zDst)
+		{
+			ExtendedBlockStorage blockStorageSrc = getBlockStorage(cSrc, ySrc >> 4);
+			ExtendedBlockStorage blockStorageDst = getBlockStorage(cDst, yDst >> 4);
+			xSrc &= 15; ySrc &= 15; zSrc &= 15; xDst &= 15; yDst &= 15; zDst &= 15;
+			
+			blockStorageDst.set(xDst, yDst, zDst, blockStorageSrc.get(xSrc, ySrc, zSrc));
+			blockStorageDst.setBlockLight(xDst, yDst, zDst, blockStorageSrc.getBlockLight(xSrc, ySrc, zSrc));
+			if(blockStorageSrc.getSkyLight() != null)
+				blockStorageDst.setSkyLight(xDst, yDst, zDst, blockStorageSrc.getSkyLight(xSrc, ySrc, zSrc));
+		}
+		
+		private ExtendedBlockStorage getBlockStorage(Chunk c, int y)
+		{
+			ExtendedBlockStorage blockStorage = c.getBlockStorageArray()[y];
+			if(blockStorage == null)
+				blockStorage = c.getBlockStorageArray()[y] = new ExtendedBlockStorage(y << 4, c.getWorld().provider.hasSkyLight());
+			return blockStorage;
+		}
+	}
+}
