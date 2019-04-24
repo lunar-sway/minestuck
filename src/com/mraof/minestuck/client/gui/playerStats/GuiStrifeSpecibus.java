076163eacf10688bec274122f2899ebaa581b7fb
Cibernet <cyberneticGuardian@gmail.com>
2019-04-23 21:07:37 -0500

    got the portfolio layering to work properly

--- src/com/mraof/minestuck/client/gui/playerStats/GuiStrifeSpecibus.java
+++ src/com/mraof/minestuck/client/gui/playerStats/GuiStrifeSpecibus.java
@@ -1,25 +1,19 @@
 package com.mraof.minestuck.client.gui.playerStats;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
-import com.mraof.minestuck.block.MinestuckBlocks;
-import com.mraof.minestuck.inventory.specibus.StrifePortfolioHandler;
+import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.inventory.specibus.StrifeSpecibus;
-import com.mraof.minestuck.item.MinestuckItems;
 import com.mraof.minestuck.util.KindAbstratusType;
 import com.mraof.minestuck.util.MinestuckPlayerData;
 
-import io.netty.util.concurrent.GlobalEventExecutor;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import scala.swing.event.MouseButtonEvent;
 
 public class GuiStrifeSpecibus extends GuiPlayerStats
 {
@@ -37,6 +31,7 @@ public class GuiStrifeSpecibus extends GuiPlayerStats
 	private static ArrayList<StrifeSpecibus> portfolio;
 	private static int blankCardCount = 0;
 	private static int selectedCard = -1;
+	private static final FontRenderer font = Minestuck.fontSpecibus;
 	
 	public GuiStrifeSpecibus()
 	{
@@ -44,7 +39,6 @@ public class GuiStrifeSpecibus extends GuiPlayerStats
 		guiWidth = 226;
 		guiHeight = 188;
 		portfolio = MinestuckPlayerData.getClientPortfolio();
-		System.out.println(portfolio);
 		if(portfolio == null) portfolio = new ArrayList<StrifeSpecibus>();
 	}
 	
@@ -72,19 +66,24 @@ public class GuiStrifeSpecibus extends GuiPlayerStats
 		this.mc.getTextureManager().bindTexture(guiStrifePortfolioBg);
 		this.drawTexturedModalRect(xOffset, yOffset, 0, 0, guiWidth, guiHeight);
 		
+
+		this.mc.getTextureManager().bindTexture(guiPortfolioTabs);
+		this.drawTexturedModalRect(xOffset, yOffset+0, 20, 58, 98, 94);
+		
 		drawCard(11, 9, cardScale, 6);
 		this.mc.getTextureManager().bindTexture(guiPortfolioTabs);
-		//this.drawTexturedModalRect(xOffset+6, yOffset+6, 14, 54, 98, 98);
+		this.drawTexturedModalRect(xOffset+2, yOffset+6, 4, 44, 132, 120);
 		
 		drawCard(59, 7, cardScale, 7);
 		drawCard(12, 50, cardScale, 9);
 		this.mc.getTextureManager().bindTexture(guiPortfolioTabs);
-		this.drawTexturedModalRect(xOffset+28, yOffset+6, 0, 18, 152, 134);
+		this.drawTexturedModalRect(xOffset+21, yOffset+4, 0, 18, 152, 134);
 		
 		drawCard(107, 7, cardScale, 8);
 		drawCard(56, 40, cardScale, 0);
 		this.mc.getTextureManager().bindTexture(guiPortfolioTabs);
 		this.drawTexturedModalRect(xOffset+45, yOffset+10, 0, 0, 164, 152);
+		this.drawTexturedModalRect(xOffset+173, yOffset+6, 124, 0, 7, 4);
 		
 		drawCard(107, 33, cardScale, 5);
 		drawCard(56, 80, cardScale, 4);
@@ -110,11 +109,16 @@ public class GuiStrifeSpecibus extends GuiPlayerStats
 		i = 0;
 		for(StrifeSpecibus specibus : portfolio)
 		{
-			
+			setScale((float)1/16);
+			this.mc.getTextureManager().bindTexture(new ResourceLocation("minestuck", iconsLoc+specibus.getAbstratus().getUnlocalizedName()+".png"));
+			this.drawTexturedModalRect((xOffset+23+(20*i))/scale,(yOffset+166)/scale,0,0,256,256);
 			i++;
 		}
+		setScale(1);
 		
-		System.out.println("a");
+		drawCard(110, -10, 1, 0);
+		
+		//System.out.println("a");
 	}
 	
 	public void drawCard(int cardX, int cardY, float cardScale, int index)
@@ -134,6 +138,8 @@ public class GuiStrifeSpecibus extends GuiPlayerStats
 		String unlocalizedName = abstratus.getUnlocalizedName();
 		String displayName = abstratus.getDisplayName();
 		int txOffset = mc.fontRenderer.getStringWidth(displayName);
+		List<ItemStack> items = specibus.getItems();
+		int totalItems = items.size();
 		
 		int x = (int) ((xOffset+cardX));
 		int y = (int) ((yOffset+cardY));
@@ -143,11 +149,33 @@ public class GuiStrifeSpecibus extends GuiPlayerStats
 		setScale(cardScale);
 		this.mc.getTextureManager().bindTexture(guiStrifeCard);
 		this.drawTexturedModalRect(x/scale, y/scale, 28, 0, 200, 256);
-		setScale(cardScale/4);
+		setScale(cardScale/2.5f);
 		this.mc.getTextureManager().bindTexture(icon);
-		this.drawTexturedModalRect((x+60)/scale, (y+52)/scale, 0, 0, 256, 256);
+		this.drawTexturedModalRect(x/scale+57, y/scale+148, 0, 0, 256, 256);
 		
+		setScale(cardScale*2);
+		font.drawString("strife specibus", x/scale+5, y/scale+4, 0xFF00E371, false);
+		setScale(cardScale*2.5f);
+		mc.fontRenderer.drawString(displayName, x/scale+52-font.getStringWidth(displayName), y/scale+91, 0xFF00E371, false);
+		setScale(cardScale);
+		font.drawString("sylladex :: strife deck", x/scale+16, y/scale+179, 0xFFFFFFFF, false);
+		
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		
+		for(ItemStack i : items)
+		{
+			int ixoff = 20;
+			int iyoff = 10;
+			setScale(cardScale);
+			mc.getTextureManager().bindTexture(icons);
+			drawTexturedModalRect(x/scale + ixoff, y/scale + iyoff, 0, 122, 21, 26);
+			drawItemStack(i, (int)(x/scale) + ixoff+2, (int)(y/scale) + iyoff+4, i.getDisplayName());
+		}
+		
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
 		setScale(1);
+		
+		
 		
 	}
 	
