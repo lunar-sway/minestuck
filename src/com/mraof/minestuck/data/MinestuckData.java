52762325a243457cf3f5ec922a30f6aa4342ba8e
kirderf1 <kirderf4@gmail.com>
2019-08-26 20:40:40 +0200

    Added block tag data provider and generated new block tags
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

---
+++ src/com/mraof/minestuck/data/MinestuckData.java
@@ -0,0 +1,23 @@
+package com.mraof.minestuck.data;
+
+import com.mraof.minestuck.Minestuck;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@SuppressWarnings("unused")
+@Mod.EventBusSubscriber(modid = Minestuck.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MinestuckData
+{
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event)
+	{
+		DataGenerator gen = event.getGenerator();
+		
+		if(event.includeServer())
+		{
+			gen.addProvider(new MinestuckBlockTagsProvider(gen));
+		}
+	}
+}
