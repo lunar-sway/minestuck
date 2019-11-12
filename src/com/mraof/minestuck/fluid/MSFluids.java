d06812b551bcfe8dcbf1e51fe501979599e8ca44
kirderf1 <kirderf4@gmail.com>
2019-11-12 16:49:55 +0100

    Fixed texture and model-related errors
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/com/mraof/minestuck/fluid/MSFluids.java
+++ src/com/mraof/minestuck/fluid/MSFluids.java
@@ -12,7 +12,7 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class MSFluids
+public final class MSFluids
 {
 	public static final ResourceLocation STILL_OIL_TEXTURE = new ResourceLocation(Minestuck.MOD_ID, "block/still_oil");
 	public static final ResourceLocation FLOWING_OIL_TEXTURE = new ResourceLocation(Minestuck.MOD_ID, "block/flowing_oil");
@@ -29,18 +29,18 @@ public class MSFluids
 	
 	public static final DeferredRegister<Fluid> FLUIDS = new DeferredRegister<>(ForgeRegistries.FLUIDS, Minestuck.MOD_ID);
 	
-	public static RegistryObject<FlowingFluid> OIL = FLUIDS.register("oil", () -> new ForgeFlowingFluid.Source(MSFluids.OIL_PROPERTIES));
-	public static RegistryObject<FlowingFluid> FLOWING_OIL = FLUIDS.register("flowing_oil", () -> new ForgeFlowingFluid.Flowing(MSFluids.OIL_PROPERTIES));
-	public static RegistryObject<FlowingFluid> LIGHT_WATER = FLUIDS.register("light_water", () -> new ForgeFlowingFluid.Source(MSFluids.LIGHT_WATER_PROPERTIES));
-	public static RegistryObject<FlowingFluid> FLOWING_LIGHT_WATER = FLUIDS.register("flowing_light_water", () -> new ForgeFlowingFluid.Flowing(MSFluids.LIGHT_WATER_PROPERTIES));
-	public static RegistryObject<FlowingFluid> BLOOD = FLUIDS.register("blood", () -> new ForgeFlowingFluid.Source(MSFluids.BLOOD_PROPERTIES));
-	public static RegistryObject<FlowingFluid> FLOWING_BLOOD = FLUIDS.register("flowing_blood", () -> new ForgeFlowingFluid.Flowing(MSFluids.BLOOD_PROPERTIES));
-	public static RegistryObject<FlowingFluid> BRAIN_JUICE = FLUIDS.register("brain_juice", () -> new ForgeFlowingFluid.Source(MSFluids.BRAIN_JUICE_PROPERTIES));
-	public static RegistryObject<FlowingFluid> FLOWING_BRAIN_JUICE = FLUIDS.register("flowing_brain_juice", () -> new ForgeFlowingFluid.Flowing(MSFluids.BRAIN_JUICE_PROPERTIES));
-	public static RegistryObject<FlowingFluid> WATER_COLORS = FLUIDS.register("water_colors", () -> new ForgeFlowingFluid.Source(MSFluids.WATER_COLORS_PROPERTIES));
-	public static RegistryObject<FlowingFluid> FLOWING_WATER_COLORS = FLUIDS.register("flowing_water_colors", () -> new ForgeFlowingFluid.Flowing(MSFluids.WATER_COLORS_PROPERTIES));
-	public static RegistryObject<FlowingFluid> ENDER = FLUIDS.register("ender", () -> new ForgeFlowingFluid.Source(MSFluids.ENDER_PROPERTIES));
-	public static RegistryObject<FlowingFluid> FLOWING_ENDER = FLUIDS.register("flowing_ender", () -> new ForgeFlowingFluid.Flowing(MSFluids.ENDER_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> OIL = FLUIDS.register("oil", () -> new ForgeFlowingFluid.Source(MSFluids.OIL_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> FLOWING_OIL = FLUIDS.register("flowing_oil", () -> new ForgeFlowingFluid.Flowing(MSFluids.OIL_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> LIGHT_WATER = FLUIDS.register("light_water", () -> new ForgeFlowingFluid.Source(MSFluids.LIGHT_WATER_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> FLOWING_LIGHT_WATER = FLUIDS.register("flowing_light_water", () -> new ForgeFlowingFluid.Flowing(MSFluids.LIGHT_WATER_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> BLOOD = FLUIDS.register("blood", () -> new ForgeFlowingFluid.Source(MSFluids.BLOOD_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> FLOWING_BLOOD = FLUIDS.register("flowing_blood", () -> new ForgeFlowingFluid.Flowing(MSFluids.BLOOD_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> BRAIN_JUICE = FLUIDS.register("brain_juice", () -> new ForgeFlowingFluid.Source(MSFluids.BRAIN_JUICE_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> FLOWING_BRAIN_JUICE = FLUIDS.register("flowing_brain_juice", () -> new ForgeFlowingFluid.Flowing(MSFluids.BRAIN_JUICE_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> WATER_COLORS = FLUIDS.register("water_colors", () -> new ForgeFlowingFluid.Source(MSFluids.WATER_COLORS_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> FLOWING_WATER_COLORS = FLUIDS.register("flowing_water_colors", () -> new ForgeFlowingFluid.Flowing(MSFluids.WATER_COLORS_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> ENDER = FLUIDS.register("ender", () -> new ForgeFlowingFluid.Source(MSFluids.ENDER_PROPERTIES));
+	public static final RegistryObject<FlowingFluid> FLOWING_ENDER = FLUIDS.register("flowing_ender", () -> new ForgeFlowingFluid.Flowing(MSFluids.ENDER_PROPERTIES));
 	
 	public static final ForgeFlowingFluid.Properties OIL_PROPERTIES = new ForgeFlowingFluid.Properties(OIL, FLOWING_OIL, FluidAttributes.builder(STILL_OIL_TEXTURE, FLOWING_OIL_TEXTURE)).bucket(() -> MSItems.OIL_BUCKET).block(() -> MSBlocks.OIL).slopeFindDistance(3).explosionResistance(100F);
 	public static final ForgeFlowingFluid.Properties LIGHT_WATER_PROPERTIES = new ForgeFlowingFluid.Properties(LIGHT_WATER, FLOWING_LIGHT_WATER, FluidAttributes.builder(STILL_LIGHT_WATER_TEXTURE, FLOWING_LIGHT_WATER_TEXTURE)).canMultiply().bucket(() -> MSItems.LIGHT_WATER_BUCKET).block(() -> MSBlocks.LIGHT_WATER).explosionResistance(100F);
