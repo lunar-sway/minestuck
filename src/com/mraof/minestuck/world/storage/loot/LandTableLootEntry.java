c56ee3c269180214b311aeccd3b2a81f3de8f931
kirderf1 <kirderf4@gmail.com>
2020-01-05 17:20:04 +0100

    If the accesstransformer isn't working, use reflection!
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/com/mraof/minestuck/world/storage/loot/LandTableLootEntry.java
+++ src/com/mraof/minestuck/world/storage/loot/LandTableLootEntry.java
@@ -6,13 +6,17 @@ import com.google.gson.JsonSerializationContext;
 import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.world.MSDimensions;
 import com.mraof.minestuck.world.lands.LandTypePair;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.*;
 import net.minecraft.world.storage.loot.conditions.ILootCondition;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.lang.reflect.Field;
+import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
@@ -38,8 +42,8 @@ public class LandTableLootEntry extends LootEntry
 		LandTypePair aspects = MSDimensions.getAspects(context.getWorld().getServer(), context.getWorld().dimension.getType());
 		if(test(context) && aspects != null)
 		{
-			ResourceLocation terrainTableName = new ResourceLocation(table.getNamespace(), table.getPath() + "/" + Objects.requireNonNull(aspects.terrain.getRegistryName()).toString().replace(':', '/'));
-			ResourceLocation titleTableName = new ResourceLocation(table.getNamespace(), table.getPath() + "/" + Objects.requireNonNull(aspects.title.getRegistryName()).toString().replace(':', '/'));
+			ResourceLocation terrainTableName = new ResourceLocation(table.getNamespace(), table.getPath() + "/terrain/" + Objects.requireNonNull(aspects.terrain.getRegistryName()).toString().replace(':', '/'));
+			ResourceLocation titleTableName = new ResourceLocation(table.getNamespace(), table.getPath() + "/title/" + Objects.requireNonNull(aspects.title.getRegistryName()).toString().replace(':', '/'));
 			
 			expandFrom(terrainTableName, context, lootGenCollector);
 			expandFrom(titleTableName, context, lootGenCollector);
@@ -53,20 +57,75 @@ public class LandTableLootEntry extends LootEntry
 	private void expandFrom(ResourceLocation tableName, LootContext context, Consumer<ILootGenerator> lootGenCollector)
 	{
 		LootTable lootTable = context.getLootTableManager().getLootTableFromLocation(tableName);
+		if(lootTable == null)
+			return;
 		if(context.addLootTable(lootTable))
 		{
 			LootPool pool = lootTable.getPool(poolName);
 			if(pool != null)
 			{
-				/*for(LootEntry entry : pool.lootEntries) TODO fix accesstransformer stuff or use different approach
-					entry.expand(context, lootGenCollector); The alternative is to generate from the loot pool directly, however that'd mean that we're not taking into account the weight of table entries related to the weight of other loot tables*/
+				List<LootEntry> entries = accessWithReflection(pool);
+				if(entries != null)
+				{
+					for(LootEntry entry : entries)
+						entry.expand(context, lootGenCollector);
+				} else
+				{
+					lootGenCollector.accept(new ILootGenerator()
+					{
+						@Override
+						public int getEffectiveWeight(float v)
+						{
+							return 30;
+						}
+						
+						@Override
+						public void func_216188_a(Consumer<ItemStack> consumer, LootContext lootContext)
+						{
+							pool.generate(consumer, lootContext);
+						}
+					});
+				}
 			} else
 			{
 				LOGGER.warn("Could not find pool by name {} in loot table {}", poolName, tableName);
 			}
+			context.removeLootTable(lootTable);
 		} else
 		{
 			LOGGER.warn("Detected infinite loop in loot tables");
+		}
+	}
+	
+	private static final Field lootEntries = getLootEntryField();
+	
+	private static Field getLootEntryField()
+	{
+		try
+		{
+			return ObfuscationReflectionHelper.findField(LootPool.class, "field_186453_a");
+		} catch(ObfuscationReflectionHelper.UnableToFindFieldException e)
+		{
+			LOGGER.error("Unable to get field for lootPool.lootEntries. Will be unable to fully insert loot from land type loot tables.", e);
+			return null;
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<LootEntry> accessWithReflection(LootPool pool)
+	{
+		if(lootEntries == null)
+			return null;
+		else
+		{
+			try
+			{
+				return (List<LootEntry>) lootEntries.get(pool);
+			} catch(Exception e)
+			{
+				LOGGER.error("Got exception when accessing loot entries field for loot pool. Will use simpler behaviour for this time.", e);
+				return null;
+			}
 		}
 	}
 	
@@ -112,6 +171,7 @@ public class LandTableLootEntry extends LootEntry
 			this.table = table;
 		}
 		
+		@Override
 		protected BuilderImpl func_212845_d_()
 		{
 			return this;
@@ -123,6 +183,7 @@ public class LandTableLootEntry extends LootEntry
 			return this;
 		}
 		
+		@Override
 		public LootEntry build()
 		{
 			if(pool == null)
