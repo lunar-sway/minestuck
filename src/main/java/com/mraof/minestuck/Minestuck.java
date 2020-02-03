2bc6d74cc9f36193d9479cfb69e2e6b9ad657ac1
kirderf1 <kirderf4@gmail.com>
2020-02-03 18:24:26 +0100

    Delete CommonProxy and put its code in Minestuck
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/Minestuck.java
+++ src/main/java/com/mraof/minestuck/Minestuck.java
@@ -1,8 +1,24 @@
 package com.mraof.minestuck;
 
+import com.mraof.minestuck.advancements.MSCriteriaTriggers;
 import com.mraof.minestuck.client.ClientProxy;
+import com.mraof.minestuck.computer.ComputerProgram;
+import com.mraof.minestuck.computer.SburbClient;
+import com.mraof.minestuck.computer.SburbServer;
+import com.mraof.minestuck.computer.editmode.DeployList;
+import com.mraof.minestuck.entity.MSEntityTypes;
+import com.mraof.minestuck.entity.consort.ConsortDialogue;
+import com.mraof.minestuck.entity.consort.ConsortRewardHandler;
 import com.mraof.minestuck.fluid.MSFluids;
+import com.mraof.minestuck.item.MSItems;
+import com.mraof.minestuck.item.crafting.alchemy.AlchemyRecipes;
+import com.mraof.minestuck.network.MSPacketHandler;
+import com.mraof.minestuck.player.KindAbstratusList;
+import com.mraof.minestuck.skaianet.SessionHandler;
+import com.mraof.minestuck.world.biome.MSBiomes;
 import com.mraof.minestuck.world.gen.MSSurfaceBuilders;
+import com.mraof.minestuck.world.gen.feature.MSFillerBlockTypes;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.WorldPersistenceHooks;
@@ -13,6 +29,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import static com.mraof.minestuck.Minestuck.MOD_ID;
+import static com.mraof.minestuck.world.gen.OreGeneration.setupOverworldOreGeneration;
 
 @Mod(MOD_ID)
 public class Minestuck
@@ -39,7 +56,34 @@ public class Minestuck
 	
 	private void setup(final FMLCommonSetupEvent event)
 	{
-		CommonProxy.init();
+		MSCriteriaTriggers.register();
+		MSEntityTypes.registerPlacements();
+		MSFillerBlockTypes.init();
+		
+		//register ore generation
+		setupOverworldOreGeneration();
+		//GameRegistry.registerWorldGenerator(oreHandler, 0);
+		
+		//register channel handler
+		MSPacketHandler.setupChannel();
+		
+		//register grist costs and combination recipes
+		AlchemyRecipes.registerVanillaRecipes();
+		AlchemyRecipes.registerMinestuckRecipes();
+		
+		//register consort shop prices
+		ConsortRewardHandler.registerMinestuckPrices();
+		
+		MSBiomes.init();
+		ConsortDialogue.init();
+		
+		KindAbstratusList.registerTypes();
+		DeployList.registerItems();
+		
+		ComputerProgram.registerProgram(0, SburbClient.class, new ItemStack(MSItems.CLIENT_DISK));	//This idea was kind of bad and should be replaced
+		ComputerProgram.registerProgram(1, SburbServer.class, new ItemStack(MSItems.SERVER_DISK));
+		
+		SessionHandler.maxSize = 144;//acceptTitleCollision?(generateSpecialClasses?168:144):12;
 	}
 	
 	private void clientSetup(final FMLClientSetupEvent event)
