db951cd87d49c9f453aae58727bc48f19205ebe0
Nhezak <nhezak@users.noreply.github.com>
2020-04-09 18:53:58 -0300

    I made a wooden carrot, woohoo.

--- src/main/java/com/mraof/minestuck/data/MinestuckEnUsLanguageProvider.java
+++ src/main/java/com/mraof/minestuck/data/MinestuckEnUsLanguageProvider.java
@@ -480,6 +480,9 @@ public class MinestuckEnUsLanguageProvider extends MinestuckLanguageProvider
 		addTooltip(MSItems.ROCK_COOKIE, "It's as edible as a rock");
 		addStore(MSItems.ROCK_COOKIE, "Grey-Colored Cookie");
 		addStoreTooltip(MSItems.ROCK_COOKIE, "This here is a true wonder-cookie! Totally! And they are even cheaper than regular cookies!");
+		add(MSItems.WOODEN_CARROT, "Wooden Carrot");
+		addTooltip(MSItems.WOODEN_CARROT, "It's as edible as a plank.");
+		addStoreTooltip(MSItems.WOODEN_CARROT, "You're not sure if this thing is edible but you sure as fuck don't want to try...... probably...");
 		add(MSItems.FUNGAL_SPORE, "Fungal Spore");
 		addTooltip(MSItems.FUNGAL_SPORE, "You must live off the land, and in this land, it means eating this.");
 		addStore(MSItems.FUNGAL_SPORE, "Mushroom Skittles");
