29f719b95effe256b10fb7f7814a21cf558519df
Padamariuse <noterrik@gmail.com>
2020-04-14 13:15:55 -0400

    reworked BreathFloatWeaponItem into AspectBasedEffectWeaponItem

--- src/main/java/com/mraof/minestuck/item/weapon/AspectBasedEffectWeaponItem.java
+++ src/main/java/com/mraof/minestuck/item/weapon/AspectBasedEffectWeaponItem.java
@@ -1,11 +1,13 @@
 package com.mraof.minestuck.item.weapon;
 
+import com.mraof.minestuck.player.EnumAspect;
 import com.mraof.minestuck.world.storage.PlayerSavedData;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.world.World;
@@ -14,11 +16,20 @@ import javax.annotation.Nullable;
 
 import static com.mraof.minestuck.player.EnumAspect.BREATH;
 
-public class BreathFloatWeaponItem extends WeaponItem
+public class AspectBasedEffectWeaponItem extends WeaponItem
 {
-    public BreathFloatWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, @Nullable MSToolType toolType, Properties builder)
+    private EnumAspect aspect;
+    private Effect effect;
+    private int duration;
+    private int effectTier;
+    
+    public AspectBasedEffectWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, EnumAspect aspect, Effect effect, int duration, int effectTier, @Nullable MSToolType toolType, Properties builder)
     {
         super(tier, attackDamageIn, attackSpeedIn, efficiency, toolType, builder);
+        this.aspect = aspect;
+        this.effect = effect;
+        this.duration = duration;
+        this.effectTier = effectTier;
     }
     
     @Override
@@ -26,9 +37,9 @@ public class BreathFloatWeaponItem extends WeaponItem
     {
         if(entityIn instanceof ServerPlayerEntity)
         {
-            if (PlayerSavedData.getData((ServerPlayerEntity) entityIn).getTitle().getHeroAspect() == BREATH && isSelected)
+            if (PlayerSavedData.getData((ServerPlayerEntity) entityIn).getTitle().getHeroAspect() == aspect && isSelected)
             {
-                ((ServerPlayerEntity) entityIn).addPotionEffect(new EffectInstance(Effects.SLOW_FALLING, 20, 2));
+                ((ServerPlayerEntity) entityIn).addPotionEffect(new EffectInstance(effect, duration, effectTier));
             }
         }
     }
