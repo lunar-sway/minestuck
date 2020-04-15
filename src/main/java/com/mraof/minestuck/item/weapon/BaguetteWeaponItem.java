84fc4e4e8b897ad8148dcddce8be449cdf268e3e
Padamariuse <noterrik@gmail.com>
2020-04-15 09:14:27 -0400

    made baguettes edible

--- src/main/java/com/mraof/minestuck/item/weapon/BaguetteWeaponItem.java
+++ src/main/java/com/mraof/minestuck/item/weapon/BaguetteWeaponItem.java
@@ -15,18 +15,20 @@ import net.minecraft.world.World;
 
 import java.util.Random;
 
-public class BaguetteWeaponItem extends WeaponItem
+public class BaguetteWeaponItem extends ConsumableWeaponItem
 {
-    public BaguetteWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, MSToolType toolType, Properties builder)
+    
+    public BaguetteWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, int healAmount, float saturationModifier, MSToolType toolType, Properties builder)
     {
-        super(tier, attackDamageIn, attackSpeedIn, efficiency, toolType, builder);
+        super(tier, attackDamageIn, attackSpeedIn, efficiency, healAmount, saturationModifier, toolType, builder);
     }
     
     @Override
     public boolean hitEntity(ItemStack itemStack, LivingEntity target, LivingEntity attacker)
     {
         ItemStack crumbs = new ItemStack(MSItems.BREADCRUMBS, 1);
-        if(!target.world.isRemote) {
+        if(!target.world.isRemote)
+        {
             ItemEntity item = new ItemEntity(target.world, target.posX, target.posY, target.posZ, crumbs);
             target.world.addEntity(item);
         }
@@ -34,11 +36,14 @@ public class BaguetteWeaponItem extends WeaponItem
     }
     
     @Override
-    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
-        Random rand = new Random();
-        int num = rand.nextInt(10);
-        ItemStack crumbs = new ItemStack(MSItems.BREADCRUMBS, num);
-        if(entityLiving instanceof PlayerEntity && !entityLiving.world.isRemote) {
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving)
+    {
+        if(entityLiving instanceof PlayerEntity && !entityLiving.world.isRemote)
+        {
+            Random rand = new Random();
+            int num = rand.nextInt(10);
+            ItemStack crumbs = new ItemStack(MSItems.BREADCRUMBS, num);
+            
             PlayerEntity entityplayer = (PlayerEntity)entityLiving;
             entityplayer.addItemStackToInventory(crumbs);
         }
