8bd91afb771c47b20b324f8006126564fb41ecc7
kirderf1 <kirderf4@gmail.com>
2020-07-03 12:03:06 +0200

    Fix horn sound to properly execute server-side
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/item/SoundItem.java
+++ src/main/java/com/mraof/minestuck/item/SoundItem.java
@@ -1,10 +1,8 @@
 package com.mraof.minestuck.item;
 
-import com.mraof.minestuck.util.MSSoundEvents;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUseContext;
 import net.minecraft.util.*;
 import net.minecraft.world.World;
 
@@ -23,7 +21,8 @@ public class SoundItem extends Item
     @Override
     public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity player, Hand handIn)
     {
-        worldIn.playSound(player.posX, player.posY, player.posZ, sound.get(), SoundCategory.PLAYERS, 1.0F, 1.0F, true);
-        return super.onItemRightClick(worldIn, player, handIn);
+        worldIn.playSound(player, player.posX, player.posY, player.posZ, sound.get(), SoundCategory.PLAYERS, 1.0F, 1.0F);
+    
+        return new ActionResult<>(ActionResultType.SUCCESS, player.getHeldItem(handIn));
     }
 }
