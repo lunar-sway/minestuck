b13aff8922ebdf854677d25c3b2cb6647c0bbf02
bconlon <aidan90803@icloud.com>
2020-07-30 17:42:57 -0700

    Drinkable items are now drinkable.

--- src/main/java/com/mraof/minestuck/item/foods/DrinkableItem.java
+++ src/main/java/com/mraof/minestuck/item/foods/DrinkableItem.java
@@ -1,8 +1,20 @@
 package com.mraof.minestuck.item.foods;
 
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.item.UseAction;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.PotionUtils;
+import net.minecraft.stats.Stats;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
+import net.minecraft.world.World;
 
 public class DrinkableItem extends Item {
     public DrinkableItem(Properties properties){
@@ -10,7 +22,33 @@ public class DrinkableItem extends Item {
     }
 
     @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) {
+        PlayerEntity playerentity = entityLiving instanceof PlayerEntity ? (PlayerEntity)entityLiving : null;
+
+        if (playerentity instanceof ServerPlayerEntity) {
+            CriteriaTriggers.CONSUME_ITEM.trigger((ServerPlayerEntity)playerentity, stack);
+        }
+
+        if (playerentity != null) {
+            playerentity.addStat(Stats.ITEM_USED.get(this));
+        }
+
+        return entityLiving.onFoodEaten(worldIn, stack);
+    }
+
+    @Override
+    public int getUseDuration(ItemStack stack) {
+        return 32;
+    }
+
+    @Override
     public UseAction getUseAction(ItemStack stack){
         return UseAction.DRINK;
+    }
+
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn) {
+        playerIn.setActiveHand(handIn);
+        return new ActionResult<>(ActionResultType.SUCCESS, playerIn.getHeldItem(handIn));
     }
 }
