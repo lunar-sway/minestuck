8e5ab2ec69e5902cc9f591d3165820d5979f7bee
bconlon <aidan90803@icloud.com>
2020-08-01 09:48:40 -0700

    Begun porting basilisk model.

--- src/main/java/com/mraof/minestuck/client/model/BasiliskModel.java
+++ src/main/java/com/mraof/minestuck/client/model/BasiliskModel.java
@@ -1,53 +1,54 @@
 package com.mraof.minestuck.client.model;
 
+import com.google.common.collect.ImmutableList;
 import com.mraof.minestuck.entity.underling.BasiliskEntity;
 import net.minecraft.client.renderer.entity.model.EntityModel;
-import net.minecraft.client.renderer.entity.model.RendererModel;
+import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.math.MathHelper;
 
 public class BasiliskModel<T extends BasiliskEntity> extends EntityModel<T>
 {
-	private RendererModel head;
-	private RendererModel body;
-	private RendererModel leftFrontLeg;
-	private RendererModel rightFrontLeg;
-	private RendererModel leftBackLeg;
-	private RendererModel rightBackLeg;
-	private RendererModel tail0;
-	private RendererModel tail1;
-	private RendererModel tail2;
+	private ModelRenderer head;
+	private ModelRenderer body;
+	private ModelRenderer leftFrontLeg;
+	private ModelRenderer rightFrontLeg;
+	private ModelRenderer leftBackLeg;
+	private ModelRenderer rightBackLeg;
+	private ModelRenderer tail0;
+	private ModelRenderer tail1;
+	private ModelRenderer tail2;
 	public BasiliskModel()
 	{
 		this.textureWidth = 128;
 		this.textureHeight = 128;
 		float offsetY = 24;
 		//setTextureOffset("null.Tongue", 84, 0);
-		head = new RendererModel(this, 0, 0);
+		head = new ModelRenderer(this, 0, 0);
 		head.addBox(-15F, -16F, -6F, 30, 16, 12);
 		head.func_217178_a("Tongue", -3F, -5F, -8F, 6, 12, 2, 0.0f, 84, 0);
 		head.setRotationPoint(0F, -16 + offsetY, -24F);
-		body = new RendererModel(this, 0, 28);
+		body = new ModelRenderer(this, 0, 28);
 		body.addBox(-7, 0, -20, 14, 14, 40);
 		body.setRotationPoint(0F, -24 + offsetY, 0F);
-		leftFrontLeg = new RendererModel(this, 100, 0);
+		leftFrontLeg = new ModelRenderer(this, 100, 0);
 		leftFrontLeg.addBox(-2, 0, -2, 4, 14, 4);
 		leftFrontLeg.setRotationPoint(-9F, -14 + offsetY, -14);
-		rightFrontLeg = new RendererModel(this, 100, 0);
+		rightFrontLeg = new ModelRenderer(this, 100, 0);
 		rightFrontLeg.addBox(-2, 0, -2, 4, 14, 4);
 		rightFrontLeg.setRotationPoint(9F, -14 + offsetY, -14);
-		leftBackLeg = new RendererModel(this, 100, 0);
+		leftBackLeg = new ModelRenderer(this, 100, 0);
 		leftBackLeg.addBox(-2, 0, -2, 4, 14, 4);
 		leftBackLeg.setRotationPoint(-9F, -14 + offsetY, 14);
-		rightBackLeg = new RendererModel(this, 100, 0);
+		rightBackLeg = new ModelRenderer(this, 100, 0);
 		rightBackLeg.addBox(-2, 0, -2, 4, 14, 4);
 		rightBackLeg.setRotationPoint(9F, -14 + offsetY, 14);
-		tail0 = new RendererModel(this, 0, 82);
+		tail0 = new ModelRenderer(this, 0, 82);
 		tail0.addBox(-5, 0, -2, 10, 10, 20);
 		tail0.setRotationPoint(0F, -22 + offsetY, 18F);
-		tail1 = new RendererModel(this, 60, 82);
+		tail1 = new ModelRenderer(this, 60, 82);
 		tail1.addBox(-3, 0, -2, 6, 6, 20);
 		tail1.setRotationPoint(0F, -18 + offsetY, 36F);
-		tail2 = new RendererModel(this, 68, 28);
+		tail2 = new ModelRenderer(this, 68, 28);
 		tail2.addBox(-3, 0, -2, 4, 4, 20);
 		tail2.setRotationPoint(0F, -15 + offsetY, 54F);
 	}
