149aa08d32211fcba04eadf5c47068c94a9d0cd4
bconlon <aidan90803@icloud.com>
2020-08-01 11:28:10 -0700

    World gen changes, probably will break something.

--- src/main/java/com/mraof/minestuck/world/biome/gen/LandBiomeProviderSettings.java
+++ src/main/java/com/mraof/minestuck/world/biome/gen/LandBiomeProviderSettings.java
@@ -1,11 +1,13 @@
 package com.mraof.minestuck.world.biome.gen;
 
 import com.mraof.minestuck.world.gen.LandGenSettings;
+import net.minecraft.world.WorldType;
 import net.minecraft.world.biome.provider.IBiomeProviderSettings;
 
 public class LandBiomeProviderSettings implements IBiomeProviderSettings
 {
 	private long seed;
+	private WorldType worldType;
 	private LandGenSettings genSettings;
 	
 	public long getSeed()
@@ -23,10 +25,15 @@ public class LandBiomeProviderSettings implements IBiomeProviderSettings
 	{
 		return genSettings;
 	}
+
+	public WorldType getWorldType() {
+		return this.worldType;
+	}
 	
 	public LandBiomeProviderSettings setGenSettings(LandGenSettings genSettings)
 	{
 		this.genSettings = genSettings;
+		this.worldType = WorldType.DEFAULT;
 		return this;
 	}
 }
