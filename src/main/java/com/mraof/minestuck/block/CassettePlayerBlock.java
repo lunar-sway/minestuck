7f2a2b414c0f79e12fea429c6a07446c9c70e50c
LunaticCat <yannbrun1507@outlook.fr>
2020-08-16 07:55:02 +0200

    reformating CassettePlayerBlock code

--- src/main/java/com/mraof/minestuck/block/CassettePlayerBlock.java
+++ src/main/java/com/mraof/minestuck/block/CassettePlayerBlock.java
@@ -24,105 +24,129 @@ import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 
-public class CassettePlayerBlock extends DecorBlock {
-    public static final BooleanProperty HAS_CASSETTE = ModBlockStateProperties.HAS_CASSETTE;
-    public static final BooleanProperty IS_OPEN = ModBlockStateProperties.IS_OPEN;
-    private final Supplier<TileEntityType<?>> tileType;
-
-    public CassettePlayerBlock(Properties properties, CustomVoxelShape shape, Supplier<TileEntityType<?>> tileType) {
-        super(properties, shape);
-        this.setDefaultState(this.stateContainer.getBaseState().with(HAS_CASSETTE, false));
-        this.tileType = tileType;
-    }
-
-    @Override
-    @SuppressWarnings("deprecation")
-    public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit) {
-        if (player.isSneaking()) {
-            state = state.cycle(IS_OPEN);
-            worldIn.setBlockState(pos, state, 2);
-            return true;
-        } else if (state.get(HAS_CASSETTE) && state.get(IS_OPEN)) {
-            this.dropCassette(worldIn, pos);
-            state = state.with(HAS_CASSETTE, false);
-            worldIn.setBlockState(pos, state, 2);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public void insertCassette(IWorld worldIn, BlockPos pos, BlockState state, ItemStack cassetteStack) {
-        TileEntity tileentity = worldIn.getTileEntity(pos);
-        if (tileentity instanceof CassettePlayerTileEntity && state.get(IS_OPEN)) {
-            ((CassettePlayerTileEntity) tileentity).setCassette(cassetteStack.copy());
-            worldIn.setBlockState(pos, state.with(HAS_CASSETTE, true), 2);
-        }
-    }
-
-    private void dropCassette(World worldIn, BlockPos pos) {
-        if (!worldIn.isRemote) {
-            TileEntity tileentity = worldIn.getTileEntity(pos);
-            if (tileentity instanceof CassettePlayerTileEntity) {
-                CassettePlayerTileEntity cassetteplayertileentity = (CassettePlayerTileEntity)tileentity;
-                ItemStack itemstack = cassetteplayertileentity.getCassette();
-                if (!itemstack.isEmpty()) {
-                    worldIn.playEvent(1010, pos, 0);
-                    cassetteplayertileentity.clear();
-                    float f = 0.7F;
-                    double d0 = (double)(worldIn.rand.nextFloat() * 0.7F) + (double)0.15F;
-                    double d1 = (double)(worldIn.rand.nextFloat() * 0.7F) + (double)0.060000002F + 0.6D;
-                    double d2 = (double)(worldIn.rand.nextFloat() * 0.7F) + (double)0.15F;
-                    ItemStack itemstack1 = itemstack.copy();
-                    ItemEntity itementity = new ItemEntity(worldIn, (double)pos.getX() + d0, (double)pos.getY() + d1, (double)pos.getZ() + d2, itemstack1);
-                    itementity.setDefaultPickupDelay();
-                    worldIn.addEntity(itementity);
-                }
-            }
-        }
-    }
-
-    public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-        if (state.getBlock() != newState.getBlock()) {
-            this.dropCassette(worldIn, pos);
-            super.onReplaced(state, worldIn, pos, newState, isMoving);
-        }
-    }
-
-    @Override
-    public boolean hasTileEntity(BlockState state)
-    {
-        return true;
-    }
-
-    @Nullable
-    @Override
-    public TileEntity createTileEntity(BlockState state, IBlockReader world)
-    {
-        return tileType.get().create();
-    }
-
-    public boolean hasComparatorInputOverride(BlockState state) {
-        return true;
-    }
-
-    public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos) {
-        TileEntity tileentity = worldIn.getTileEntity(pos);
-        if (tileentity instanceof CassettePlayerTileEntity) {
-            Item item = ((CassettePlayerTileEntity)tileentity).getCassette().getItem();
-            if (item instanceof CassetteItem) {
-                return ((CassetteItem)item).getComparatorValue();
-            }
-        }
-
-        return 0;
-    }
-
-    @Override
-    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
-    {
-        builder.add(FACING);
-        builder.add(HAS_CASSETTE);
-        builder.add(IS_OPEN);
-    }
+public class CassettePlayerBlock extends DecorBlock
+{
+	public static final BooleanProperty HAS_CASSETTE = ModBlockStateProperties.HAS_CASSETTE;
+	public static final BooleanProperty IS_OPEN = ModBlockStateProperties.IS_OPEN;
+	private final Supplier<TileEntityType<?>> tileType;
+	
+	public CassettePlayerBlock(Properties properties, CustomVoxelShape shape, Supplier<TileEntityType<?>> tileType)
+	{
+		super(properties, shape);
+		this.setDefaultState(this.stateContainer.getBaseState().with(HAS_CASSETTE, false));
+		this.tileType = tileType;
+	}
+	
+	@Override
+	@SuppressWarnings("deprecation")
+	public boolean onBlockActivated(BlockState state, World worldIn, BlockPos pos, PlayerEntity player, Hand handIn, BlockRayTraceResult hit)
+	{
+		if(player.isSneaking())
+		{
+			state = state.cycle(IS_OPEN);
+			worldIn.setBlockState(pos, state, 2);
+			return true;
+		} else if(state.get(HAS_CASSETTE) && state.get(IS_OPEN))
+		{
+			this.dropCassette(worldIn, pos);
+			state = state.with(HAS_CASSETTE, false);
+			worldIn.setBlockState(pos, state, 2);
+			return true;
+		} else
+		{
+			return false;
+		}
+	}
+	
+	public void insertCassette(IWorld worldIn, BlockPos pos, BlockState state, ItemStack cassetteStack)
+	{
+		TileEntity tileentity = worldIn.getTileEntity(pos);
+		if(tileentity instanceof CassettePlayerTileEntity && state.get(IS_OPEN))
+		{
+			if(state.get(HAS_CASSETTE))
+			{
+				this.dropCassette((World) worldIn, pos);
+			} else
+			{
+				((CassettePlayerTileEntity) tileentity).setCassette(cassetteStack.copy());
+				worldIn.setBlockState(pos, state.with(HAS_CASSETTE, true), 2);
+			}
+		}
+	}
+	
+	private void dropCassette(World worldIn, BlockPos pos)
+	{
+		if(!worldIn.isRemote)
+		{
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			if(tileentity instanceof CassettePlayerTileEntity)
+			{
+				CassettePlayerTileEntity cassetteplayertileentity = (CassettePlayerTileEntity) tileentity;
+				ItemStack itemstack = cassetteplayertileentity.getCassette();
+				if(!itemstack.isEmpty())
+				{
+					worldIn.playEvent(1010, pos, 0);
+					cassetteplayertileentity.clear();
+					float f = 0.7F;
+					double randomX = (double) (worldIn.rand.nextFloat() * 0.7F) + (double) 0.15F;
+					double randomY = (double) (worldIn.rand.nextFloat() * 0.7F) + (double) 0.060000002F + 0.6D;
+					double randomZ = (double) (worldIn.rand.nextFloat() * 0.7F) + (double) 0.15F;
+					ItemStack itemstack1 = itemstack.copy();
+					ItemEntity itementity = new ItemEntity(worldIn, (double) pos.getX() + randomX, (double) pos.getY() + randomY, (double) pos.getZ() + randomZ, itemstack1);
+					itementity.setDefaultPickupDelay();
+					worldIn.addEntity(itementity);
+				}
+			}
+		}
+	}
+	
+	public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving)
+	{
+		if(state.getBlock() != newState.getBlock())
+		{
+			this.dropCassette(worldIn, pos);
+			super.onReplaced(state, worldIn, pos, newState, isMoving);
+		}
+	}
+	
+	@Override
+	public boolean hasTileEntity(BlockState state)
+	{
+		return true;
+	}
+	
+	@Nullable
+	@Override
+	public TileEntity createTileEntity(BlockState state, IBlockReader world)
+	{
+		return tileType.get().create();
+	}
+	
+	public boolean hasComparatorInputOverride(BlockState state)
+	{
+		return true;
+	}
+	
+	public int getComparatorInputOverride(BlockState blockState, World worldIn, BlockPos pos)
+	{
+		TileEntity tileentity = worldIn.getTileEntity(pos);
+		if(tileentity instanceof CassettePlayerTileEntity)
+		{
+			Item item = ((CassettePlayerTileEntity) tileentity).getCassette().getItem();
+			if(item instanceof CassetteItem)
+			{
+				return ((CassetteItem) item).getComparatorValue();
+			}
+		}
+		
+		return 0;
+	}
+	
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder)
+	{
+		builder.add(FACING);
+		builder.add(HAS_CASSETTE);
+		builder.add(IS_OPEN);
+	}
 }
