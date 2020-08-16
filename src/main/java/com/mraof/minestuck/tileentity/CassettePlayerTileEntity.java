2b5488818786fe94e97109cfc2a8947decb71fa4
LunaticCat <yannbrun1507@outlook.fr>
2020-08-16 08:19:48 +0200

    reformatting CassettePlayerTileEntity and changing variable name

--- src/main/java/com/mraof/minestuck/tileentity/CassettePlayerTileEntity.java
+++ src/main/java/com/mraof/minestuck/tileentity/CassettePlayerTileEntity.java
@@ -5,40 +5,49 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.TileEntity;
 
-public class CassettePlayerTileEntity extends TileEntity implements IClearable {
-    private ItemStack cassette = ItemStack.EMPTY;
-
-    public CassettePlayerTileEntity() {
-        super(MSTileEntityTypes.CASSETTE_PLAYER);
-    }
-
-    public void read(CompoundNBT compound) {
-        super.read(compound);
-        if (compound.contains("CassetteItem", 10)) {
-            this.setCassette(ItemStack.read(compound.getCompound("CassetteItem")));
-        }
-
-    }
-
-    public CompoundNBT write(CompoundNBT compound) {
-        super.write(compound);
-        if (!this.getCassette().isEmpty()) {
-            compound.put("CassetteItem", this.getCassette().write(new CompoundNBT()));
-        }
-
-        return compound;
-    }
-
-    public ItemStack getCassette() {
-        return this.cassette;
-    }
-
-    public void setCassette(ItemStack p_195535_1_) {
-        this.cassette = p_195535_1_;
-        this.markDirty();
-    }
-
-    public void clear() {
-        this.setCassette(ItemStack.EMPTY);
-    }
+public class CassettePlayerTileEntity extends TileEntity implements IClearable
+{
+	private ItemStack cassette = ItemStack.EMPTY;
+	
+	public CassettePlayerTileEntity()
+	{
+		super(MSTileEntityTypes.CASSETTE_PLAYER);
+	}
+	
+	public void read(CompoundNBT compound)
+	{
+		super.read(compound);
+		if(compound.contains("CassetteItem", 10))
+		{
+			this.setCassette(ItemStack.read(compound.getCompound("CassetteItem")));
+		}
+		
+	}
+	
+	public CompoundNBT write(CompoundNBT compound)
+	{
+		super.write(compound);
+		if(!this.getCassette().isEmpty())
+		{
+			compound.put("CassetteItem", this.getCassette().write(new CompoundNBT()));
+		}
+		
+		return compound;
+	}
+	
+	public ItemStack getCassette()
+	{
+		return this.cassette;
+	}
+	
+	public void setCassette(ItemStack stack)
+	{
+		this.cassette = stack;
+		this.markDirty();
+	}
+	
+	public void clear()
+	{
+		this.setCassette(ItemStack.EMPTY);
+	}
 }
