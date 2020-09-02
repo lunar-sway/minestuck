8f5b29cbfb3717af5de85ecf89a456a1ddbeecb2
kirderf1 <kirderf4@gmail.com>
2020-09-02 14:11:32 +0200

    Fixed porting mistake in ocean rundown
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/world/gen/feature/OceanRundownFeature.java
+++ src/main/java/com/mraof/minestuck/world/gen/feature/OceanRundownFeature.java
@@ -5,7 +5,6 @@ import com.mraof.minestuck.world.biome.MSBiomes;
 import net.minecraft.block.BlockState;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IWorld;
-import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.GenerationSettings;
 import net.minecraft.world.gen.Heightmap;
@@ -15,7 +14,6 @@ import net.minecraft.world.gen.feature.NoFeatureConfig;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
 import java.util.function.Function;
 
 public class OceanRundownFeature extends Feature<NoFeatureConfig>
@@ -36,12 +34,11 @@ public class OceanRundownFeature extends Feature<NoFeatureConfig>
 		}
 		//Look for ocean and pick pos2 and pos3
 		List<BlockPos> oceanPos = new ArrayList<>();
-		Set<Biome> biomes = generator.getBiomeProvider().getBiomes(pos.getX() - 8, worldIn.getSeaLevel(), pos.getZ() - 8, 16);
 		for(int posX = 0; posX < 16; posX++)
 		{
 			for(int posZ = 0; posZ < 16; posZ++)
 			{
-				if(biomes.toArray()[posX + posZ * 16].equals(MSBiomes.LAND_OCEAN))
+				if(generator.getBiomeProvider().getNoiseBiome(pos.getX() + posX - 8, pos.getY(), pos.getZ() + posZ - 8).equals(MSBiomes.LAND_OCEAN))
 					oceanPos.add(pos.add(posX - 8, 0, posZ - 8));
 			}
 		}
