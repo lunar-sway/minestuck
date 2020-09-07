bf07b0c5e8ed5dcedbbe854c1f9a8a4b11099340
kirderf1 <kirderf4@gmail.com>
2020-09-07 17:26:14 +0200

    Uncomment jei plugin
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/jei/GristIngredientHelper.java
+++ src/main/java/com/mraof/minestuck/jei/GristIngredientHelper.java
@@ -1,84 +1,84 @@
 package com.mraof.minestuck.jei;
 
-//import com.mraof.minestuck.item.crafting.alchemy.GristAmount;
-//import com.mraof.minestuck.item.crafting.alchemy.GristType;
-//import com.mraof.minestuck.item.crafting.alchemy.GristTypes;
-//import mezz.jei.api.ingredients.IIngredientHelper;
-//
-//import javax.annotation.Nullable;
-//import java.util.ArrayList;
-//import java.util.Collections;
-//import java.util.List;
-//
-//public class GristIngredientHelper implements IIngredientHelper<GristAmount>
-//{
-//	public static List<GristAmount> createList()
-//	{
-//		List<GristAmount> list = new ArrayList<>();
-//		for(GristType gristType : GristTypes.values())
-//			list.add(new GristAmount(gristType, 1));
-//		return list;
-//	}
-//
-//	@Nullable
-//	@Override
-//	public GristAmount getMatch(Iterable<GristAmount> ingredients, GristAmount ingredientToMatch)
-//	{
-//		for(GristAmount grist : ingredients)
-//			if(grist.getType() == ingredientToMatch.getType())
-//				return grist;
-//		return null;
-//	}
-//
-//	@Override
-//	public String getDisplayName(GristAmount ingredient)
-//	{
-//		return ingredient.getType().getDisplayName().getFormattedText();
-//	}
-//
-//	@Override
-//	public String getUniqueId(GristAmount ingredient)
-//	{
-//		return "grist:" + ingredient.getType().getRegistryName();
-//	}
-//
-//	@Override
-//	public String getWildcardId(GristAmount ingredient)
-//	{
-//		return getUniqueId(ingredient);
-//	}
-//
-//	@Override
-//	public String getModId(GristAmount ingredient)
-//	{
-//		return ingredient.getType().getRegistryName().getNamespace();
-//	}
-//
-//	@Override
-//	public Iterable<Integer> getColors(GristAmount ingredient)
-//	{
-//		return Collections.emptyList();	//Not dealing with this right now
-//	}
-//
-//	@Override
-//	public String getResourceId(GristAmount ingredient)
-//	{
-//		return ingredient.getType().getRegistryName().getPath();
-//	}
-//
-//	@Override
-//	public GristAmount copyIngredient(GristAmount ingredient)
-//	{
-//		return new GristAmount(ingredient.getType(), ingredient.getAmount());
-//	}
-//
-//	@Override
-//	public String getErrorInfo(@Nullable GristAmount ingredient)
-//	{
-//		if(ingredient == null)
-//			return "grist:null";
-//		else if(ingredient.getType() == null)
-//			return "grist:null:"+ingredient.getAmount();
-//		else return "grist:"+ingredient.getType().getRegistryName()+":"+ingredient.getAmount();
-//	}
-//}
+import com.mraof.minestuck.item.crafting.alchemy.GristAmount;
+import com.mraof.minestuck.item.crafting.alchemy.GristType;
+import com.mraof.minestuck.item.crafting.alchemy.GristTypes;
+import mezz.jei.api.ingredients.IIngredientHelper;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GristIngredientHelper implements IIngredientHelper<GristAmount>
+{
+	public static List<GristAmount> createList()
+	{
+		List<GristAmount> list = new ArrayList<>();
+		for(GristType gristType : GristTypes.values())
+			list.add(new GristAmount(gristType, 1));
+		return list;
+	}
+
+	@Nullable
+	@Override
+	public GristAmount getMatch(Iterable<GristAmount> ingredients, GristAmount ingredientToMatch)
+	{
+		for(GristAmount grist : ingredients)
+			if(grist.getType() == ingredientToMatch.getType())
+				return grist;
+		return null;
+	}
+
+	@Override
+	public String getDisplayName(GristAmount ingredient)
+	{
+		return ingredient.getType().getDisplayName().getFormattedText();
+	}
+
+	@Override
+	public String getUniqueId(GristAmount ingredient)
+	{
+		return "grist:" + ingredient.getType().getRegistryName();
+	}
+
+	@Override
+	public String getWildcardId(GristAmount ingredient)
+	{
+		return getUniqueId(ingredient);
+	}
+
+	@Override
+	public String getModId(GristAmount ingredient)
+	{
+		return ingredient.getType().getRegistryName().getNamespace();
+	}
+
+	@Override
+	public Iterable<Integer> getColors(GristAmount ingredient)
+	{
+		return Collections.emptyList();	//Not dealing with this right now
+	}
+
+	@Override
+	public String getResourceId(GristAmount ingredient)
+	{
+		return ingredient.getType().getRegistryName().getPath();
+	}
+
+	@Override
+	public GristAmount copyIngredient(GristAmount ingredient)
+	{
+		return new GristAmount(ingredient.getType(), ingredient.getAmount());
+	}
+
+	@Override
+	public String getErrorInfo(@Nullable GristAmount ingredient)
+	{
+		if(ingredient == null)
+			return "grist:null";
+		else if(ingredient.getType() == null)
+			return "grist:null:"+ingredient.getAmount();
+		else return "grist:"+ingredient.getType().getRegistryName()+":"+ingredient.getAmount();
+	}
+}
--- src/main/java/com/mraof/minestuck/jei/GristIngredientRenderer.java
+++ src/main/java/com/mraof/minestuck/jei/GristIngredientRenderer.java
@@ -1,59 +1,59 @@
 package com.mraof.minestuck.jei;
 
-//import com.mojang.blaze3d.platform.GlStateManager;
-//import com.mraof.minestuck.item.crafting.alchemy.GristAmount;
-//import mezz.jei.api.ingredients.IIngredientRenderer;
-//import net.minecraft.client.Minecraft;
-//import net.minecraft.client.renderer.BufferBuilder;
-//import net.minecraft.client.renderer.Tessellator;
-//import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-//import net.minecraft.client.util.ITooltipFlag;
-//import net.minecraft.util.ResourceLocation;
-//import net.minecraft.util.text.TextFormatting;
-//
-//import javax.annotation.Nullable;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class GristIngredientRenderer implements IIngredientRenderer<GristAmount>
-//{
-//	@Override
-//	public void render(int xPosition, int yPosition, @Nullable GristAmount ingredient)
-//	{
-//		if(ingredient == null)
-//			return;
-//		GlStateManager.enableBlend();
-//		GlStateManager.enableAlphaTest();
-//		GlStateManager.color4f(1, 1, 1, 1);
-//
-//		ResourceLocation icon = ingredient.getType().getIcon();
-//		Minecraft.getInstance().getTextureManager().bindTexture(icon);
-//
-//		float scale = (float) 1 / 16;
-//
-//		int iconX = 16;
-//		int iconY = 16;
-//		int iconU = 0;
-//		int iconV = 0;
-//
-//		BufferBuilder render = Tessellator.getInstance().getBuffer();
-//		render.begin(7, DefaultVertexFormats.POSITION_TEX);
-//		render.pos(xPosition, yPosition + iconY, 0D).tex((iconU) * scale, (iconV + iconY) * scale).endVertex();
-//		render.pos(xPosition + iconX, yPosition + iconY, 0D).tex((iconU + iconX) * scale, (iconV + iconY) * scale).endVertex();
-//		render.pos(xPosition + iconX, yPosition, 0D).tex((iconU + iconX) * scale, (iconV) * scale).endVertex();
-//		render.pos(xPosition, yPosition, 0D).tex((iconU) * scale, (iconV) * scale).endVertex();
-//		Tessellator.getInstance().draw();
-//
-//		GlStateManager.disableAlphaTest();
-//		GlStateManager.disableBlend();
-//	}
-//
-//	@Override
-//	public List<String> getTooltip(GristAmount ingredient, ITooltipFlag tooltipFlag)
-//	{
-//		List<String> list = new ArrayList<>();
-//		list.add(ingredient.getType().getDisplayName().getFormattedText());
-//		list.add(TextFormatting.DARK_GRAY+""+ingredient.getType().getRegistryName());
-//		return list;
-//	}
-//}
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mraof.minestuck.item.crafting.alchemy.GristAmount;
+import mezz.jei.api.ingredients.IIngredientRenderer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BufferBuilder;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextFormatting;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GristIngredientRenderer implements IIngredientRenderer<GristAmount>
+{
+	@Override
+	public void render(int xPosition, int yPosition, @Nullable GristAmount ingredient)
+	{
+		if(ingredient == null)
+			return;
+		RenderSystem.enableBlend();
+		RenderSystem.enableAlphaTest();
+		RenderSystem.color4f(1, 1, 1, 1);
+
+		ResourceLocation icon = ingredient.getType().getIcon();
+		Minecraft.getInstance().getTextureManager().bindTexture(icon);
+
+		float scale = (float) 1 / 16;
+
+		int iconX = 16;
+		int iconY = 16;
+		int iconU = 0;
+		int iconV = 0;
+
+		BufferBuilder render = Tessellator.getInstance().getBuffer();
+		render.begin(7, DefaultVertexFormats.POSITION_TEX);
+		render.pos(xPosition, yPosition + iconY, 0D).tex((iconU) * scale, (iconV + iconY) * scale).endVertex();
+		render.pos(xPosition + iconX, yPosition + iconY, 0D).tex((iconU + iconX) * scale, (iconV + iconY) * scale).endVertex();
+		render.pos(xPosition + iconX, yPosition, 0D).tex((iconU + iconX) * scale, (iconV) * scale).endVertex();
+		render.pos(xPosition, yPosition, 0D).tex((iconU) * scale, (iconV) * scale).endVertex();
+		Tessellator.getInstance().draw();
+		
+		RenderSystem.disableAlphaTest();
+		RenderSystem.disableBlend();
+	}
+
+	@Override
+	public List<String> getTooltip(GristAmount ingredient, ITooltipFlag tooltipFlag)
+	{
+		List<String> list = new ArrayList<>();
+		list.add(ingredient.getType().getDisplayName().getFormattedText());
+		list.add(TextFormatting.DARK_GRAY+""+ingredient.getType().getRegistryName());
+		return list;
+	}
+}
