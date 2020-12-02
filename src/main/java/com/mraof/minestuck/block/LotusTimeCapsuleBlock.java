bd487a2c464b1dc19566172c77f4ecee4eafb043
Dweblenod <ryanpgunnison@gmail.com>
2020-12-02 14:37:34 -0500

    Messing around. Added copy of loot table outside of chest folder. Probably non functional

--- src/main/java/com/mraof/minestuck/block/LotusTimeCapsuleBlock.java
+++ src/main/java/com/mraof/minestuck/block/LotusTimeCapsuleBlock.java
@@ -3,15 +3,25 @@ package com.mraof.minestuck.block;
 import com.mraof.minestuck.item.MSItems;
 import com.mraof.minestuck.util.MSSoundEvents;
 import net.minecraft.block.*;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.brain.BrainUtil;
 import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.world.storage.loot.LootParameterSets;
+import net.minecraft.world.storage.loot.LootParameters;
+import net.minecraft.world.storage.loot.LootTable;
 
+import java.util.List;
 import java.util.Random;
 
 public class LotusTimeCapsuleBlock extends Block
@@ -28,12 +38,15 @@ public class LotusTimeCapsuleBlock extends Block
 		{
 			if(!worldIn.isRemote/* && UNACTIVATED.equals(true)*/)
 			{
-				Direction direction = hit.getFace();
-				Direction direction1 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
+				//Direction direction = hit.getFace();
+				//Direction direction1 = direction.getAxis() == Direction.Axis.Y ? player.getHorizontalFacing().getOpposite() : direction;
 				worldIn.playSound((PlayerEntity) null, pos, MSSoundEvents.EVENT_ECHELADDER_INCREASE, SoundCategory.BLOCKS, 1.0F, 1.0F);
-				ItemEntity itementity = new ItemEntity(worldIn, (double) pos.getX() + 0.5D + (double) direction1.getXOffset() * 0.65D, (double) pos.getY() + 0.1D, (double) pos.getZ() + 0.5D + (double) direction1.getZOffset() * 0.65D, new ItemStack(MSItems.SERVER_DISK, 1));
-				itementity.setMotion(0.05D * (double) direction1.getXOffset() + worldIn.rand.nextDouble() * 0.02D, 0.05D, 0.05D * (double) direction1.getZOffset() + worldIn.rand.nextDouble() * 0.02D);
-				worldIn.addEntity(itementity);
+				//ItemEntity itementity = new ItemEntity(worldIn, (double) pos.getX() + 0.5D + (double) direction1.getXOffset() * 0.65D, (double) pos.getY() + 0.1D, (double) pos.getZ() + 0.5D + (double) direction1.getZOffset() * 0.65D, new ItemStack(MSItems.SERVER_DISK, 1));
+				//itementity.setMotion(0.05D * (double) direction1.getXOffset() + worldIn.rand.nextDouble() * 0.02D, 0.05D, 0.05D * (double) direction1.getZOffset() + worldIn.rand.nextDouble() * 0.02D);
+				//worldIn.addEntity(itementity);
+				
+				//List<ItemStack> itemStack = this.generateLoot(lootTable, player);
+				
 				//ItemEntity itementity1 = new ItemEntity(worldIn, (double) pos.getX() + 0.5D + (double) direction1.getXOffset() * 0.65D, (double) pos.getY() + 0.1D, (double) pos.getZ() + 0.5D + (double) direction1.getZOffset() * 0.65D, new ItemStack(MSItems.CLIENT_DISK, 1));
 				//itementity1.setMotion(0.05D * (double) direction1.getXOffset() + worldIn.rand.nextDouble() * 0.02D, 0.05D, 0.05D * (double) direction1.getZOffset() + worldIn.rand.nextDouble() * 0.02D);
 				//worldIn.addEntity(itementity1);
@@ -54,4 +67,16 @@ public class LotusTimeCapsuleBlock extends Block
 			worldIn.setBlockState(pos, MSBlocks.MINI_FROG_STATUE.getDefaultState());
 		}
 	}
+	
+	public List<ItemStack>  generateLoot(ResourceLocation lootTable, PlayerEntity playerEntity) {
+		//LootTable loottable = playerEntity.world.getServer().getLootTableManager().getLootTableFromLocation(playerEntity.getLootTableResourceLocation());
+		//LootContext.Builder lootcontext$builder = (new LootContext.Builder((ServerWorld)playerEntity.world)).withParameter(LootParameters.POSITION, new BlockPos(playerEntity)).withParameter(LootParameters.THIS_ENTITY, playerEntity).withRandom(playerEntity.getRNG());
+		//return loottable.generate(lootcontext$builder.build(LootParameterSets.CHEST));
+		
+		LootContext.Builder contextBuilder = new LootContext.Builder((ServerWorld) playerEntity.world).withParameter(LootParameters.THIS_ENTITY, playerEntity).withParameter(LootParameters.POSITION, new BlockPos(playerEntity));
+		List<ItemStack> itemStacks = playerEntity.getServer().getLootTableManager().getLootTableFromLocation(lootTable).generate(contextBuilder.build(LootParameterSets.GIFT));
+		return itemStacks;
+	}
+	
+	
 }
