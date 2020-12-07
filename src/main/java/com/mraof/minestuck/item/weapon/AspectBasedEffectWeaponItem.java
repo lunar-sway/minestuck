521e0ae61673790921b5c762311e126c46485496
kirderf1 <kirderf4@gmail.com>
2020-12-07 13:12:39 +0100

    Modified code for aspect weapons slightly
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/item/weapon/AspectBasedEffectWeaponItem.java
+++ src/main/java/com/mraof/minestuck/item/weapon/AspectBasedEffectWeaponItem.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.SoundCategory;
 
@@ -18,40 +17,33 @@ import java.util.function.Supplier;
 
 public class AspectBasedEffectWeaponItem extends WeaponItem
 {
-    private final EnumAspect aspect;
-    private final Supplier<Effect> effect;
-    private final int duration;
-    private final int effectTier;
-    
-    public AspectBasedEffectWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, EnumAspect aspect, Supplier<Effect> effect, int duration, int effectTier, @Nullable MSToolType toolType, Properties builder)
-    {
-        super(tier, attackDamageIn, attackSpeedIn, efficiency, toolType, builder);
-        this.aspect = aspect;
-        this.effect = effect;
-        this.duration = duration;
-        this.effectTier = effectTier;
-    }
-    
-    @Override
-    public boolean hitEntity(ItemStack itemStack, LivingEntity target, LivingEntity attacker)
-    {
-        Title title = PlayerSavedData.getData((ServerPlayerEntity) attacker).getTitle();
-        
-        if(attacker instanceof ServerPlayerEntity && attacker.getRNG().nextFloat() < .1)
-        {
-            if(title != null)
-            {
-                if (title.getHeroAspect() == aspect)
-                {
-                    ((ServerPlayerEntity) attacker).addPotionEffect(new EffectInstance(effect.get(), duration, effectTier));
-                }
-            }
-        }
-    
-        if (itemStack.getItem() == MSItems.CLOWN_CLUB) {
-            attacker.world.playSound(null, attacker.posX, attacker.posY, attacker.posZ, MSSoundEvents.ITEM_HORN_USE, SoundCategory.AMBIENT, 1.5F, 1.0F);
-        }
-        
-        return super.hitEntity(itemStack, target, attacker);
-    }
+	private final EnumAspect aspect;
+	private final Supplier<EffectInstance> effect;
+	
+	public AspectBasedEffectWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, EnumAspect aspect, Supplier<EffectInstance> effect, @Nullable MSToolType toolType, Properties builder)
+	{
+		super(tier, attackDamageIn, attackSpeedIn, efficiency, toolType, builder);
+		this.aspect = aspect;
+		this.effect = effect;
+	}
+	
+	@Override
+	public boolean hitEntity(ItemStack itemStack, LivingEntity target, LivingEntity attacker)
+	{
+		if(attacker instanceof ServerPlayerEntity)
+		{
+			Title title = PlayerSavedData.getData((ServerPlayerEntity) attacker).getTitle();
+			
+			if(title != null && attacker.getRNG().nextFloat() < .1)
+			{
+				if(title.getHeroAspect() == aspect)
+					attacker.addPotionEffect(effect.get());
+			}
+		}
+		
+		if(itemStack.getItem() == MSItems.CLOWN_CLUB)
+			attacker.world.playSound(null, attacker.posX, attacker.posY, attacker.posZ, MSSoundEvents.ITEM_HORN_USE, SoundCategory.AMBIENT, 1.5F, 1.0F);
+		
+		return super.hitEntity(itemStack, target, attacker);
+	}
 }
