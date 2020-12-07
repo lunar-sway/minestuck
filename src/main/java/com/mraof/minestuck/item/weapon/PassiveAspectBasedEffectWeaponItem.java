de639c453ce4f1657914d873e9f47430083c17e0
kirderf1 <kirderf4@gmail.com>
2020-12-07 13:12:39 +0100

    Modified code for aspect weapons slightly
    Signed-off-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/item/weapon/PassiveAspectBasedEffectWeaponItem.java
+++ src/main/java/com/mraof/minestuck/item/weapon/PassiveAspectBasedEffectWeaponItem.java
@@ -7,43 +7,35 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.ItemStack;
-import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
-
 import java.util.function.Supplier;
 
 public class PassiveAspectBasedEffectWeaponItem extends WeaponItem
 {
-    private final EnumAspect aspect;
-    private final Supplier<Effect> effect;
-    private final int duration;
-    private final int effectTier;
-    
-    public PassiveAspectBasedEffectWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, EnumAspect aspect, Supplier<Effect> effect, int duration, int effectTier, @Nullable MSToolType toolType, Properties builder)
-    {
-        super(tier, attackDamageIn, attackSpeedIn, efficiency, toolType, builder);
-        this.aspect = aspect;
-        this.effect = effect;
-        this.duration = duration;
-        this.effectTier = effectTier;
-    }
-    
-    @Override
-    public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
-    {
-        if(isSelected && entityIn instanceof ServerPlayerEntity)
-        {
-            Title title = PlayerSavedData.getData((ServerPlayerEntity) entityIn).getTitle();
-            if(title != null)
-            {
-                if (title.getHeroAspect() == aspect)
-                {
-                    ((ServerPlayerEntity) entityIn).addPotionEffect(new EffectInstance(effect.get(), duration, effectTier));
-                }
-            }
-        }
-    }
+	private final EnumAspect aspect;
+	private final Supplier<EffectInstance> effect;
+	
+	public PassiveAspectBasedEffectWeaponItem(IItemTier tier, int attackDamageIn, float attackSpeedIn, float efficiency, EnumAspect aspect, Supplier<EffectInstance> effect, @Nullable MSToolType toolType, Properties builder)
+	{
+		super(tier, attackDamageIn, attackSpeedIn, efficiency, toolType, builder);
+		this.aspect = aspect;
+		this.effect = effect;
+	}
+	
+	@Override
+	public void inventoryTick(ItemStack stack, World worldIn, Entity entityIn, int itemSlot, boolean isSelected)
+	{
+		if(isSelected && entityIn instanceof ServerPlayerEntity)
+		{
+			Title title = PlayerSavedData.getData((ServerPlayerEntity) entityIn).getTitle();
+			if(title != null)
+			{
+				if(title.getHeroAspect() == aspect)
+					((ServerPlayerEntity) entityIn).addPotionEffect(effect.get());
+			}
+		}
+	}
 }
