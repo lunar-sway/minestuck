d5a7d33eff2ecf175e56452576f2179580fcc1f2
Dweblenod <63124354+Dweblenod@users.noreply.github.com>
2021-02-23 04:53:37 -0500

    Adding "Reckoning" inspired rightclickeffect to King Scepters (#280)

--- src/main/java/com/mraof/minestuck/item/weapon/ItemRightClickEffect.java
+++ src/main/java/com/mraof/minestuck/item/weapon/ItemRightClickEffect.java
@@ -1,12 +1,18 @@
 package com.mraof.minestuck.item.weapon;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FireballEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public interface ItemRightClickEffect
@@ -28,6 +34,39 @@ public interface ItemRightClickEffect
 				newItem.setTag(itemStackIn.getTag());
 				
 				return ActionResult.resultSuccess(newItem);
+			}
+			return ActionResult.resultPass(itemStackIn);
+		};
+	}
+	
+	static ItemRightClickEffect summonFireball()
+	{
+		return (world, player, hand) -> {
+			ItemStack itemStackIn = player.getHeldItem(hand);
+			world.playSound(null, player.getPosX(), player.getPosY(), player.getPosZ(), SoundEvents.ENTITY_BLAZE_SHOOT, SoundCategory.PLAYERS, 1.0F, 0.8F);
+			
+			AxisAlignedBB axisalignedbb = player.getBoundingBox().grow(32.0D, 32.0D, 32.0D);
+			List<LivingEntity> list = player.world.getEntitiesWithinAABB(LivingEntity.class, axisalignedbb);
+			list.remove(player);
+			if (!list.isEmpty() && !world.isRemote)
+			{
+				for(LivingEntity livingentity : list){
+					FireballEntity fireball = new FireballEntity(world, player, 0, -8.0, 0);
+					fireball.explosionPower = 1;
+					fireball.setPosition(livingentity.getPosX()+(player.getRNG().nextInt(6)-3), livingentity.getPosY()+40, livingentity.getPosZ()+(player.getRNG().nextInt(6)-3));
+					player.getCooldownTracker().setCooldown(itemStackIn.getItem(), 20);
+					itemStackIn.damageItem(2, player, playerEntity -> playerEntity.sendBreakAnimation(Hand.MAIN_HAND));
+					world.addEntity(fireball);
+				}
+			}
+			else if (!world.isRemote)
+			{
+				FireballEntity fireball = new FireballEntity(world, player, 0, -8.0, 0);
+				fireball.explosionPower = 1;
+				fireball.setPosition(player.getPosX()+(player.getRNG().nextInt(20)-10), player.getPosY()+40, player.getPosZ()+(player.getRNG().nextInt(20)-10));
+				player.getCooldownTracker().setCooldown(itemStackIn.getItem(), 20);
+				itemStackIn.damageItem(2, player, playerEntity -> playerEntity.sendBreakAnimation(Hand.MAIN_HAND));
+				world.addEntity(fireball);
 			}
 			return ActionResult.resultPass(itemStackIn);
 		};
