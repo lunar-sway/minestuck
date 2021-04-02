8f5cc27c914a5935d2e8f717c5c5bb568e4bed86
Dweblenod <63124354+Dweblenod@users.noreply.github.com>
2021-04-02 16:00:10 -0400

    Bulk items and blocks (#290)

---
+++ src/main/java/com/mraof/minestuck/item/RightClickMessageItem.java
@@ -0,0 +1,53 @@
+package com.mraof.minestuck.item;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.*;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import java.util.function.Supplier;
+
+public class RightClickMessageItem extends Item
+{
+	private final Type type;
+	
+	public enum Type {
+		EIGHTBALL,
+		DICE,
+		DEFAULT
+	}
+	
+	public RightClickMessageItem(Properties properties, Type type)
+	{
+		super(properties);
+		this.type = type;
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, PlayerEntity playerIn, Hand handIn)
+	{
+		if(worldIn.isRemote)
+		{
+			if(type == Type.EIGHTBALL)
+			{
+				int key = playerIn.getRNG().nextInt(20);
+				ITextComponent message = new TranslationTextComponent("message.eightball." + key);
+				playerIn.sendMessage(message.applyTextStyle(TextFormatting.BLUE));
+			} else if(type == Type.DICE)
+			{
+				int key = playerIn.getRNG().nextInt(6);
+				ITextComponent message = new TranslationTextComponent("message.dice." + key);
+				playerIn.sendMessage(message.applyTextStyle(TextFormatting.WHITE));
+			} else
+			{
+				playerIn.sendMessage(new TranslationTextComponent(getTranslationKey() + ".message")); //default, creates message for that item under that item's "addExtra" in MSEnUsLang provider
+			}
+		}
+		
+		return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
+	}
+}
