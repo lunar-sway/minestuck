4f85908719bc1a591e10bdc6263db2af9cee5db0
QuartzSuperNova <CoderCat012@gmail.com>
2021-04-19 23:34:08 -0500

    Added Tuix Bar, a food item i made to practice adding items (fixed an error made while adding the bar, it is too powerful)

--- src/main/java/com/mraof/minestuck/data/recipe/MinestuckGristCostsProvider.java
+++ src/main/java/com/mraof/minestuck/data/recipe/MinestuckGristCostsProvider.java
@@ -535,7 +535,7 @@ public class MinestuckGristCostsProvider extends RecipeProvider
 		GristCostRecipeBuilder.of(MSItems.SPORK).grist(BUILD, 13).build(recipeSaver);
 		GristCostRecipeBuilder.of(MSItems.CANDY_CORN).grist(CHALK, 1).grist(SULFUR, 1).grist(IODINE, 1).build(recipeSaver);
 		GristCostRecipeBuilder.of(MSItems.TUIX_BAR).grist(BUILD, 5).grist(IODINE, 1).build(recipeSaver);
-		for(GristType type : values())
+		for(GristType type : GristTypes.values())
 		{
 			if(type.getRegistryName().getNamespace().equals(Minestuck.MOD_ID))
 				GristCostRecipeBuilder.of(type.getCandyItem().getItem()).grist(type, 3).build(recipeSaver);
