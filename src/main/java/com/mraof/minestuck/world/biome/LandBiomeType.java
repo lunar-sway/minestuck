7c9bde709f64bd807b27ba0a46808c10f0c95f50
kirderf1 <kirderf4@gmail.com>
2021-10-05 15:07:41 +0200

    Fix first gate destination search

--- src/main/java/com/mraof/minestuck/world/biome/LandBiomeType.java
+++ src/main/java/com/mraof/minestuck/world/biome/LandBiomeType.java
@@ -1,13 +1,6 @@
 package com.mraof.minestuck.world.biome;
 
-import net.minecraft.world.biome.Biome;
-
 public enum LandBiomeType
 {
 	NORMAL, ROUGH, OCEAN;
-	
-	public boolean isBiomeOfType(Biome biome)
-	{
-		return false;//biome instanceof LandBiome && ((LandBiome) biome).type == this; TODO
-	}
 }
