ab3d69f42ca9f541e6c3a998c78bcdf50ba4ca6f
Amiwyn <7491829+amiwyn@users.noreply.github.com>
2022-05-09 17:30:47 -0400

    pawn textures

--- src/main/java/com/mraof/minestuck/client/model/PawnModel.java
+++ src/main/java/com/mraof/minestuck/client/model/PawnModel.java
@@ -1,24 +1,29 @@
 package com.mraof.minestuck.client.model;
 
 import com.mraof.minestuck.Minestuck;
+import com.mraof.minestuck.entity.carapacian.EnumEntityKingdom;
 import com.mraof.minestuck.entity.carapacian.PawnEntity;
 import net.minecraft.util.ResourceLocation;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
-public class PawnModel extends AnimatedGeoModel<PawnEntity>
-{
-	@Override
-	public ResourceLocation getModelLocation(PawnEntity object) {
-		return new ResourceLocation(Minestuck.MOD_ID, "geo/entity/carapacian/pawn.geo.json");
-	}
+public class PawnModel extends AnimatedGeoModel<PawnEntity> {
+    @Override
+    public ResourceLocation getModelLocation(PawnEntity object) {
+        return new ResourceLocation(Minestuck.MOD_ID, "geo/entity/carapacian/pawn.geo.json");
+    }
 
-	@Override
-	public ResourceLocation getTextureLocation(PawnEntity object) {
-		return new ResourceLocation(Minestuck.MOD_ID, "textures/entity/carapacian/pawn.png");
-	}
+    @Override
+    public ResourceLocation getTextureLocation(PawnEntity object) {
+        if (object.getKingdom() == EnumEntityKingdom.DERSITE) {
+            return new ResourceLocation(Minestuck.MOD_ID, "textures/entity/carapacian/derse_pawn.png");
+        } else {
+            return new ResourceLocation(Minestuck.MOD_ID, "textures/entity/carapacian/prospit_pawn.png");
+        }
 
-	@Override
-	public ResourceLocation getAnimationFileLocation(PawnEntity animatable) {
-		return new ResourceLocation(Minestuck.MOD_ID, "animations/entity/carapacian/pawn.animation.json");
-	}
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(PawnEntity animatable) {
+        return new ResourceLocation(Minestuck.MOD_ID, "animations/entity/carapacian/pawn.animation.json");
+    }
 }
