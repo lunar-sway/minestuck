7183a7598239e02d700c6014de11148eff621014
kirderf1 <kirderf4@gmail.com>
2022-07-17 21:55:26 +0200

    Update datagen for regular recipes

--- src/main/java/com/mraof/minestuck/data/recipe/IrradiatingFallbackRecipeBuilder.java
+++ src/main/java/com/mraof/minestuck/data/recipe/IrradiatingFallbackRecipeBuilder.java
@@ -2,41 +2,41 @@ package com.mraof.minestuck.data.recipe;
 
 import com.google.gson.JsonObject;
 import com.mraof.minestuck.item.crafting.MSRecipeTypes;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.item.crafting.AbstractCookingRecipe;
-import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraft.item.crafting.IRecipeType;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.registry.Registry;
+import net.minecraft.core.Registry;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.AbstractCookingRecipe;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.RecipeType;
 
 import javax.annotation.Nullable;
 import java.util.function.Consumer;
 
 public class IrradiatingFallbackRecipeBuilder
 {
-	private final IRecipeType<? extends AbstractCookingRecipe> fallbackType;
+	private final RecipeType<? extends AbstractCookingRecipe> fallbackType;
 	
-	public IrradiatingFallbackRecipeBuilder(IRecipeType<? extends AbstractCookingRecipe> fallbackType)
+	public IrradiatingFallbackRecipeBuilder(RecipeType<? extends AbstractCookingRecipe> fallbackType)
 	{
 		this.fallbackType = fallbackType;
 	}
 	
-	public static IrradiatingFallbackRecipeBuilder fallback(IRecipeType<? extends AbstractCookingRecipe> fallbackType)
+	public static IrradiatingFallbackRecipeBuilder fallback(RecipeType<? extends AbstractCookingRecipe> fallbackType)
 	{
 		return new IrradiatingFallbackRecipeBuilder(fallbackType);
 	}
 	
-	public void build(Consumer<IFinishedRecipe> recipeSaver, ResourceLocation id)
+	public void build(Consumer<FinishedRecipe> recipeSaver, ResourceLocation id)
 	{
 		recipeSaver.accept(new Result(id, fallbackType));
 	}
 	
-	public static class Result implements IFinishedRecipe
+	public static class Result implements FinishedRecipe
 	{
 		private final ResourceLocation id;
-		private final IRecipeType<? extends AbstractCookingRecipe> fallbackType;
+		private final RecipeType<? extends AbstractCookingRecipe> fallbackType;
 		
-		public Result(ResourceLocation id, IRecipeType<? extends AbstractCookingRecipe> fallbackType)
+		public Result(ResourceLocation id, RecipeType<? extends AbstractCookingRecipe> fallbackType)
 		{
 			this.id = id;
 			this.fallbackType = fallbackType;
@@ -55,7 +55,7 @@ public class IrradiatingFallbackRecipeBuilder
 		}
 		
 		@Override
-		public IRecipeSerializer<?> getType()
+		public RecipeSerializer<?> getType()
 		{
 			return MSRecipeTypes.IRRADIATING_FALLBACK;
 		}
--- src/main/java/com/mraof/minestuck/data/recipe/NonMirroredRecipeBuilder.java
+++ src/main/java/com/mraof/minestuck/data/recipe/NonMirroredRecipeBuilder.java
@@ -2,44 +2,44 @@ package com.mraof.minestuck.data.recipe;
 
 import com.google.gson.JsonObject;
 import com.mraof.minestuck.item.crafting.MSRecipeTypes;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.ShapedRecipeBuilder;
-import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraft.util.IItemProvider;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.ItemLike;
 
 import javax.annotation.Nullable;
 import java.util.function.Consumer;
 
 public class NonMirroredRecipeBuilder extends ShapedRecipeBuilder
 {
-	public NonMirroredRecipeBuilder(IItemProvider result, int size)
+	public NonMirroredRecipeBuilder(ItemLike result, int size)
 	{
 		super(result, size);
 	}
 	
-	public static NonMirroredRecipeBuilder nonMirroredRecipe(IItemProvider result)
+	public static NonMirroredRecipeBuilder nonMirroredRecipe(ItemLike result)
 	{
 		return nonMirroredRecipe(result, 1);
 	}
 	
-	public static NonMirroredRecipeBuilder nonMirroredRecipe(IItemProvider result, int size)
+	public static NonMirroredRecipeBuilder nonMirroredRecipe(ItemLike result, int size)
 	{
 		return new NonMirroredRecipeBuilder(result, size);
 	}
 	
 	@Override
-	public void save(Consumer<IFinishedRecipe> recipeBuilder, ResourceLocation recipeName)
+	public void save(Consumer<FinishedRecipe> recipeBuilder, ResourceLocation recipeName)
 	{
 		super.save(recipe -> recipeBuilder.accept(new ResultWrapper(recipe)), recipeName);
 		
 	}
 	
-	private static class ResultWrapper implements IFinishedRecipe
+	private static class ResultWrapper implements FinishedRecipe
 	{
-		private final IFinishedRecipe shapedRecipe;
+		private final FinishedRecipe shapedRecipe;
 		
-		private ResultWrapper(IFinishedRecipe shapedRecipe)
+		private ResultWrapper(FinishedRecipe shapedRecipe)
 		{
 			this.shapedRecipe = shapedRecipe;
 		}
@@ -57,7 +57,7 @@ public class NonMirroredRecipeBuilder extends ShapedRecipeBuilder
 		}
 		
 		@Override
-		public IRecipeSerializer<?> getType()
+		public RecipeSerializer<?> getType()
 		{
 			return MSRecipeTypes.NON_MIRRORED;
 		}
