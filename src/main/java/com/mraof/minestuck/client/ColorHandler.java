0c005620151dfdf38e33d62dcff38b55e1373724
Dweblenod <ryanpgunnison@gmail.com>
2022-10-06 10:00:45 -0400

    Gave the two varieties of strawberry stems a plant render type in ClientProxy, added a method for handling stem color and added coloring to attached stem block in ColorHandler

--- src/main/java/com/mraof/minestuck/client/ColorHandler.java
+++ src/main/java/com/mraof/minestuck/client/ColorHandler.java
@@ -10,6 +10,7 @@ import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.client.color.item.ItemColors;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.StemBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ColorHandlerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -33,14 +34,21 @@ public class ColorHandler
     {
         BlockColors colors = event.getBlockColors();
         colors.register(new BlockColorCruxite(), MSBlocks.ALCHEMITER.TOTEM_PAD.get(), MSBlocks.TOTEM_LATHE.DOWEL_ROD.get(), MSBlocks.CRUXITE_DOWEL.get());
-        colors.register((state, worldIn, pos, tintIndex) ->
-        {
-            int age = state.getValue(StemBlock.AGE);
-            int red = age * 32;
-            int green = 255 - age * 8;
-            int blue = age * 4;
-            return red << 16 | green << 8 | blue;
-        }, MSBlocks.STRAWBERRY_STEM.get());
+        colors.register((state, worldIn, pos, tintIndex) -> stemColor(state), MSBlocks.STRAWBERRY_STEM.get());
+        colors.register((state, worldIn, pos, tintIndex) -> stemColor(state), MSBlocks.ATTACHED_STRAWBERRY_STEM.get());
+    }
+    
+    public static int stemColor(BlockState stemBlockState)
+    {
+        int age;
+        if(stemBlockState.getBlock() instanceof StemBlock)
+            age = stemBlockState.getValue(StemBlock.AGE);
+        else
+            age = 7; //assumed to be attached stem, equivalent to a fully grown stem block
+        int red = age * 32;
+        int green = 255 - age * 8;
+        int blue = age * 4;
+        return red << 16 | green << 8 | blue;
     }
 
     protected static class FrogItemColor implements ItemColor
