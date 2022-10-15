2daeb68c16431833e84a4c6dd747f59e7c58fd2a
kirderf1 <kirderf4@gmail.com>
2022-10-15 16:09:27 +0200

    Item supplier in ModusType

--- src/main/java/com/mraof/minestuck/inventory/captchalogue/ModusTypes.java
+++ src/main/java/com/mraof/minestuck/inventory/captchalogue/ModusTypes.java
@@ -19,12 +19,12 @@ public final class ModusTypes
 	@SuppressWarnings("unchecked")
 	public static final Supplier<IForgeRegistry<ModusType<?>>> REGISTRY = REGISTER.makeRegistry((Class<ModusType<?>>) (Object) ModusType.class, RegistryBuilder::new);
 	
-	public static final RegistryObject<ModusType<StackModus>> STACK = REGISTER.register("stack", () -> new ModusType<>(StackModus::new, MSItems.STACK_MODUS_CARD.get()));
-	public static final RegistryObject<ModusType<QueueModus>> QUEUE = REGISTER.register("queue", () -> new ModusType<>(QueueModus::new, MSItems.QUEUE_MODUS_CARD.get()));
-	public static final RegistryObject<ModusType<QueueStackModus>> QUEUE_STACK = REGISTER.register("queue_stack", () -> new ModusType<>(QueueStackModus::new, MSItems.QUEUESTACK_MODUS_CARD.get()));
-	public static final RegistryObject<ModusType<TreeModus>> TREE = REGISTER.register("tree", () -> new ModusType<>(TreeModus::new, MSItems.TREE_MODUS_CARD.get()));
-	public static final RegistryObject<ModusType<HashMapModus>> HASH_MAP = REGISTER.register("hash_map", () -> new ModusType<>(HashMapModus::new, MSItems.HASHMAP_MODUS_CARD.get()));
-	public static final RegistryObject<ModusType<SetModus>> SET = REGISTER.register("set", () -> new ModusType<>(SetModus::new, MSItems.SET_MODUS_CARD.get()));
+	public static final RegistryObject<ModusType<StackModus>> STACK = REGISTER.register("stack", () -> new ModusType<>(StackModus::new, MSItems.STACK_MODUS_CARD));
+	public static final RegistryObject<ModusType<QueueModus>> QUEUE = REGISTER.register("queue", () -> new ModusType<>(QueueModus::new, MSItems.QUEUE_MODUS_CARD));
+	public static final RegistryObject<ModusType<QueueStackModus>> QUEUE_STACK = REGISTER.register("queue_stack", () -> new ModusType<>(QueueStackModus::new, MSItems.QUEUESTACK_MODUS_CARD));
+	public static final RegistryObject<ModusType<TreeModus>> TREE = REGISTER.register("tree", () -> new ModusType<>(TreeModus::new, MSItems.TREE_MODUS_CARD));
+	public static final RegistryObject<ModusType<HashMapModus>> HASH_MAP = REGISTER.register("hash_map", () -> new ModusType<>(HashMapModus::new, MSItems.HASHMAP_MODUS_CARD));
+	public static final RegistryObject<ModusType<SetModus>> SET = REGISTER.register("set", () -> new ModusType<>(SetModus::new, MSItems.SET_MODUS_CARD));
 	
 	@Nullable
 	public static ModusType<?> getTypeFromItem(Item item)
