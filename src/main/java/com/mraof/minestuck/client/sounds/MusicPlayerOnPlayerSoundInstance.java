c28ab47c5687d2c8d991704e14b6a59f8e3f9ee7
LunaticCat <yannbrun1507@outlook.fr>
2022-10-31 14:22:46 +0100

    changed the soundInstance to extend entityBound

--- src/main/java/com/mraof/minestuck/client/sounds/MusicPlayerOnPlayerSoundInstance.java
+++ src/main/java/com/mraof/minestuck/client/sounds/MusicPlayerOnPlayerSoundInstance.java
@@ -1,6 +1,6 @@
 package com.mraof.minestuck.client.sounds;
 
-import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
+import net.minecraft.client.resources.sounds.EntityBoundSoundInstance;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.player.Player;
@@ -9,28 +9,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-public class MusicPlayerOnPlayerSoundInstance extends AbstractTickableSoundInstance
+public class MusicPlayerOnPlayerSoundInstance extends EntityBoundSoundInstance
 {
-	private final Player player;
-	private final SoundEvent soundEvent;
-	
 	private static Map<Integer, MusicPlayerOnPlayerSoundInstance> entitiesMap = new HashMap<>();
 	
 	public MusicPlayerOnPlayerSoundInstance(Player player, SoundEvent musicPlaying)
 	{
-		super(musicPlaying, SoundSource.PLAYERS);
-		this.looping = false;
-		this.delay = 0;
-		this.volume = 10.0F;
-		this.player = player;
-		this.soundEvent = musicPlaying;
-	}
-	
-	public void tick()
-	{
-			this.x = (float) this.player.getX();
-			this.y = (float) this.player.getY();
-			this.z = (float) this.player.getZ();
+		super(musicPlaying, SoundSource.PLAYERS, 10.0F, 0.0F, player);
 	}
 	
 	public static Map<Integer, MusicPlayerOnPlayerSoundInstance> getEntitiesMap()
