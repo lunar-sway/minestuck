fc9d91a6e9b45e3eba86f778a35790e1c93dd81e
LunaticCat <yannbrun1507@outlook.fr>
2022-11-23 12:42:29 +0100

    added javadoc to IMusicPlaying

--- src/main/java/com/mraof/minestuck/inventory/musicplayer/IMusicPlaying.java
+++ src/main/java/com/mraof/minestuck/inventory/musicplayer/IMusicPlaying.java
@@ -1,8 +1,15 @@
 package com.mraof.minestuck.inventory.musicplayer;
 
 import com.mraof.minestuck.block.EnumCassetteType;
-import net.minecraft.sounds.SoundEvent;
+import com.mraof.minestuck.item.weapon.MusicPlayerItem;
 import net.minecraft.world.item.ItemStack;
+
+/**
+ * This capability interface is meant to keep track of the current music being played,
+ * as well as which music player is currently playing it.
+ *
+ * @see MusicPlayerItem
+ */
 
 public interface IMusicPlaying
 {
