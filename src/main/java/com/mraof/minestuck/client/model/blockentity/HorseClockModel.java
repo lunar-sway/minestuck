28b2df5e70df6fe145de68405c8a38d3a5422d46
Dweblenod <ryanpgunnison@gmail.com>
2022-11-27 15:50:04 -0500

    Implemented model and texture changes by Riotmode, separated the model texture into versions for each clock facing

--- src/main/java/com/mraof/minestuck/client/model/blockentity/HorseClockModel.java
+++ src/main/java/com/mraof/minestuck/client/model/blockentity/HorseClockModel.java
@@ -3,6 +3,7 @@ package com.mraof.minestuck.client.model.blockentity;
 import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.blockentity.HorseClockBlockEntity;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class HorseClockModel extends AnimatedGeoModel<HorseClockBlockEntity> {
@@ -13,7 +14,29 @@ public class HorseClockModel extends AnimatedGeoModel<HorseClockBlockEntity> {
 
     @Override
     public ResourceLocation getTextureLocation(HorseClockBlockEntity object) {
-        return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock.png");
+        Level level = object.getLevel();
+        if(level != null)
+        {
+            int dayTime = (int) (level.getDayTime() % 24000);
+            if(dayTime < 3000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock0.png");
+            else if(dayTime < 6000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock1.png");
+            else if(dayTime < 9000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock2.png");
+            else if(dayTime < 12000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock3.png");
+            else if(dayTime < 15000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock4.png");
+            else if(dayTime < 18000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock5.png");
+            else if(dayTime < 21000)
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock6.png");
+            else
+                return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock7.png");
+        }
+        
+        return new ResourceLocation(Minestuck.MOD_ID, "textures/block/horseclock0.png");
     }
 
     @Override
