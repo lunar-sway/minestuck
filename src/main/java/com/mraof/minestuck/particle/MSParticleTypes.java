9814260819c6552f4aba8531fb1081944ba3ef84
KingBalDoro <59238634+KingBalDoro@users.noreply.github.com>
2022-12-01 11:29:04 -0500

    Update MSParticleTypes.java

--- src/main/java/com/mraof/minestuck/particle/MSParticleTypes.java
+++ src/main/java/com/mraof/minestuck/particle/MSParticleTypes.java
@@ -1,14 +1,9 @@
 package com.mraof.minestuck.particle;
 
 import com.mraof.minestuck.Minestuck;
-import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleType;
 import net.minecraft.core.particles.SimpleParticleType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -18,8 +13,6 @@ public class MSParticleTypes
 	public static final DeferredRegister<ParticleType<?>> REGISTER =
 		DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, Minestuck.MOD_ID);
 	
-		//particles.register(MSParticleType.FIREFLY.get(), FireflyParticle.Factory::new);
-		
 	public static final RegistryObject<SimpleParticleType> LowRollParticle =
 			REGISTER.register("LOW_ROLL", () -> new SimpleParticleType(true));
 	
@@ -40,8 +33,9 @@ public class MSParticleTypes
 	
 	public static final RegistryObject<SimpleParticleType> CaegerScratchParticle =
 			REGISTER.register("CAEGER_SCRATCH", () -> new SimpleParticleType(true));
-		
-		@OnlyIn(Dist.CLIENT) @SubscribeEvent
-		public static void registerFactories(ParticleFactoryRegisterEvent event) {
-			ParticleManager particles = Minecraft.getInstance().particleEngine;}
+
+	public static void register(IEventBus eventBus)
+	{
+		REGISTER.register(eventBus);
+	}
 }
