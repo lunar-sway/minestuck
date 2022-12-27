946d1b47147cbb01d8595f8023e73f6de3ea38fa
Dweblenod <ryanpgunnison@gmail.com>
2022-12-27 10:54:44 -0500

    Added item textures that were finished by Riotmode, moved the amphibeanie model up farther on the head to prevent clipping

--- src/main/java/com/mraof/minestuck/client/model/armor/AmphibeanieModel.java
+++ src/main/java/com/mraof/minestuck/client/model/armor/AmphibeanieModel.java
@@ -20,7 +20,7 @@ public class AmphibeanieModel
 				.texOffs(0, 0).addBox(-4.5F, -3.25F, -4.5F, 9.0F, 4.0F, 9.0F)
 				.texOffs(0, 13).addBox(-5.5F, -6.25F, -1.0F, 4.0F, 3.0F, 2.0F)
 				.texOffs(0, 13).addBox(1.5F, -6.25F, -1.0F, 4.0F, 3.0F, 2.0F),
-				PartPose.offsetAndRotation(0.0F, -5.0F, 0.0F, -0.0873F, 0.0F, 0.0F));
+				PartPose.offsetAndRotation(0.0F, -5.25F, 0.0F, -0.0873F, 0.0F, 0.0F));
 		
 		PartDefinition leftpigtail = beanie.addOrReplaceChild("leftpigtail", CubeListBuilder.create()
 				.texOffs(9, 17).addBox(-1.3867F, -2.1354F, 1.0285F, 3.0F, 3.0F, 4.0F)
