503e32da36974edd78560e217783c3bc38ff1a4d
KingBalDoro <59238634+KingBalDoro@users.noreply.github.com>
2023-02-13 23:34:37 -0500

    Doro's ores (#417)
    * Updating the ice shard item texture(updates by Riotmode, touched up by rose_bushes_)
    
    * uranium, cruxite addition- 2/12/2023
    
    mycelium cru, ura ore additions
    
    * review 1
    
    * review 1_2
    
    * review 2
    
    ---------
    
    Co-authored-by: Dweblenod <ryanpgunnison@gmail.com>
    Co-authored-by: Dweblenod <63124354+Dweblenod@users.noreply.github.com>

--- src/main/java/com/mraof/minestuck/world/lands/terrain/FungiLandType.java
+++ src/main/java/com/mraof/minestuck/world/lands/terrain/FungiLandType.java
@@ -69,6 +69,8 @@ public class FungiLandType extends TerrainLandType
 		registry.setBlock("bush", Blocks.BROWN_MUSHROOM);
 		registry.setBlock("structure_wool_1", Blocks.LIME_WOOL);
 		registry.setBlock("structure_wool_3", Blocks.GRAY_WOOL);
+		registry.setBlock("cruxite_ore", MSBlocks.MYCELIUM_STONE_CRUXITE_ORE);
+		registry.setBlock("uranium_ore", MSBlocks.MYCELIUM_STONE_URANIUM_ORE);
 	}
 	
 	@Override
@@ -101,14 +103,6 @@ public class FungiLandType extends TerrainLandType
 		builder.addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, PlacementUtils.inlinePlaced(Feature.ORE,
 						new OreConfiguration(blocks.getGroundType(), Blocks.GRAVEL.defaultBlockState(), 33),
 						CountPlacement.of(10), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(256)), BiomeFilter.biome()),
-				LandBiomeType.any());
-		builder.addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, PlacementUtils.inlinePlaced(Feature.ORE,
-						new OreConfiguration(blocks.getGroundType(), Blocks.IRON_ORE.defaultBlockState(), 9),
-						CountPlacement.of(48), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(64)), BiomeFilter.biome()),
-				LandBiomeType.any());
-		builder.addFeature(GenerationStep.Decoration.UNDERGROUND_ORES, PlacementUtils.inlinePlaced(Feature.ORE,
-						new OreConfiguration(blocks.getGroundType(), Blocks.REDSTONE_ORE.defaultBlockState(), 8),
-						CountPlacement.of(36), InSquarePlacement.spread(), HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(32)), BiomeFilter.biome()),
 				LandBiomeType.any());
 	}
 	
