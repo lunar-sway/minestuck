2be1167f00f4c3b054b3ed2b6813016e32c75d18
kirderf1 <kirderf4@gmail.com>
2023-03-24 16:01:55 +0100

    Clean up grist gutter implementation a bit

--- src/main/java/com/mraof/minestuck/alchemy/GristGutter.java
+++ src/main/java/com/mraof/minestuck/alchemy/GristGutter.java
@@ -14,10 +14,6 @@ import net.minecraft.world.level.Level;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.Map;
 
 /**
  * A class that handles Grist overflow whenever you aqcuire too much grist.
@@ -26,26 +22,17 @@ import java.util.Map;
 @Mod.EventBusSubscriber(modid = Minestuck.MOD_ID, bus=Mod.EventBusSubscriber.Bus.FORGE)
 public class GristGutter
 {
-	private static final Logger LOGGER = LogManager.getLogger();
 	public static final int GUTTER_CAPACITY = 10000;
 	
 	private final GristSet gristSet = new GristSet();
-	public int gutterTotal = -1;
+	private long gutterTotal = 0;
 	
-	public static double getGutterCapacity(Session session)
+	public static long getGutterCapacity(Session session)
 	{
-		return (GUTTER_CAPACITY * session.getGutterMultiplier());
+		return (long) (GUTTER_CAPACITY * session.getGutterMultiplier());
 	}
 	public long getGutterTotal()
 	{
-		if(gutterTotal < 0)
-		{
-			gutterTotal = 0;
-			for(Map.Entry<GristType, Long> pair : gristSet.getMap().entrySet())
-			{
-				gutterTotal += pair.getValue();
-			}
-		}
 		return gutterTotal;
 	}
 	public GristSet gristToSpill = new GristSet();
@@ -63,44 +50,24 @@ public class GristGutter
 	
 	public void addGrist(GristType type, long amount, Session session)
 	{
-		GristSet sOverflowGrist = new GristSet();//creates a new gristset called Super overflow
-		long originalAmount = gristSet.getMap().getOrDefault(type, 0L);
-		long maximumAllowed = (long) (gutterTotal - getGutterCapacity(session) + originalAmount);
+		long maximumAllowed = gutterTotal - getGutterCapacity(session);
 		
-		gristSet.getMap().compute(type, (key, value) -> value == null ? amount : value + amount);
-		gutterTotal += amount;//adds grist to gutter
+		this.addGristInternal(type, Math.min(maximumAllowed, amount));
 		
-		//logger
-		LOGGER.debug("Gutter after adding " + amount + " " + type.getDisplayName().toString() + " grist:");
-		LOGGER.debug("Total: " + getGutterTotal());
-		for(GristType t : gristSet.getMap().keySet())
-		{
-			LOGGER.debug(t.getDisplayName().toString() + ": " + gristSet.getMap().get(t));
-		}
-		logGutter(type, amount);
+		long remaining = amount - maximumAllowed;
 		
-		//not used by anything currently
-		if(gutterTotal > GUTTER_CAPACITY)
-		{
-			System.out.println("gutter has capped out");
-			long sOverflowAmount = (long) (gutterTotal - getGutterCapacity(session));
-			gristSet.getMap().put(type, maximumAllowed);
-			sOverflowGrist.addGrist(type, sOverflowAmount);
-			gristToSpill.addGrist(sOverflowGrist);
-			gutterTotal -= sOverflowAmount;
-		}
+		if(remaining > 0)
+			gristToSpill.addGrist(type, remaining);
 	}
-	public GristGutter logGutter(GristType type, long amount)
+	
+	/**
+	 * The grist set is currently only modified here,
+	 * which lets us be certain that gutterTotal is accurate.
+	 */
+	private void addGristInternal(GristType type, long amount)
 	{
-		LOGGER.debug("Gutter after adding " + amount + " " + type.getDisplayName().toString() + " grist:");
-		LOGGER.debug("Total: " + getGutterTotal());
-		
-		for(GristType t : gristSet.getMap().keySet())
-		{
-			LOGGER.debug(t.getDisplayName().toString() + ": " + gristSet.getMap().get(t));
-		}
-		
-		return this;
+		this.gristSet.addGrist(type, amount);
+		this.gutterTotal += amount;
 	}
 	
 	/**
@@ -110,14 +77,14 @@ public class GristGutter
 	{
 		GristSet spliceSet = new GristSet();
 		
-		for(GristType t : gristSet.getMap().keySet())
+		for(GristAmount gristAmount : gristSet.getAmounts())
 		{
-			long xMover = Math.max(Math.min(gristSet.getMap().get(t), i), 0);
-			spliceSet.addGrist(t, xMover);//spliceSet calls addgrist with the amount and type specified
+			long xMover = Math.max(Math.min(gristAmount.getAmount(), i), 0);
+			spliceSet.addGrist(gristAmount.getType(), xMover);
 			
-			gutterTotal -= xMover;//takes grist from the gutter
+			this.addGristInternal(gristAmount.getType(), -xMover);
 		}
-		return spliceSet;//returns splice set to be used to distribute itself to the player's caches
+		return spliceSet;
 	}
 	
 	@SubscribeEvent
