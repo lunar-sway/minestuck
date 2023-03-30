33122da1301bdd61b9b67e02795f4fd7a45f9a6a
Dweblenod <ryanpgunnison@gmail.com>
2023-03-30 17:00:26 -0400

    Switched to overriding getToolModifiedState in StrippableFlammableLogBlock instead of the use method

--- src/main/java/com/mraof/minestuck/block/plant/StrippableFlammableLogBlock.java
+++ src/main/java/com/mraof/minestuck/block/plant/StrippableFlammableLogBlock.java
@@ -1,19 +1,10 @@
 package com.mraof.minestuck.block.plant;
 
-import net.minecraft.advancements.CriteriaTriggers;
-import net.minecraft.core.BlockPos;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.gameevent.GameEvent;
-import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.common.ToolAction;
 import net.minecraftforge.common.ToolActions;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Supplier;
 
@@ -27,30 +18,13 @@ public class StrippableFlammableLogBlock extends FlammableLogBlock
 		this.strippedState = strippedState;
 	}
 	
+	@Nullable
 	@Override
-	@SuppressWarnings("deprecation")
-	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit)
+	public BlockState getToolModifiedState(BlockState state, UseOnContext context, ToolAction toolAction, boolean simulate)
 	{
-		ItemStack itemStack = player.getItemInHand(hand);
-		
-		if(itemStack.canPerformAction(ToolActions.AXE_STRIP))
-		{
-			if(player instanceof ServerPlayer)
-			{
-				CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, pos, itemStack);
-			}
-			
-			level.playSound(player, pos, SoundEvents.AXE_STRIP, SoundSource.BLOCKS, 1.0F, 1.0F);
-			
-			level.setBlock(pos, strippedState.get().setValue(AXIS, state.getValue(StrippableFlammableLogBlock.AXIS)), 11);
-			level.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(player, state));
-			itemStack.hurtAndBreak(1, player, (playerEntity) -> {
-				playerEntity.broadcastBreakEvent(hand);
-			});
-			
-			return InteractionResult.sidedSuccess(level.isClientSide);
-		}
-		
-		return InteractionResult.PASS;
+		if(toolAction == ToolActions.AXE_STRIP && context.getItemInHand().canPerformAction(ToolActions.AXE_STRIP))
+			return this.strippedState.get();
+		else
+			return super.getToolModifiedState(state, context, toolAction, simulate);
 	}
 }
