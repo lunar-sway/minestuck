324e361afc1c35090f678d4835cafe0034c3651b
seriousCoolness <wyattrobert42@gmail.com>
2023-04-04 12:37:48 -0400

    Update src/main/java/com/mraof/minestuck/network/EditmodeFillPacket.java
    Co-authored-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/network/EditmodeFillPacket.java
+++ src/main/java/com/mraof/minestuck/network/EditmodeFillPacket.java
@@ -346,7 +346,7 @@ public final class EditmodeFillPacket
 		@Override
 		public void execute(ServerPlayer player)
 		{
-			IEditTools cap = player.getCapability(MSCapabilities.EDIT_TOOLS_CAPABILITY).orElseThrow(() -> new IllegalStateException("EditTool Capability is empty on player " + player.getDisplayName().toString() + " on server-side (during packet execution)!"));
+			IEditTools cap = player.getCapability(MSCapabilities.EDIT_TOOLS_CAPABILITY).orElseThrow(() -> new IllegalStateException("EditTool Capability is missing on player " + player.getDisplayName().getString() + " on server-side (during packet execution)!"));
 			
 			ServerEditHandler.removeCursorEntity(player, true);
 			cap.resetDragTools();
