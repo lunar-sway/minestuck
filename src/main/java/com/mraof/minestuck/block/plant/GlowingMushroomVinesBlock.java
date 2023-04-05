b1fd5088809c29ce52b91015aa8ed9aa4e0e0dc2
Dweblenod <ryanpgunnison@gmail.com>
2023-04-05 09:10:45 -0400

    Fixing issue with glowing vine survival

--- src/main/java/com/mraof/minestuck/block/plant/GlowingMushroomVinesBlock.java
+++ src/main/java/com/mraof/minestuck/block/plant/GlowingMushroomVinesBlock.java
@@ -2,11 +2,14 @@ package com.mraof.minestuck.block.plant;
 
 import com.mraof.minestuck.util.MSTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
@@ -21,6 +24,7 @@ public class GlowingMushroomVinesBlock extends Block
 		super(properties);
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource rand)
 	{
@@ -31,6 +35,7 @@ public class GlowingMushroomVinesBlock extends Block
 		}
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public boolean canSurvive(BlockState state, LevelReader level, BlockPos pos)
 	{
@@ -38,10 +43,20 @@ public class GlowingMushroomVinesBlock extends Block
 		return aboveState.is(this) || aboveState.is(MSTags.Blocks.SHADEWOOD_LEAVES);
 	}
 	
+	@SuppressWarnings("deprecation")
 	@Override
 	public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext)
 	{
 		Vec3 vec3 = pState.getOffset(pLevel, pPos);
 		return SHAPE.move(vec3.x, vec3.y, vec3.z);
+	}
+	
+	@SuppressWarnings("deprecation")
+	@Override
+	public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos)
+	{
+		return direction == Direction.UP && !this.canSurvive(state, level, currentPos)
+				? Blocks.AIR.defaultBlockState()
+				: super.updateShape(state, direction, neighborState, level, currentPos, neighborPos);
 	}
 }
