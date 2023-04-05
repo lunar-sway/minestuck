479c9b4cb31c91bef9732d42351e656bdb8eacd0
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-04-05 00:16:45 -0400

    better icons and computer gui

--- src/main/java/com/mraof/minestuck/client/gui/ComputerScreen.java
+++ src/main/java/com/mraof/minestuck/client/gui/ComputerScreen.java
@@ -31,7 +31,7 @@ public class ComputerScreen extends Screen
 	public static final int ySize = 166;
 	
 	private final List<ComputerIcon> icons;
-	private XButton xButton;
+	private PowerButton powerButton;
 	public final ComputerBlockEntity be;
 	public ComputerProgram program;
 	
@@ -51,7 +51,7 @@ public class ComputerScreen extends Screen
 	{
 		super.init();
 		genIcons();
-		xButton = addRenderableWidget(new XButton());
+		powerButton = addRenderableWidget(new PowerButton());
 		setProgram(be.programSelected);
 	}
 	
@@ -68,8 +68,6 @@ public class ComputerScreen extends Screen
 		RenderSystem.setShaderColor(1, 1, 1, 1);
 		RenderSystem.setShaderTexture(0, bsod ? guiBsod : guiBackground);
 		blit(poseStack, xOffset, yOffset, 0, 0, xSize, ySize);
-		// Sburb logo
-		blit(poseStack, xOffset+7, yOffset+6, 0, ySize, 77, 26);
 		
 		if(!bsod)
 		{
@@ -80,10 +78,15 @@ public class ComputerScreen extends Screen
 			if(program != null) program.paintGui(poseStack, this, be);
 			
 			super.render(poseStack, mouseX, mouseY, partialTicks);
+			
+			//corner bits (goes on top of computer screen slightly)
+			RenderSystem.setShaderTexture(0, guiBackground);
+			blit(poseStack, xOffset+9, yOffset+38, xSize, 0, 3, 3);
+			blit(poseStack, xOffset+164, yOffset+38, xSize+3, 0, 3, 3);
 		}
 	}
 	
-	private void xButton(Button b)
+	private void exit(Button b)
 	{
 		if(program == null)
 			minecraft.setScreen(null);
@@ -104,7 +107,10 @@ public class ComputerScreen extends Screen
 		// TODO WRAP ONCE LONG ENOUGH
 		for(Entry<Integer, Boolean> entry : be.installedPrograms.entrySet())
 			if(entry.getValue())
-				icons.add( addRenderableWidget(new ComputerIcon(xOffset+15, yOffset+44 + entry.getKey()*20, entry.getKey())) );
+				icons.add( addRenderableWidget(new ComputerIcon(
+						xOffset+15 + Math.floorDiv(entry.getKey(), 5)*20,
+						yOffset+44 + entry.getKey()%5 *20,
+						entry.getKey())));
 	}
 	
 	protected void setProgram(int id)
@@ -134,7 +140,7 @@ public class ComputerScreen extends Screen
 		icons.clear();
 		
 		genIcons();
-		addRenderableWidget(xButton);
+		addRenderableWidget(powerButton);
 		
 		updateGui();
 	}
@@ -190,21 +196,15 @@ public class ComputerScreen extends Screen
 		}
 	}
 	
-	private class XButton extends ExtendedButton
+	private class PowerButton extends Button
 	{
-		
-		public XButton()
+		public PowerButton()
 		{
-			super((ComputerScreen.this.width-xSize)/2 +153, (ComputerScreen.this.height-ySize)/2 +7, 16, 16, Component.empty(), ComputerScreen.this::xButton);
+			super((ComputerScreen.this.width-xSize)/2 +143, (ComputerScreen.this.height-ySize)/2 +3,
+					29, 29, Component.empty(), ComputerScreen.this::exit);
 		}
 		
 		@Override
-		public void renderButton(PoseStack poseStack, int mouseX, int mouseY, float partialTick)
-		{
-			RenderSystem.setShader(GameRenderer::getPositionTexShader);
-			RenderSystem.setShaderColor(1, 1, 1, 1);
-			RenderSystem.setShaderTexture(0, be.theme.getTexture());
-			blit(poseStack, x, y, 158, 40, 20, 20);
-		}
+		public void render(PoseStack poseStack, int mouseX, int mouseY, float pt) {}
 	}
 }
