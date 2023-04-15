156d8a6883908eb37416e73eec371bc2de044d75
Dweblenod <ryanpgunnison@gmail.com>
2023-04-15 18:25:15 -0400

    Creating Title-Subtitle style Land Entry message

--- src/main/java/com/mraof/minestuck/data/MinestuckEnUsLanguageProvider.java
+++ src/main/java/com/mraof/minestuck/data/MinestuckEnUsLanguageProvider.java
@@ -1699,7 +1699,8 @@ public class MinestuckEnUsLanguageProvider extends MinestuckLanguageProvider
 		addLand(MonstersLandType.MONSTERS, "Monsters");
 		addLand(TowersLandType.TOWERS, "Towers");
 		add(LandTypePair.FORMAT, "Land of %s and %s");
-		add(SburbHandler.LAND_ENTRY, "Welcome to %s.");
+		add(SburbHandler.CHAT_LAND_ENTRY, "Welcome to %s.");
+		add(SburbHandler.TITLE_LAND_ENTRY, "%s");
 		
 		addEffect(MSEffects.CREATIVE_SHOCK, "Creative Shock");
 		
