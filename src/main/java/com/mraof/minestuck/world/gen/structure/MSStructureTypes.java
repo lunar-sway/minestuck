57070a3204e630efc1132ed9098fa91e75fbfd97
kirderf1 <kirderf4@gmail.com>
2023-06-20 17:43:04 +0200

    Helper function for structure types for better readability

--- src/main/java/com/mraof/minestuck/world/gen/structure/MSStructureTypes.java
+++ src/main/java/com/mraof/minestuck/world/gen/structure/MSStructureTypes.java
@@ -1,10 +1,12 @@
 package com.mraof.minestuck.world.gen.structure;
 
+import com.mojang.serialization.Codec;
 import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.world.gen.structure.castle.CastleStructure;
 import com.mraof.minestuck.world.gen.structure.gate.GateStructure;
 import com.mraof.minestuck.world.gen.structure.village.ConsortVillageStructure;
 import net.minecraft.core.Registry;
+import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,14 +16,19 @@ public final class MSStructureTypes
 	public static final DeferredRegister<StructureType<?>> REGISTER = DeferredRegister.create(Registry.STRUCTURE_TYPE_REGISTRY, Minestuck.MOD_ID);
 	
 	// Overworld
-	public static final RegistryObject<StructureType<FrogTempleStructure>> FROG_TEMPLE = REGISTER.register("frog_temple", () -> () -> FrogTempleStructure.CODEC);
+	public static final RegistryObject<StructureType<FrogTempleStructure>> FROG_TEMPLE = REGISTER.register("frog_temple", () -> asType(FrogTempleStructure.CODEC));
 	
 	// Land
-	public static final RegistryObject<StructureType<GateStructure>> LAND_GATE = REGISTER.register("land_gate", () -> () -> GateStructure.CODEC);
-	public static final RegistryObject<StructureType<SmallRuinStructure>> SMALL_RUIN = REGISTER.register("small_ruin", () -> () -> SmallRuinStructure.CODEC);
-	public static final RegistryObject<StructureType<ImpDungeonStructure>> IMP_DUNGEON = REGISTER.register("imp_dungeon", () -> () -> ImpDungeonStructure.CODEC);
-	public static final RegistryObject<StructureType<ConsortVillageStructure>> CONSORT_VILLAGE = REGISTER.register("consort_village", () -> () -> ConsortVillageStructure.CODEC);
+	public static final RegistryObject<StructureType<GateStructure>> LAND_GATE = REGISTER.register("land_gate", () -> asType(GateStructure.CODEC));
+	public static final RegistryObject<StructureType<SmallRuinStructure>> SMALL_RUIN = REGISTER.register("small_ruin", () -> asType(SmallRuinStructure.CODEC));
+	public static final RegistryObject<StructureType<ImpDungeonStructure>> IMP_DUNGEON = REGISTER.register("imp_dungeon", () -> asType(ImpDungeonStructure.CODEC));
+	public static final RegistryObject<StructureType<ConsortVillageStructure>> CONSORT_VILLAGE = REGISTER.register("consort_village", () -> asType(ConsortVillageStructure.CODEC));
 	
 	// Skaia
-	public static final RegistryObject<StructureType<CastleStructure>> SKAIA_CASTLE = REGISTER.register("skaia_castle", () -> () -> CastleStructure.CODEC);
+	public static final RegistryObject<StructureType<CastleStructure>> SKAIA_CASTLE = REGISTER.register("skaia_castle", () -> asType(CastleStructure.CODEC));
+	
+	private static <S extends Structure> StructureType<S> asType(Codec<S> codec)
+	{
+		return () -> codec;
+	}
 }
