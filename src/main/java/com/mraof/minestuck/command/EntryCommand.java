c3f0b70d82934303d8b58090aa2f5ad2cb2a49b9
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2023-07-01 00:10:35 -0400

    clean up command builder code

--- src/main/java/com/mraof/minestuck/command/EntryCommand.java
+++ src/main/java/com/mraof/minestuck/command/EntryCommand.java
@@ -6,48 +6,33 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.EntityArgument;
 import net.minecraft.commands.arguments.coordinates.BlockPosArgument;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
 
 public class EntryCommand
 {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher)
 	{
-		dispatcher.register(
-				Commands.literal("enter")
-						.requires(c -> c.hasPermission(Commands.LEVEL_GAMEMASTERS))
-						.then(
-								Commands.argument("player", EntityArgument.player())
-										.then(
-												Commands.argument("pos", BlockPosArgument.blockPos())
-														.executes(ctx -> {
-															// both player and position specified
-															EntryProcess.onArtifactActivated(
-																	EntityArgument.getPlayer(ctx, "player"),
-																	BlockPosArgument.getLoadedBlockPos(ctx, "pos")
-															);
-															return 0;
-														})
-										)
-										.executes(ctx -> {
-											// some other player at their position
-											EntryProcess.onArtifactActivated(EntityArgument.getPlayer(ctx, "player"));
-											return 0;
-										})
-						)
-						.then(
-								Commands.argument("pos", BlockPosArgument.blockPos())
-										.executes(ctx -> {
-											// position and self
-											EntryProcess.onArtifactActivated(
-													ctx.getSource().getPlayerOrException(),
-													BlockPosArgument.getLoadedBlockPos(ctx, "pos"));
-											return 0;
-										})
-						)
-						.executes(ctx -> {
-							// no arguments, just the self
-							EntryProcess.onArtifactActivated(ctx.getSource().getPlayerOrException());
-							return 0;
-						})
+		dispatcher.register(Commands.literal("enter").requires(c -> c.hasPermission(Commands.LEVEL_GAMEMASTERS))
+				.then(Commands.argument("player", EntityArgument.player())
+								.then(Commands.argument("pos", BlockPosArgument.blockPos())
+										.executes(ctx -> enter(EntityArgument.getPlayer(ctx, "player"), BlockPosArgument.getLoadedBlockPos(ctx, "pos"))))
+								.executes(ctx -> enter(EntityArgument.getPlayer(ctx, "player"))))
+				.then(Commands.argument("pos", BlockPosArgument.blockPos())
+						.executes(ctx -> enter(ctx.getSource().getPlayerOrException(), BlockPosArgument.getLoadedBlockPos(ctx, "pos"))))
+				.executes(ctx -> enter(ctx.getSource().getPlayerOrException()))
 		);
+	}
+	
+	private static int enter(ServerPlayer player)
+	{
+		EntryProcess.onArtifactActivated(player);
+		return 0;
+	}
+	
+	private static int enter(ServerPlayer player, BlockPos pos)
+	{
+		EntryProcess.onArtifactActivated(player, pos);
+		return 0;
 	}
 }
