0062de67a1c58e5f7f70593553b45c1b6625d031
GideonHead <129314523+GideonHead@users.noreply.github.com>
2023-07-09 14:48:27 -0500

    added datagen for item and block models, and blockstates. (#491)
    * added datagen for item and block models, and blockstates.
    
    * Update src/main/java/com/mraof/minestuck/data/MinestuckData.java
    
    Co-authored-by: kirderf1 <kirderf4@gmail.com>
    
    * reformatted
    
    * reformatted
    
    * reformatted
    
    * auto-reformatted
    
    ---------
    
    Co-authored-by: Zepsun <51090210+SputterSizzle@users.noreply.github.com>
    Co-authored-by: kirderf1 <kirderf4@gmail.com>

---
+++ src/main/java/com/mraof/minestuck/data/MinestuckBlockStateProvider.java
@@ -0,0 +1,27 @@
+package com.mraof.minestuck.data;
+
+import com.mraof.minestuck.Minestuck;
+import com.mraof.minestuck.block.MSBlocks;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class MinestuckBlockStateProvider extends BlockStateProvider
+{
+	public MinestuckBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper)
+	{
+		super(gen, Minestuck.MOD_ID, exFileHelper);
+	}
+	
+	@Override
+	protected void registerStatesAndModels()
+	{
+		
+		simpleBlock(MSBlocks.UNCARVED_WOOD.get());
+		
+	}
+}
