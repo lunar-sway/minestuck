691e94b17a30f1566bad5cce1c2480086460fa06
Zepsun <51090210+SputterSizzle@users.noreply.github.com>
2023-07-23 15:04:06 -0500

    optomized generation of block items

--- src/main/java/com/mraof/minestuck/data/MinestuckBlockStateProvider.java
+++ src/main/java/com/mraof/minestuck/data/MinestuckBlockStateProvider.java
@@ -3,8 +3,13 @@ package com.mraof.minestuck.data;
 import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.block.MSBlocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.client.model.generators.ConfiguredModel;
+import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class MinestuckBlockStateProvider extends BlockStateProvider
 {
@@ -17,7 +22,28 @@ public class MinestuckBlockStateProvider extends BlockStateProvider
 	protected void registerStatesAndModels()
 	{
 		
-		simpleBlock(MSBlocks.UNCARVED_WOOD.get());
+		blockWithItem(MSBlocks.UNCARVED_WOOD.get());
 		
+	}
+	
+	private void blockWithItem(Block block)
+	{
+		simpleBlock(block, new ConfiguredModel(cubeAll(block)));
+		simpleBlockItem(block, cubeAll(block));
+	}
+	
+	public void simpleBlockItem(Block block, ModelFile model)
+	{
+		itemModels().getBuilder(key(block).getPath()).parent(model);
+	}
+	
+	public void simpleBlock(Block block, ConfiguredModel... models)
+	{
+		getVariantBuilder(block).partialState().setModels(models);
+	}
+	
+	private ResourceLocation key(Block block)
+	{
+		return ForgeRegistries.BLOCKS.getKey(block);
 	}
 }
