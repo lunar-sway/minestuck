6ae46680abed8d4a790c44adb0291692838b1d43
kirderf1 <kirderf4@gmail.com>
2023-08-13 09:06:19 +0200

    Add back null checks to JeiGristCost constructors

--- src/main/java/com/mraof/minestuck/api/alchemy/recipe/JeiGristCost.java
+++ src/main/java/com/mraof/minestuck/api/alchemy/recipe/JeiGristCost.java
@@ -1,8 +1,12 @@
 package com.mraof.minestuck.api.alchemy.recipe;
 
 import com.mraof.minestuck.api.alchemy.ImmutableGristSet;
+import net.minecraft.FieldsAreNonnullByDefault;
 import net.minecraft.world.item.crafting.Ingredient;
 
+import java.util.Objects;
+
+@FieldsAreNonnullByDefault
 public sealed interface JeiGristCost permits JeiGristCost.Set, JeiGristCost.Wildcard
 {
 	String GRIST_COSTS = "minestuck.jei.grist_costs";
@@ -10,8 +14,19 @@ public sealed interface JeiGristCost permits JeiGristCost.Set, JeiGristCost.Wild
 	Ingredient ingredient();
 	
 	record Set(Ingredient ingredient, ImmutableGristSet gristSet) implements JeiGristCost
-	{}
+	{
+		public Set
+		{
+			Objects.requireNonNull(ingredient);
+			Objects.requireNonNull(gristSet);
+		}
+	}
 	
 	record Wildcard(Ingredient ingredient, long wildcardAmount) implements JeiGristCost
-	{}
+	{
+		public Wildcard
+		{
+			Objects.requireNonNull(ingredient);
+		}
+	}
 }
