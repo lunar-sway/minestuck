27a4e405ac7d30029cf0fcf028ff05d400e117ee
Dweblenod <ryanpgunnison@gmail.com>
2023-09-23 18:59:57 -0400

    - Prevented server side crashes from fake players injuring underlings. Rearranged code remains untested in a server setting

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/).
 
 ## [Unreleased]
 
+### Fixed
+
+- Fake players no longer crash the game when injuring underlings
+
 ## [1.19.2-1.11.0.0] - 2023-07-22
 
 ### Added
--- src/main/java/com/mraof/minestuck/event/ServerEventHandler.java
+++ src/main/java/com/mraof/minestuck/event/ServerEventHandler.java
@@ -24,6 +24,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.monster.*;
@@ -142,20 +143,23 @@ public class ServerEventHandler
 	{
 		if(event.getSource().getEntity() != null)
 		{
-			if (event.getSource().getEntity() instanceof ServerPlayer)
+			Entity attacker = event.getSource().getEntity();
+			Entity injured = event.getEntity();
+			
+			if(injured != null)
 			{
-				ServerPlayer player = (ServerPlayer) event.getSource().getEntity();
-				if (event.getEntity() instanceof UnderlingEntity)
-				{    //Increase damage to underling
-					double modifier = PlayerSavedData.getData(player).getEcheladder().getUnderlingDamageModifier();
+				boolean attackerIsRealPlayer = attacker instanceof Player && !(attacker instanceof FakePlayer);
+				
+				if(attackerIsRealPlayer && injured instanceof UnderlingEntity)
+				{
+					//Increase damage to underling
+					double modifier = PlayerSavedData.getData((ServerPlayer) attacker).getEcheladder().getUnderlingDamageModifier();
 					event.setAmount((float) (event.getAmount() * modifier));
-				}
-			}
-			else if (event.getEntity() instanceof ServerPlayer && event.getSource().getEntity() instanceof UnderlingEntity)
-			{    //Decrease damage to player
-				ServerPlayer player = (ServerPlayer) event.getEntity();
+				} else if (injured instanceof ServerPlayer player && attacker instanceof UnderlingEntity)
+				{    //Decrease damage to player
 					double modifier = PlayerSavedData.getData(player).getEcheladder().getUnderlingProtectionModifier();
 					event.setAmount((float) (event.getAmount() * modifier));
+				}
 			}
 		}
 	}
