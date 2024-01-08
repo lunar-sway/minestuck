5d342a25c2e0f0a64badf51d978ae52da87559e5
InfectedGriffon <79546684+InfectedGriffon@users.noreply.github.com>
2024-01-08 05:07:01 -0500

    Dispenser behaviours for barbasol bombs and minestuck buckets (#546)

--- src/main/java/com/mraof/minestuck/item/BarbasolBombItem.java
+++ src/main/java/com/mraof/minestuck/item/BarbasolBombItem.java
@@ -2,21 +2,33 @@ package com.mraof.minestuck.item;
 
 import com.mraof.minestuck.entity.MSEntityTypes;
 import com.mraof.minestuck.entity.item.BarbasolBombEntity;
+import net.minecraft.core.Position;
+import net.minecraft.core.dispenser.AbstractProjectileDispenseBehavior;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Projectile;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.DispenserBlock;
 
 public class BarbasolBombItem extends Item
 {
 	public BarbasolBombItem(Properties properties)
 	{
 		super(properties);
+		DispenserBlock.registerBehavior(this, new AbstractProjectileDispenseBehavior()
+		{
+			@Override
+			protected Projectile getProjectile(Level level, Position pos, ItemStack stack)
+			{
+				return new BarbasolBombEntity(MSEntityTypes.BARBASOL_BOMB.get(), pos.x(), pos.y(), pos.z(), level);
+			}
+		});
 	}
 	
 	@Override
