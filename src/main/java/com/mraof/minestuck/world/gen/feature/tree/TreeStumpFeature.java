7b3300ca43ee40acbb766d5973da028f7f53ec7f
kirderf1 <kirderf4@gmail.com>
2024-02-01 12:11:12 +0100

    For tree stump y placement, use height of center pos instead of corner pos

--- src/main/java/com/mraof/minestuck/world/gen/feature/tree/TreeStumpFeature.java
+++ src/main/java/com/mraof/minestuck/world/gen/feature/tree/TreeStumpFeature.java
@@ -3,6 +3,7 @@ package com.mraof.minestuck.world.gen.feature.tree;
 import com.mojang.serialization.Codec;
 import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.world.gen.feature.TemplatePlacement;
+import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
@@ -30,12 +31,13 @@ public class TreeStumpFeature extends Feature<NoneFeatureConfiguration>
 		WorldGenLevel level = context.level();
 		RandomSource rand = context.random();
 		StructureTemplate template = level.getLevel().getStructureManager().getOrCreate(TREE_STUMP);
-		TemplatePlacement placement = TemplatePlacement.centeredWithRandomRotation(template, context.origin(), rand);
+		BlockPos centerPos = context.origin();
+		TemplatePlacement placement = TemplatePlacement.centeredWithRandomRotation(template, centerPos, rand);
 		
 		if(placement.heightRange(Heightmap.Types.OCEAN_FLOOR, context.level()).difference() > 2)
 			return false;
 		
-		int y = level.getHeight(Heightmap.Types.OCEAN_FLOOR, placement.cornerPos().getX(), placement.cornerPos().getZ()) - rand.nextInt(1) - 4;
+		int y = level.getHeight(Heightmap.Types.OCEAN_FLOOR, centerPos.getX(), centerPos.getZ()) - rand.nextInt(1) - 4;
 		placement.placeWithStructureBlockRegistry(y, context);
 		
 		return true;
