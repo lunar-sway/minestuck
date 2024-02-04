142de9e86454e03d91d6cb56be32d7d84e5edc49
dweb <ryanpgunnison@gmail.com>
2024-02-04 14:39:47 -0500

    Adapted to changes from PR#557

--- src/main/java/com/mraof/minestuck/world/gen/feature/FrogRuinFeature.java
+++ src/main/java/com/mraof/minestuck/world/gen/feature/FrogRuinFeature.java
@@ -3,18 +3,19 @@ package com.mraof.minestuck.world.gen.feature;
 import com.mojang.serialization.Codec;
 import com.mraof.minestuck.Minestuck;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Vec3i;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
 /**
  * Structure nbt defined dilapidated frog ruins for Frog Lands
  */
-public class FrogRuinFeature extends AbstractTemplateFeature<NoneFeatureConfiguration>
+public class FrogRuinFeature extends Feature<NoneFeatureConfiguration>
 {
 	private static final ResourceLocation FROG_RUINS_0 = new ResourceLocation(Minestuck.MOD_ID, "frog_ruins_0");
 	private static final ResourceLocation FROG_RUINS_1 = new ResourceLocation(Minestuck.MOD_ID, "frog_ruins_1");
@@ -24,8 +25,7 @@ public class FrogRuinFeature extends AbstractTemplateFeature<NoneFeatureConfigur
 		super(codec);
 	}
 	
-	@Override
-	protected ResourceLocation pickTemplate(RandomSource random)
+	private static ResourceLocation pickTemplate(RandomSource random)
 	{
 		return random.nextBoolean() ? FROG_RUINS_0: FROG_RUINS_1;
 	}
@@ -33,16 +33,18 @@ public class FrogRuinFeature extends AbstractTemplateFeature<NoneFeatureConfigur
 	@Override
 	public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context)
 	{
-		//continue with normal placement only if the terrain is relatively even
-		if(isEvenTerrain(context, context.origin().offset(8, 0, 8), 3))
-			return super.place(context);
-		else
+		WorldGenLevel level = context.level();
+		RandomSource rand = context.random();
+		StructureTemplate template = level.getLevel().getStructureManager().getOrCreate(pickTemplate(rand));
+		BlockPos centerPos = context.origin();
+		TemplatePlacement placement = TemplatePlacement.centeredWithRandomRotation(template, centerPos, rand);
+		
+		if(placement.heightRange(Heightmap.Types.OCEAN_FLOOR, context.level()).difference() > 3)
 			return false;
-	}
-	
-	@Override
-	protected int pickY(WorldGenLevel level, BlockPos pos, Vec3i templateSize, RandomSource random)
-	{
-		return level.getHeight(Heightmap.Types.OCEAN_FLOOR, pos.getX(), pos.getZ()) - random.nextInt(1) - 3;
+		
+		int y = level.getHeight(Heightmap.Types.OCEAN_FLOOR, centerPos.getX(), centerPos.getZ()) - rand.nextInt(1) - 3;
+		placement.placeWithStructureBlockRegistry(y, context);
+		
+		return true;
 	}
 }
