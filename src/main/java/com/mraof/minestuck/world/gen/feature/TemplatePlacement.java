4b3ae7f5d446fb37ed22679aedb3f791574c421b
kirderf1 <kirderf4@gmail.com>
2024-02-10 15:34:07 +0100

    Use TemplatePlacement for SmallLibraryFeature and add more functionality

--- src/main/java/com/mraof/minestuck/world/gen/feature/TemplatePlacement.java
+++ src/main/java/com/mraof/minestuck/world/gen/feature/TemplatePlacement.java
@@ -10,14 +10,20 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Mirror;
 import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructurePlaceSettings;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate;
 
 /**
- * A helper class for placing a structure template from a {@link net.minecraft.world.level.levelgen.feature.Feature}.
+ * A helper class for placing a structure template from a {@link Feature}.
+ * Some functions take positions in "unrotated coordinate space",
+ * which is the coordinate space that the template itself is in before it is rotated and placed into the world,
+ * which covers (0, 0, 0) to (size.x - 1, size.y - 1, size.z - 1).
+ * "level space" instead refers to the actual, in-world position of a block.
  */
+@SuppressWarnings("unused")
 public record TemplatePlacement(StructureTemplate template, BlockPos zeroPos, Rotation rotation)
 {
 	public static TemplatePlacement centeredWithRandomRotation(StructureTemplate template, BlockPos centerPos, RandomSource randomSource)
@@ -34,6 +40,14 @@ public record TemplatePlacement(StructureTemplate template, BlockPos zeroPos, Ro
 		return this.template.getSize(this.rotation);
 	}
 	
+	public BlockPos actualFromRelative(int x, int y, int z)
+	{
+		return this.actualFromRelative(new BlockPos(x, y, z));
+	}
+	
+	/**
+	 * Takes a position in unrotated template space and translates it into level space.
+	 */
 	public BlockPos actualFromRelative(BlockPos templatePos)
 	{
 		return this.zeroPos.offset(StructureTemplate.transform(templatePos, Mirror.NONE, this.rotation, BlockPos.ZERO));
@@ -42,25 +56,40 @@ public record TemplatePlacement(StructureTemplate template, BlockPos zeroPos, Ro
 	public Iterable<BlockPos> xzPlacedRange()
 	{
 		Vec3i unrotatedSize = this.template.getSize();
-		return BlockPos.betweenClosed(
-				actualFromRelative(BlockPos.ZERO),
-				actualFromRelative(new BlockPos(unrotatedSize.getX() - 1, 0, unrotatedSize.getZ() - 1)));
+		return xzRange(0, 0, unrotatedSize.getX() - 1, unrotatedSize.getZ() - 1);
+	}
+	
+	/**
+	 * Takes coordinates that forms a rectangle in unrotated template space,
+	 * and returns all positions in this rectangle in level space.
+	 */
+	public Iterable<BlockPos> xzRange(int x1, int z1, int x2, int z2)
+	{
+		return BlockPos.betweenClosed(actualFromRelative(x1, 0, z1), actualFromRelative(x2, 0, z2));
 	}
 	
 	public int minHeight(Heightmap.Types type, LevelReader level)
 	{
+		return minHeight(type, level, this.xzPlacedRange());
+	}
+	public int minHeight(Heightmap.Types type, LevelReader level, Iterable<BlockPos> positions)
+	{
 		int minY = Integer.MAX_VALUE;
-		for(BlockPos floorPos : this.xzPlacedRange())
+		for(BlockPos floorPos : positions)
 			minY = Math.min(minY, level.getHeight(type, floorPos.getX(), floorPos.getZ()));
 		
 		return minY;
 	}
 	
-	@SuppressWarnings("unused")
 	public int maxHeight(Heightmap.Types type, LevelReader level)
 	{
+		return maxHeight(type, level, this.xzPlacedRange());
+	}
+	
+	public static int maxHeight(Heightmap.Types type, LevelReader level, Iterable<BlockPos> positions)
+	{
 		int maxY = Integer.MIN_VALUE;
-		for(BlockPos floorPos : this.xzPlacedRange())
+		for(BlockPos floorPos : positions)
 			maxY = Math.max(maxY, level.getHeight(type, floorPos.getX(), floorPos.getZ()));
 		
 		return maxY;
@@ -68,9 +97,14 @@ public record TemplatePlacement(StructureTemplate template, BlockPos zeroPos, Ro
 	
 	public Range heightRange(Heightmap.Types type, LevelReader level)
 	{
+		return heightRange(type, level, this.xzPlacedRange());
+	}
+	
+	public static Range heightRange(Heightmap.Types type, LevelReader level, Iterable<BlockPos> positions)
+	{
 		int minY = Integer.MAX_VALUE;
 		int maxY = Integer.MIN_VALUE;
-		for(BlockPos floorPos : this.xzPlacedRange())
+		for(BlockPos floorPos : positions)
 		{
 			int height = level.getHeight(type, floorPos.getX(), floorPos.getZ());
 			minY = Math.min(minY, height);
