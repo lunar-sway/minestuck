001658de7fbfd4d4ce97335ef53c172b56880964
kirderf1 <kirderf4@gmail.com>
2024-02-29 17:28:44 +0100

    Move aspect tree language names out of MinestuckEnUsLanguageProvider to new class

---
+++ src/main/java/com/mraof/minestuck/data/AspectTreeBlocksData.java
@@ -0,0 +1,201 @@
+package com.mraof.minestuck.data;
+
+import static com.mraof.minestuck.block.AspectTreeBlocks.*;
+
+public final class AspectTreeBlocksData
+{
+	public static void addEnUsTranslations(MinestuckLanguageProvider provider)
+	{
+		provider.addBlock(BLOOD_ASPECT_LOG, "Blood Log");
+		provider.addBlock(BLOOD_ASPECT_LEAVES, "Blood Leaves");
+		provider.addBlock(BLOOD_ASPECT_SAPLING, "Blood Sapling");
+		provider.addBlockTooltip(BLOOD_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(BLOOD_ASPECT_PLANKS, "Blood Planks");
+		provider.addBlock(BLOOD_ASPECT_STAIRS, "Blood Stairs");
+		provider.addBlock(BLOOD_ASPECT_SLAB, "Blood Slab");
+		provider.addBlock(BLOOD_ASPECT_FENCE, "Blood Fence");
+		provider.addBlock(BLOOD_ASPECT_FENCE_GATE, "Blood Fence Gate");
+		provider.addBlock(BLOOD_ASPECT_DOOR, "Blood Door");
+		provider.addBlock(BLOOD_ASPECT_TRAPDOOR, "Blood Trapdoor");
+		provider.addBlock(BLOOD_ASPECT_PRESSURE_PLATE, "Blood Pressure Plate");
+		provider.addBlock(BLOOD_ASPECT_BUTTON, "Blood Button");
+		provider.addBlock(BLOOD_ASPECT_BOOKSHELF, "Blood Bookshelf");
+		provider.addBlock(BLOOD_ASPECT_LADDER, "Blood Ladder");
+		
+		provider.addBlock(BREATH_ASPECT_LOG, "Breath Log");
+		provider.addBlock(BREATH_ASPECT_LEAVES, "Breath Leaves");
+		provider.addBlock(BREATH_ASPECT_SAPLING, "Breath Sapling");
+		provider.addBlockTooltip(BREATH_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(BREATH_ASPECT_PLANKS, "Breath Planks");
+		provider.addBlock(BREATH_ASPECT_STAIRS, "Breath Stairs");
+		provider.addBlock(BREATH_ASPECT_SLAB, "Breath Slab");
+		provider.addBlock(BREATH_ASPECT_FENCE, "Breath Fence");
+		provider.addBlock(BREATH_ASPECT_FENCE_GATE, "Breath Fence Gate");
+		provider.addBlock(BREATH_ASPECT_DOOR, "Breath Door");
+		provider.addBlock(BREATH_ASPECT_TRAPDOOR, "Breath Trapdoor");
+		provider.addBlock(BREATH_ASPECT_PRESSURE_PLATE, "Breath Pressure Plate");
+		provider.addBlock(BREATH_ASPECT_BUTTON, "Breath Button");
+		provider.addBlock(BREATH_ASPECT_BOOKSHELF, "Breath Bookshelf");
+		provider.addBlock(BREATH_ASPECT_LADDER, "Breath Ladder");
+		
+		provider.addBlock(DOOM_ASPECT_LOG, "Doom Log");
+		provider.addBlock(DOOM_ASPECT_LEAVES, "Doom Leaves");
+		provider.addBlock(DOOM_ASPECT_SAPLING, "Doom Sapling");
+		provider.addBlockTooltip(DOOM_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(DOOM_ASPECT_PLANKS, "Doom Planks");
+		provider.addBlock(DOOM_ASPECT_STAIRS, "Doom Stairs");
+		provider.addBlock(DOOM_ASPECT_SLAB, "Doom Slab");
+		provider.addBlock(DOOM_ASPECT_FENCE, "Doom Fence");
+		provider.addBlock(DOOM_ASPECT_FENCE_GATE, "Doom Fence Gate");
+		provider.addBlock(DOOM_ASPECT_DOOR, "Doom Door");
+		provider.addBlock(DOOM_ASPECT_TRAPDOOR, "Doom Trapdoor");
+		provider.addBlock(DOOM_ASPECT_PRESSURE_PLATE, "Doom Pressure Plate");
+		provider.addBlock(DOOM_ASPECT_BUTTON, "Doom Button");
+		provider.addBlock(DOOM_ASPECT_BOOKSHELF, "Doom Bookshelf");
+		provider.addBlock(DOOM_ASPECT_LADDER, "Doom Ladder");
+		
+		provider.addBlock(HEART_ASPECT_LOG, "Heart Log");
+		provider.addBlock(HEART_ASPECT_LEAVES, "Heart Leaves");
+		provider.addBlock(HEART_ASPECT_SAPLING, "Heart Sapling");
+		provider.addBlockTooltip(HEART_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(HEART_ASPECT_PLANKS, "Heart Planks");
+		provider.addBlock(HEART_ASPECT_STAIRS, "Heart Stairs");
+		provider.addBlock(HEART_ASPECT_SLAB, "Heart Slab");
+		provider.addBlock(HEART_ASPECT_FENCE, "Heart Fence");
+		provider.addBlock(HEART_ASPECT_FENCE_GATE, "Heart Fence Gate");
+		provider.addBlock(HEART_ASPECT_DOOR, "Heart Door");
+		provider.addBlock(HEART_ASPECT_TRAPDOOR, "Heart Trapdoor");
+		provider.addBlock(HEART_ASPECT_PRESSURE_PLATE, "Heart Pressure Plate");
+		provider.addBlock(HEART_ASPECT_BUTTON, "Heart Button");
+		provider.addBlock(HEART_ASPECT_BOOKSHELF, "Heart Bookshelf");
+		provider.addBlock(HEART_ASPECT_LADDER, "Heart Ladder");
+		
+		provider.addBlock(HOPE_ASPECT_LOG, "Hope Log");
+		provider.addBlock(HOPE_ASPECT_LEAVES, "Hope Leaves");
+		provider.addBlock(HOPE_ASPECT_SAPLING, "Hope Sapling");
+		provider.addBlockTooltip(HOPE_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(HOPE_ASPECT_PLANKS, "Hope Planks");
+		provider.addBlock(HOPE_ASPECT_STAIRS, "Hope Stairs");
+		provider.addBlock(HOPE_ASPECT_SLAB, "Hope Slab");
+		provider.addBlock(HOPE_ASPECT_FENCE, "Hope Fence");
+		provider.addBlock(HOPE_ASPECT_FENCE_GATE, "Hope Fence Gate");
+		provider.addBlock(HOPE_ASPECT_DOOR, "Hope Door");
+		provider.addBlock(HOPE_ASPECT_TRAPDOOR, "Hope Trapdoor");
+		provider.addBlock(HOPE_ASPECT_PRESSURE_PLATE, "Hope Pressure Plate");
+		provider.addBlock(HOPE_ASPECT_BUTTON, "Hope Button");
+		provider.addBlock(HOPE_ASPECT_BOOKSHELF, "Hope Bookshelf");
+		provider.addBlock(HOPE_ASPECT_LADDER, "Hope Ladder");
+		
+		provider.addBlock(LIFE_ASPECT_LOG, "Life Log");
+		provider.addBlock(LIFE_ASPECT_LEAVES, "Life Leaves");
+		provider.addBlock(LIFE_ASPECT_SAPLING, "Life Sapling");
+		provider.addBlockTooltip(LIFE_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(LIFE_ASPECT_PLANKS, "Life Planks");
+		provider.addBlock(LIFE_ASPECT_STAIRS, "Life Stairs");
+		provider.addBlock(LIFE_ASPECT_SLAB, "Life Slab");
+		provider.addBlock(LIFE_ASPECT_FENCE, "Life Fence");
+		provider.addBlock(LIFE_ASPECT_FENCE_GATE, "Life Fence Gate");
+		provider.addBlock(LIFE_ASPECT_DOOR, "Life Door");
+		provider.addBlock(LIFE_ASPECT_TRAPDOOR, "Life Trapdoor");
+		provider.addBlock(LIFE_ASPECT_PRESSURE_PLATE, "Life Pressure Plate");
+		provider.addBlock(LIFE_ASPECT_BUTTON, "Life Button");
+		provider.addBlock(LIFE_ASPECT_BOOKSHELF, "Life Bookshelf");
+		provider.addBlock(LIFE_ASPECT_LADDER, "Life Ladder");
+		
+		provider.addBlock(LIGHT_ASPECT_LOG, "Light Log");
+		provider.addBlock(LIGHT_ASPECT_LEAVES, "Light Leaves");
+		provider.addBlock(LIGHT_ASPECT_SAPLING, "Light Sapling");
+		provider.addBlockTooltip(LIGHT_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(LIGHT_ASPECT_PLANKS, "Light Planks");
+		provider.addBlock(LIGHT_ASPECT_STAIRS, "Light Stairs");
+		provider.addBlock(LIGHT_ASPECT_SLAB, "Light Slab");
+		provider.addBlock(LIGHT_ASPECT_FENCE, "Light Fence");
+		provider.addBlock(LIGHT_ASPECT_FENCE_GATE, "Light Fence Gate");
+		provider.addBlock(LIGHT_ASPECT_DOOR, "Light Door");
+		provider.addBlock(LIGHT_ASPECT_TRAPDOOR, "Light Trapdoor");
+		provider.addBlock(LIGHT_ASPECT_PRESSURE_PLATE, "Light Pressure Plate");
+		provider.addBlock(LIGHT_ASPECT_BUTTON, "Light Button");
+		provider.addBlock(LIGHT_ASPECT_BOOKSHELF, "Light Bookshelf");
+		provider.addBlock(LIGHT_ASPECT_LADDER, "Light Ladder");
+		
+		provider.addBlock(MIND_ASPECT_LOG, "Mind Log");
+		provider.addBlock(MIND_ASPECT_LEAVES, "Mind Leaves");
+		provider.addBlock(MIND_ASPECT_SAPLING, "Mind Sapling");
+		provider.addBlockTooltip(MIND_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(MIND_ASPECT_PLANKS, "Mind Planks");
+		provider.addBlock(MIND_ASPECT_STAIRS, "Mind Stairs");
+		provider.addBlock(MIND_ASPECT_SLAB, "Mind Slab");
+		provider.addBlock(MIND_ASPECT_FENCE, "Mind Fence");
+		provider.addBlock(MIND_ASPECT_FENCE_GATE, "Mind Fence Gate");
+		provider.addBlock(MIND_ASPECT_DOOR, "Mind Door");
+		provider.addBlock(MIND_ASPECT_TRAPDOOR, "Mind Trapdoor");
+		provider.addBlock(MIND_ASPECT_PRESSURE_PLATE, "Mind Pressure Plate");
+		provider.addBlock(MIND_ASPECT_BUTTON, "Mind Button");
+		provider.addBlock(MIND_ASPECT_BOOKSHELF, "Mind Bookshelf");
+		provider.addBlock(MIND_ASPECT_LADDER, "Mind Ladder");
+		
+		provider.addBlock(RAGE_ASPECT_LOG, "Rage Log");
+		provider.addBlock(RAGE_ASPECT_LEAVES, "Rage Leaves");
+		provider.addBlock(RAGE_ASPECT_SAPLING, "Rage Sapling");
+		provider.addBlockTooltip(RAGE_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(RAGE_ASPECT_PLANKS, "Rage Planks");
+		provider.addBlock(RAGE_ASPECT_STAIRS, "Rage Stairs");
+		provider.addBlock(RAGE_ASPECT_SLAB, "Rage Slab");
+		provider.addBlock(RAGE_ASPECT_FENCE, "Rage Fence");
+		provider.addBlock(RAGE_ASPECT_FENCE_GATE, "Rage Fence Gate");
+		provider.addBlock(RAGE_ASPECT_DOOR, "Rage Door");
+		provider.addBlock(RAGE_ASPECT_TRAPDOOR, "Rage Trapdoor");
+		provider.addBlock(RAGE_ASPECT_PRESSURE_PLATE, "Rage Pressure Plate");
+		provider.addBlock(RAGE_ASPECT_BUTTON, "Rage Button");
+		provider.addBlock(RAGE_ASPECT_BOOKSHELF, "Rage Bookshelf");
+		provider.addBlock(RAGE_ASPECT_LADDER, "Rage Ladder");
+		
+		provider.addBlock(SPACE_ASPECT_LOG, "Space Log");
+		provider.addBlock(SPACE_ASPECT_LEAVES, "Space Leaves");
+		provider.addBlock(SPACE_ASPECT_SAPLING, "Space Sapling");
+		provider.addBlockTooltip(SPACE_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(SPACE_ASPECT_PLANKS, "Space Planks");
+		provider.addBlock(SPACE_ASPECT_STAIRS, "Space Stairs");
+		provider.addBlock(SPACE_ASPECT_SLAB, "Space Slab");
+		provider.addBlock(SPACE_ASPECT_FENCE, "Space Fence");
+		provider.addBlock(SPACE_ASPECT_FENCE_GATE, "Space Fence Gate");
+		provider.addBlock(SPACE_ASPECT_DOOR, "Space Door");
+		provider.addBlock(SPACE_ASPECT_TRAPDOOR, "Space Trapdoor");
+		provider.addBlock(SPACE_ASPECT_PRESSURE_PLATE, "Space Pressure Plate");
+		provider.addBlock(SPACE_ASPECT_BUTTON, "Space Button");
+		provider.addBlock(SPACE_ASPECT_BOOKSHELF, "Space Bookshelf");
+		provider.addBlock(SPACE_ASPECT_LADDER, "Space Ladder");
+		
+		provider.addBlock(TIME_ASPECT_LOG, "Time Log");
+		provider.addBlock(TIME_ASPECT_LEAVES, "Time Leaves");
+		provider.addBlock(TIME_ASPECT_SAPLING, "Time Sapling");
+		provider.addBlockTooltip(TIME_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(TIME_ASPECT_PLANKS, "Time Planks");
+		provider.addBlock(TIME_ASPECT_STAIRS, "Time Stairs");
+		provider.addBlock(TIME_ASPECT_SLAB, "Time Slab");
+		provider.addBlock(TIME_ASPECT_FENCE, "Time Fence");
+		provider.addBlock(TIME_ASPECT_FENCE_GATE, "Time Fence Gate");
+		provider.addBlock(TIME_ASPECT_DOOR, "Time Door");
+		provider.addBlock(TIME_ASPECT_TRAPDOOR, "Time Trapdoor");
+		provider.addBlock(TIME_ASPECT_PRESSURE_PLATE, "Time Pressure Plate");
+		provider.addBlock(TIME_ASPECT_BUTTON, "Time Button");
+		provider.addBlock(TIME_ASPECT_BOOKSHELF, "Time Bookshelf");
+		provider.addBlock(TIME_ASPECT_LADDER, "Time Ladder");
+		
+		provider.addBlock(VOID_ASPECT_LOG, "Void Log");
+		provider.addBlock(VOID_ASPECT_LEAVES, "Void Leaves");
+		provider.addBlock(VOID_ASPECT_SAPLING, "Void Sapling");
+		provider.addBlockTooltip(VOID_ASPECT_SAPLING, "Not yet implemented");
+		provider.addBlock(VOID_ASPECT_PLANKS, "Void Planks");
+		provider.addBlock(VOID_ASPECT_STAIRS, "Void Stairs");
+		provider.addBlock(VOID_ASPECT_SLAB, "Void Slab");
+		provider.addBlock(VOID_ASPECT_FENCE, "Void Fence");
+		provider.addBlock(VOID_ASPECT_FENCE_GATE, "Void Fence Gate");
+		provider.addBlock(VOID_ASPECT_DOOR, "Void Door");
+		provider.addBlock(VOID_ASPECT_TRAPDOOR, "Void Trapdoor");
+		provider.addBlock(VOID_ASPECT_PRESSURE_PLATE, "Void Pressure Plate");
+		provider.addBlock(VOID_ASPECT_BUTTON, "Void Button");
+		provider.addBlock(VOID_ASPECT_BOOKSHELF, "Void Bookshelf");
+		provider.addBlock(VOID_ASPECT_LADDER, "Void Ladder");
+	}
+}
