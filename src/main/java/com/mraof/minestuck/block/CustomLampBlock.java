62712000a5257d0ca6161993619f9e02d2e0839c
glubtier <hey.tyrian@gmail.com>
2024-02-29 07:03:24 -0800

    Update src/main/java/com/mraof/minestuck/block/CustomLampBlock.java
    Co-authored-by: kirderf1 <kirderf4@gmail.com>

--- src/main/java/com/mraof/minestuck/block/CustomLampBlock.java
+++ src/main/java/com/mraof/minestuck/block/CustomLampBlock.java
@@ -24,8 +24,7 @@ public class CustomLampBlock extends Block
 	public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos,
 								 Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
 		if(!pLevel.isClientSide() && pHand == InteractionHand.MAIN_HAND) {
-			boolean currentState = pState.getValue(CLICKED);
-			pLevel.setBlock(pPos, pState.setValue(CLICKED, !currentState), 3);
+			pLevel.setBlock(pPos, pState.cycle(CLICKED), 3);
 		}
 		
 		return InteractionResult.SUCCESS;
