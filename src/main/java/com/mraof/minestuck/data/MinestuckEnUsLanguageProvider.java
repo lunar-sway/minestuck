c820d671034f1cb88a10a0b33deea2044f3cbbda
kirderf1 <kirderf4@gmail.com>
2024-03-10 15:10:50 +0100

    responseMsg() and translation keys for a lot of responses

--- src/main/java/com/mraof/minestuck/data/MinestuckEnUsLanguageProvider.java
+++ src/main/java/com/mraof/minestuck/data/MinestuckEnUsLanguageProvider.java
@@ -2484,9 +2484,6 @@ public class MinestuckEnUsLanguageProvider extends MinestuckLanguageProvider
 		addSimpleDeathMessage(MSDamageTypeProvider.DECAPITATION_MSG, "%s had their head chopped off",
 				"%1$s lost their head whilst engaged with %2$s");
 		
-		addDialogue("test1", "Press §eSHIFT§r for more info");
-		addDialogue("test_arguments", "Player name land: %s");
-		
 		add(ConsortMerchantInventory.CANT_AFFORD, "You don't have enough boondollars for that!");
 		add(MessageType.MISSING_ITEM, "You need a %s for that!");
 		addDialogue("thanks", "Thanks!");
