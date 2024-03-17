f9db80cfbe6b2e60f91ef59272ec857e9e7af99d
dweb <ryanpgunnison@gmail.com>
2024-03-17 13:05:10 -0400

    Fixed issue with hovering over and clicking custom height buttons

--- src/main/java/com/mraof/minestuck/client/gui/DialogueButton.java
+++ src/main/java/com/mraof/minestuck/client/gui/DialogueButton.java
@@ -28,26 +28,41 @@ public class DialogueButton extends ExtendedButton
 		this.trueHeight = messageLines.size() * 14;
 	}
 	
-	//TODO default height value is used to determine if hovered over
+	@Override
+	protected boolean clicked(double pMouseX, double pMouseY)
+	{
+		return activeAndVisible() && mouseOvertop(pMouseX, pMouseY);
+	}
+	
+	@Override
+	public boolean isMouseOver(double pMouseX, double pMouseY)
+	{
+		return activeAndVisible() && mouseOvertop(pMouseX, pMouseY);
+	}
+	
+	private boolean mouseOvertop(double pMouseX, double pMouseY)
+	{
+		return pMouseX >= this.getX() && pMouseY >= this.getY() && pMouseX < this.getX() + this.width && pMouseY < this.getY() + trueHeight;
+	}
+	
+	private boolean activeAndVisible()
+	{
+		return this.active && this.visible;
+	}
 	
 	//TODO allow the box and text to shift to the right when selected to match original strife flashes
-	
-	
 	
 	@Override
 	public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick)
 	{
+		//overrides setting of boolean from AbstractWidget which uses normal height value
+		this.isHovered = mouseOvertop(mouseX, mouseY);
+		
 		Minecraft mc = Minecraft.getInstance();
 		int k = !this.active ? 0 : (this.isHoveredOrFocused() ? 2 : 1);
 		
-		//final FormattedText buttonText = mc.font.substrByWidth(this.getMessage(), this.width);
-		//final FormattedText buttonText = mc.font.split(this.getMessage(), this.width);
-		//int wordWrapHeight = mc.font.wordWrapHeight(buttonText, this.width);
-		
 		guiGraphics.blitWithBorder(gui, this.getX(), this.getY(), 0, 176 + k * 20, this.width, trueHeight, 200, 20, 3, 3, 3, 3);
 		
-		//guiGraphics.drawWordWrap(mc.font, buttonText, this.getX() + this.width / 2, this.getY() + (this.height - 8) / 2, this.width, getFGColor());
-		//guiGraphics.drawCenteredString(mc.font, Language.getInstance().getVisualOrder(buttonText), this.getX() + this.width / 2, this.getY() + (this.height - 8) / 2, getFGColor());
 		int pY = this.getY() + (this.height - 8) / 2;
 		for(FormattedCharSequence line : messageLines)
 		{
