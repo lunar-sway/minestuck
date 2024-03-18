cb65de021708ae96015cb289ebfc07b9afd38621
kirderf1 <kirderf4@gmail.com>
2024-03-18 17:26:06 +0100

    Check corners for the minimum height

--- src/main/java/com/mraof/minestuck/world/gen/structure/LargeWoodObjectStructure.java
+++ src/main/java/com/mraof/minestuck/world/gen/structure/LargeWoodObjectStructure.java
@@ -2,15 +2,16 @@ package com.mraof.minestuck.world.gen.structure;
 
 import com.mojang.serialization.Codec;
 import com.mraof.minestuck.Minestuck;
+import com.mraof.minestuck.world.gen.feature.TemplatePlacement;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilder;
+import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
 
 import java.util.List;
 import java.util.Optional;
@@ -41,10 +42,16 @@ public class LargeWoodObjectStructure extends Structure
 	private static void generatePieces(StructurePiecesBuilder builder, GenerationContext context)
 	{
 		RandomSource random = context.random();
+		StructureTemplateManager templateManager = context.structureTemplateManager();
+		ResourceLocation templateId = Util.getRandom(TEMPLATES, random);
+		
 		int x = context.chunkPos().getBlockX(random.nextInt(16)), z = context.chunkPos().getBlockZ(random.nextInt(16));
-		int y = context.chunkGenerator().getBaseHeight(x, z, Heightmap.Types.OCEAN_FLOOR_WG, context.heightAccessor(), context.randomState());
-		BlockPos blockpos = new BlockPos(x, y, z);
-		SimpleTemplatePiece piece = new SimpleTemplatePiece(context.structureTemplateManager(), Util.getRandom(TEMPLATES, random), blockpos, Rotation.getRandom(random));
-		builder.addPiece(piece);
+		var placement = TemplatePlacement.centeredWithRandomRotation(templateManager.getOrCreate(templateId), new BlockPos(x, 0, z), random);
+		
+		int minY = placement.xzCorners()
+				.mapToInt(corner -> context.chunkGenerator().getBaseHeight(corner.getX(), corner.getZ(), Heightmap.Types.OCEAN_FLOOR_WG, context.heightAccessor(), context.randomState()))
+				.min().orElseThrow();
+		
+		builder.addPiece(new SimpleTemplatePiece(templateManager, templateId, placement.zeroPos().atY(minY), placement.rotation()));
 	}
 }
