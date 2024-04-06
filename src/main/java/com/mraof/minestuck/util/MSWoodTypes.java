984b35f237dfc6dc5686dd852c22b264cada6d7d
Tyrian <hey.tyrian@gmail.com>
2024-04-05 22:03:39 -0700

    Add block sets for Minestuck blocks

--- src/main/java/com/mraof/minestuck/util/MSWoodTypes.java
+++ src/main/java/com/mraof/minestuck/util/MSWoodTypes.java
@@ -1,20 +1,35 @@
 package com.mraof.minestuck.util;
 
 import com.mraof.minestuck.Minestuck;
+import com.mraof.minestuck.api.MSBlockSetType;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class MSWoodTypes
 {
-	public static final WoodType CARVED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":carved", BlockSetType.OAK));
-	public static final WoodType DEAD = WoodType.register(new WoodType(Minestuck.MOD_ID + ":dead", BlockSetType.OAK));
-	public static final WoodType END = WoodType.register(new WoodType(Minestuck.MOD_ID + ":end", BlockSetType.OAK));
-	public static final WoodType FROST = WoodType.register(new WoodType(Minestuck.MOD_ID + ":frost", BlockSetType.OAK));
-	public static final WoodType GLOWING = WoodType.register(new WoodType(Minestuck.MOD_ID + ":glowing", BlockSetType.OAK));
-	public static final WoodType RAINBOW = WoodType.register(new WoodType(Minestuck.MOD_ID + ":rainbow", BlockSetType.OAK));
-	public static final WoodType SHADEWOOD = WoodType.register(new WoodType(Minestuck.MOD_ID + ":shadewood", BlockSetType.OAK));
-	public static final WoodType TREATED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":treated", BlockSetType.OAK));
-	public static final WoodType LACQUERED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":lacquered", BlockSetType.OAK));
-	public static final WoodType PERFECTLY_GENERIC = WoodType.register(new WoodType(Minestuck.MOD_ID + ":perfectly_generic", BlockSetType.OAK));
-	public static final WoodType CINDERED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":cindered", BlockSetType.OAK));
+	public static final WoodType CARVED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":carved", MSBlockSetType.CARVED));
+	public static final WoodType DEAD = WoodType.register(new WoodType(Minestuck.MOD_ID + ":dead", MSBlockSetType.DEAD));
+	public static final WoodType END = WoodType.register(new WoodType(Minestuck.MOD_ID + ":end", MSBlockSetType.END));
+	public static final WoodType FROST = WoodType.register(new WoodType(Minestuck.MOD_ID + ":frost", MSBlockSetType.FROST));
+	public static final WoodType GLOWING = WoodType.register(new WoodType(Minestuck.MOD_ID + ":glowing", MSBlockSetType.GLOWING));
+	public static final WoodType RAINBOW = WoodType.register(new WoodType(Minestuck.MOD_ID + ":rainbow", MSBlockSetType.RAINBOW));
+	public static final WoodType SHADEWOOD = WoodType.register(new WoodType(Minestuck.MOD_ID + ":shadewood", MSBlockSetType.SHADEWOOD));
+	public static final WoodType TREATED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":treated", MSBlockSetType.TREATED));
+	public static final WoodType LACQUERED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":lacquered", MSBlockSetType.LACQUERED));
+	public static final WoodType PERFECTLY_GENERIC = WoodType.register(new WoodType(Minestuck.MOD_ID + ":perfectly_generic", MSBlockSetType.PERFECTLY_GENERIC));
+	public static final WoodType CINDERED = WoodType.register(new WoodType(Minestuck.MOD_ID + ":cindered", MSBlockSetType.CINDERED));
+	
+	public static final WoodType BLOOD = WoodType.register(new WoodType(Minestuck.MOD_ID + ":blood", MSBlockSetType.BLOOD));
+	public static final WoodType BREATH = WoodType.register(new WoodType(Minestuck.MOD_ID + ":breath", MSBlockSetType.BREATH));
+	public static final WoodType DOOM = WoodType.register(new WoodType(Minestuck.MOD_ID + ":doom", MSBlockSetType.DOOM));
+	public static final WoodType HEART = WoodType.register(new WoodType(Minestuck.MOD_ID + ":heart", MSBlockSetType.HEART));
+	public static final WoodType HOPE = WoodType.register(new WoodType(Minestuck.MOD_ID + ":hope", MSBlockSetType.HOPE));
+	public static final WoodType LIFE = WoodType.register(new WoodType(Minestuck.MOD_ID + ":life", MSBlockSetType.LIFE));
+	public static final WoodType LIGHT = WoodType.register(new WoodType(Minestuck.MOD_ID + ":light", MSBlockSetType.LIGHT));
+	public static final WoodType MIND = WoodType.register(new WoodType(Minestuck.MOD_ID + ":mind", MSBlockSetType.MIND));
+	public static final WoodType RAGE = WoodType.register(new WoodType(Minestuck.MOD_ID + ":rage", MSBlockSetType.RAGE));
+	public static final WoodType SPACE = WoodType.register(new WoodType(Minestuck.MOD_ID + ":space", MSBlockSetType.SPACE));
+	public static final WoodType TIME = WoodType.register(new WoodType(Minestuck.MOD_ID + ":time", MSBlockSetType.TIME));
+	public static final WoodType VOID = WoodType.register(new WoodType(Minestuck.MOD_ID + ":void", MSBlockSetType.VOID));
+	
 }
