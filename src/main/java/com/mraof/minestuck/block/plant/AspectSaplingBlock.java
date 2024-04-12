774242a894526fd350bdace12230bf56c1a724ac
Tyrian <hey.tyrian@gmail.com>
2024-04-12 12:40:09 -0700

    Remove individual aspect sapling blocks, readd AspectSaplingBlock to accommodate placement in different terrain types

---
+++ src/main/java/com/mraof/minestuck/block/plant/AspectSaplingBlock.java
@@ -0,0 +1,23 @@
+package com.mraof.minestuck.block.plant;
+
+import com.mraof.minestuck.util.MSTags;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.grower.AbstractTreeGrower;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class AspectSaplingBlock extends SaplingBlock
+{
+	public AspectSaplingBlock(AbstractTreeGrower pTreeGrower, Properties pProperties)
+	{
+		super(pTreeGrower, pProperties);
+	}
+	
+	@Override
+	protected boolean mayPlaceOn(BlockState pState, BlockGetter pLevel, BlockPos pPos)
+	{
+		return pState.is(MSTags.Blocks.ASPECT_TREE_PLACEABLE) || pState.is(Blocks.DIRT);
+	}
+}
