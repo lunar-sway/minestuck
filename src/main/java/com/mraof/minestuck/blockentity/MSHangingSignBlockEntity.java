d45f91911f92009c380a39051a71f35e436c5830
kirderf1 <kirderf4@gmail.com>
2024-04-13 18:03:48 +0200

    Change MSHangingSignBlockEntity to extend SignBlockEntity again

--- src/main/java/com/mraof/minestuck/blockentity/MSHangingSignBlockEntity.java
+++ src/main/java/com/mraof/minestuck/blockentity/MSHangingSignBlockEntity.java
@@ -3,23 +3,37 @@ package com.mraof.minestuck.blockentity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
+import net.minecraft.world.level.block.entity.SignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 /**
  * A version of {@link HangingSignBlockEntity} for minestuck signs.
  * To add new sign blocks, they need to use a new block entity type, which is why this class exists.
  */
-public class MSHangingSignBlockEntity extends HangingSignBlockEntity
+//todo add gui textures and extend HangingSignBlockEntity to use them
+public class MSHangingSignBlockEntity extends SignBlockEntity
 {
 	public MSHangingSignBlockEntity(BlockPos blockPos, BlockState blockState)
 	{
-		super(blockPos, blockState);
+		super(MSBlockEntityTypes.HANGING_SIGN.get(), blockPos, blockState);
 	}
 	
 	@Override
 	public BlockEntityType<?> getType()
 	{
 		return MSBlockEntityTypes.HANGING_SIGN.get();
+	}
+	
+	@Override
+	public int getTextLineHeight()
+	{
+		return 9;
+	}
+	
+	@Override
+	public int getMaxTextLineWidth()
+	{
+		return 60;
 	}
 }
 
