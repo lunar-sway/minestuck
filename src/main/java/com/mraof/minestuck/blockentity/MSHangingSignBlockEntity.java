ae1b21da085308ae9f61e3f8ec9529de6ad77393
kirderf1 <kirderf4@gmail.com>
2024-04-13 17:39:32 +0200

    Change MSHangingSignBlockEntity to override HangingSignBlockEntity

--- src/main/java/com/mraof/minestuck/blockentity/MSHangingSignBlockEntity.java
+++ src/main/java/com/mraof/minestuck/blockentity/MSHangingSignBlockEntity.java
@@ -2,18 +2,18 @@ package com.mraof.minestuck.blockentity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.entity.SignBlockEntity;
+import net.minecraft.world.level.block.entity.HangingSignBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 
 /**
- * A version of {@link SignBlockEntity} for minestuck signs.
+ * A version of {@link HangingSignBlockEntity} for minestuck signs.
  * To add new sign blocks, they need to use a new block entity type, which is why this class exists.
  */
-public class MSHangingSignBlockEntity extends SignBlockEntity
+public class MSHangingSignBlockEntity extends HangingSignBlockEntity
 {
 	public MSHangingSignBlockEntity(BlockPos blockPos, BlockState blockState)
 	{
-		super(MSBlockEntityTypes.HANGING_SIGN.get(), blockPos, blockState);
+		super(blockPos, blockState);
 	}
 	
 	@Override
