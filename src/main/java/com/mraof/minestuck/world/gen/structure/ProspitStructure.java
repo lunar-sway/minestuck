f3a9cc7f7724911397c843ae2718ae64c91a7827
kirderf1 <kirderf4@gmail.com>
2024-04-21 21:39:20 +0200

    Add bridges and connections in all directions

--- src/main/java/com/mraof/minestuck/world/gen/structure/ProspitStructure.java
+++ src/main/java/com/mraof/minestuck/world/gen/structure/ProspitStructure.java
@@ -33,6 +33,7 @@ import net.minecraft.world.level.levelgen.structure.pieces.StructurePiecesBuilde
 import net.minecraft.world.level.levelgen.structure.placement.StructurePlacement;
 import net.minecraft.world.level.levelgen.structure.placement.StructurePlacementType;
 
+import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.*;
 import java.util.function.Function;
@@ -108,9 +109,25 @@ public final class ProspitStructure
 		private void generatePieces(StructurePiecesBuilder builder, GenerationContext context)
 		{
 			List<PieceEntry> pieces = List.of(
-					new PieceEntry(pos -> null, ConnectionType.AIR, ConnectionType.AIR, Weight.of(10)),
-					new PieceEntry(SolidPiece::new, ConnectionType.SOLID, ConnectionType.SOLID, Weight.of(10)),
-					new PieceEntry(PyramidPiece::new, ConnectionType.AIR, ConnectionType.SOLID, Weight.of(2))
+					new PieceEntry(pos -> null, symmetric(ConnectionType.AIR, ConnectionType.AIR, ConnectionType.AIR), Weight.of(10)),
+					new PieceEntry(SolidPiece::new, symmetric(ConnectionType.SOLID, ConnectionType.SOLID, ConnectionType.WALL), Weight.of(10)),
+					new PieceEntry(PyramidPiece::new, symmetric(ConnectionType.SOLID, ConnectionType.AIR, ConnectionType.AIR), Weight.of(2)),
+					new PieceEntry(pos -> new BridgePiece(pos, Direction.Axis.X), Map.of(
+							Direction.DOWN, ConnectionType.AIR,
+							Direction.UP, ConnectionType.AIR,
+							Direction.NORTH, ConnectionType.AIR,
+							Direction.EAST, ConnectionType.BRIDGE,
+							Direction.SOUTH, ConnectionType.AIR,
+							Direction.WEST, ConnectionType.BRIDGE
+					), Weight.of(4)),
+					new PieceEntry(pos -> new BridgePiece(pos, Direction.Axis.Z), Map.of(
+							Direction.DOWN, ConnectionType.AIR,
+							Direction.UP, ConnectionType.AIR,
+							Direction.NORTH, ConnectionType.BRIDGE,
+							Direction.EAST, ConnectionType.AIR,
+							Direction.SOUTH, ConnectionType.BRIDGE,
+							Direction.WEST, ConnectionType.AIR
+					), Weight.of(4))
 			);
 			
 			BlockPos cornerPos = context.chunkPos().getWorldPosition().offset(-(WIDTH_IN_CHUNKS * 8), 0, -(WIDTH_IN_CHUNKS * 8));
@@ -134,7 +151,7 @@ public final class ProspitStructure
 				for(int zIndex = 0; zIndex < WIDTH_IN_PIECES; zIndex++)
 				{
 					PiecePos topPos = new PiecePos(xIndex, HEIGHT_IN_PIECES - 1, zIndex);
-					removeConflictsFromAboveConnection(topPos, Set.of(ConnectionType.AIR), availablePiecesMap);
+					removeConflictsFromConnection(topPos, Direction.UP, Set.of(ConnectionType.AIR), availablePiecesMap);
 				}
 			}
 			
@@ -160,7 +177,7 @@ public final class ProspitStructure
 					builder.addPiece(piece);
 				
 				if(availablePieces.removeIf(entry -> entry != chosenEntry.get()))
-					removeConflictingPieces(pos, availablePiecesMap);
+					removeConflictingPieces(pos, availablePiecesMap, null);
 			}
 		}
 	}
@@ -214,28 +231,25 @@ public final class ProspitStructure
 		return entropy;
 	}
 	
-	private static void removeConflictingPieces(PiecePos pos, Map<PiecePos, List<PieceEntry>> availablePiecesMap)
+	private static void removeConflictingPieces(PiecePos pos, Map<PiecePos, List<PieceEntry>> availablePiecesMap, @Nullable Direction excluding)
 	{
-		if(pos.y > 0)
+		for(Direction direction : Direction.values())
 		{
-			Set<ConnectionType> connections = availablePiecesMap.get(pos).stream().map(PieceEntry::belowConnection).collect(Collectors.toSet());
-			PiecePos belowPos = new PiecePos(pos.x, pos.y - 1, pos.z);
-			removeConflictsFromAboveConnection(belowPos, connections, availablePiecesMap);
-		}
-		
-		if(pos.y < HEIGHT_IN_PIECES - 1)
-		{
-			Set<ConnectionType> connections = availablePiecesMap.get(pos).stream().map(PieceEntry::aboveConnection).collect(Collectors.toSet());
-			PiecePos abovePos = new PiecePos(pos.x, pos.y + 1, pos.z);
-			if(availablePiecesMap.get(abovePos).removeIf(aboveEntry -> cannotConnect(aboveEntry.belowConnection(), connections)))
-				removeConflictingPieces(abovePos, availablePiecesMap);
+			if(direction == excluding)
+				continue;
+			
+			pos.tryOffset(direction).ifPresent(adjacentPos ->
+			{
+				Set<ConnectionType> connections = availablePiecesMap.get(pos).stream().map(entry -> entry.connections.get(direction)).collect(Collectors.toSet());
+				removeConflictsFromConnection(adjacentPos, direction.getOpposite(), connections, availablePiecesMap);
+			});
 		}
 	}
 	
-	private static void removeConflictsFromAboveConnection(PiecePos pos, Set<ConnectionType> connections, Map<PiecePos, List<PieceEntry>> availablePiecesMap)
+	private static void removeConflictsFromConnection(PiecePos pos, Direction direction, Set<ConnectionType> connections, Map<PiecePos, List<PieceEntry>> availablePiecesMap)
 	{
-		if(availablePiecesMap.get(pos).removeIf(belowEntry -> cannotConnect(belowEntry.aboveConnection(), connections)))
-			removeConflictingPieces(pos, availablePiecesMap);
+		if(availablePiecesMap.get(pos).removeIf(entry -> cannotConnect(entry.connections.get(direction), connections)))
+			removeConflictingPieces(pos, availablePiecesMap, direction);
 	}
 	
 	private static boolean cannotConnect(ConnectionType type, Set<ConnectionType> otherTypes)
@@ -250,9 +264,23 @@ public final class ProspitStructure
 		{
 			return cornerPos.offset(this.x * PIECE_SIZE, this.y * PIECE_SIZE, this.z * PIECE_SIZE);
 		}
+		
+		public Optional<PiecePos> tryOffset(Direction direction)
+		{
+			int newX = this.x + direction.getStepX();
+			int newY = this.y + direction.getStepY();
+			int newZ = this.z + direction.getStepZ();
+			
+			if(newX < 0 || WIDTH_IN_PIECES <= newX
+					|| newY < 0 || HEIGHT_IN_PIECES <= newY
+					|| newZ < 0 || WIDTH_IN_PIECES <= newZ)
+				return Optional.empty();
+			
+			return Optional.of(new PiecePos(newX, newY, newZ));
+		}
 	}
 	
-	private record PieceEntry(Function<BlockPos, StructurePiece> constructor, ConnectionType aboveConnection, ConnectionType belowConnection, Weight weight) implements WeightedEntry
+	private record PieceEntry(Function<BlockPos, StructurePiece> constructor, Map<Direction, ConnectionType> connections, Weight weight) implements WeightedEntry
 	{
 		@Override
 		public Weight getWeight()
@@ -261,16 +289,34 @@ public final class ProspitStructure
 		}
 	}
 	
+	private static Map<Direction, ConnectionType> symmetric(ConnectionType down, ConnectionType up, ConnectionType side)
+	{
+		return Map.of(
+				Direction.DOWN, down,
+				Direction.UP, up,
+				Direction.NORTH, side,
+				Direction.EAST, side,
+				Direction.SOUTH, side,
+				Direction.WEST, side
+		);
+	}
+	
 	private enum ConnectionType
 	{
 		AIR,
 		SOLID,
+		WALL,
+		BRIDGE,
 	}
 	
 	private static final Map<ConnectionType, Set<ConnectionType>> SUPPORTED_CONNECTIONS = Util.make(() -> {
 		List<Pair<ConnectionType, ConnectionType>> allowedConnections = List.of(
 				Pair.of(ConnectionType.AIR, ConnectionType.AIR),
-				Pair.of(ConnectionType.SOLID, ConnectionType.SOLID)
+				Pair.of(ConnectionType.SOLID, ConnectionType.SOLID),
+				Pair.of(ConnectionType.AIR, ConnectionType.WALL),
+				Pair.of(ConnectionType.WALL, ConnectionType.WALL),
+				Pair.of(ConnectionType.BRIDGE, ConnectionType.BRIDGE),
+				Pair.of(ConnectionType.BRIDGE, ConnectionType.WALL)
 		);
 		
 		ImmutableMap.Builder<ConnectionType, Set<ConnectionType>> mapBuilder = ImmutableMap.builder();
@@ -289,10 +335,12 @@ public final class ProspitStructure
 		return mapBuilder.build();
 	});
 	
-	public static final Supplier<StructurePieceType.ContextlessType> SOLID_PIECE_TYPE = MSStructurePieces.REGISTER.register("prospit_solid_piece",
+	public static final Supplier<StructurePieceType.ContextlessType> SOLID_PIECE_TYPE = MSStructurePieces.REGISTER.register("prospit_solid",
 			() -> SolidPiece::new);
-	public static final Supplier<StructurePieceType.ContextlessType> PYRAMID_PIECE_TYPE = MSStructurePieces.REGISTER.register("prospit_pyramid_piece",
+	public static final Supplier<StructurePieceType.ContextlessType> PYRAMID_PIECE_TYPE = MSStructurePieces.REGISTER.register("prospit_pyramid",
 			() -> PyramidPiece::new);
+	public static final Supplier<StructurePieceType.ContextlessType> BRIDGE_PIECE_TYPE = MSStructurePieces.REGISTER.register("prospit_bridge",
+			() -> BridgePiece::new);
 	
 	public static final class SolidPiece extends ImprovedStructurePiece
 	{
@@ -300,7 +348,7 @@ public final class ProspitStructure
 		{
 			super(SOLID_PIECE_TYPE.get(), 0, BoundingBox.fromCorners(bottomCornerPos,
 					bottomCornerPos.offset(PIECE_SIZE - 1, PIECE_SIZE - 1, PIECE_SIZE - 1)));
-			setOrientation(Direction.NORTH);
+			setOrientation(Direction.SOUTH);
 		}
 		
 		public SolidPiece(CompoundTag tag)
@@ -327,7 +375,7 @@ public final class ProspitStructure
 		{
 			super(PYRAMID_PIECE_TYPE.get(), 0, BoundingBox.fromCorners(bottomCornerPos,
 					bottomCornerPos.offset(PIECE_SIZE - 1, PIECE_SIZE - 1, PIECE_SIZE - 1)));
-			setOrientation(Direction.NORTH);
+			setOrientation(Direction.SOUTH);
 		}
 		
 		public PyramidPiece(CompoundTag tag)
@@ -350,6 +398,39 @@ public final class ProspitStructure
 			generateBox(level, box, 2, 2, 2, 5, 2, 5,
 					Blocks.GOLD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState(), false);
 			generateBox(level, box, 3, 3, 3, 4, 3, 4,
+					Blocks.GOLD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState(), false);
+		}
+	}
+	
+	public static final class BridgePiece extends ImprovedStructurePiece
+	{
+		public BridgePiece(BlockPos bottomCornerPos, Direction.Axis axis)
+		{
+			super(BRIDGE_PIECE_TYPE.get(), 0, BoundingBox.fromCorners(bottomCornerPos,
+					bottomCornerPos.offset(PIECE_SIZE - 1, PIECE_SIZE - 1, PIECE_SIZE - 1)));
+			setOrientation(axis == Direction.Axis.X ? Direction.EAST : Direction.SOUTH);
+		}
+		
+		public BridgePiece(CompoundTag tag)
+		{
+			super(BRIDGE_PIECE_TYPE.get(), tag);
+		}
+		
+		@Override
+		protected void addAdditionalSaveData(StructurePieceSerializationContext context, CompoundTag tag)
+		{
+		}
+		
+		@Override
+		public void postProcess(WorldGenLevel level, StructureManager structureManager, ChunkGenerator generator, RandomSource random, BoundingBox box, ChunkPos chunkPos, BlockPos pos)
+		{
+			generateBox(level, box, 2, 1, 0, 5, 1, 7,
+					Blocks.GOLD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState(), false);
+			generateBox(level, box, 3, 0, 0, 4, 0, 7,
+					Blocks.GOLD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState(), false);
+			generateBox(level, box, 2, 2, 0, 2, 2, 7,
+					Blocks.GOLD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState(), false);
+			generateBox(level, box, 5, 2, 0, 5, 2, 7,
 					Blocks.GOLD_BLOCK.defaultBlockState(), Blocks.GOLD_BLOCK.defaultBlockState(), false);
 		}
 	}
