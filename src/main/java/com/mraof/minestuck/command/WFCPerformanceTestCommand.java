0f12b048aa22e38236aee9b72eadd0f264eb1536
kirderf1 <kirderf4@gmail.com>
2025-01-12 17:39:05 +0100

    Add times to run argument to performance test commmand

--- src/main/java/com/mraof/minestuck/command/WFCPerformanceTestCommand.java
+++ src/main/java/com/mraof/minestuck/command/WFCPerformanceTestCommand.java
@@ -1,6 +1,7 @@
 package com.mraof.minestuck.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mraof.minestuck.world.gen.structure.wfc.ProspitStructure;
 import com.mraof.minestuck.world.gen.structure.wfc.WFC;
 import com.mraof.minestuck.world.gen.structure.wfc.WFCData;
@@ -14,18 +15,44 @@ import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import net.minecraft.world.level.levelgen.structure.StructurePiece;
 import net.minecraft.world.level.levelgen.structure.StructurePieceAccessor;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplateManager;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
 
 import javax.annotation.Nullable;
 
+@EventBusSubscriber
 public final class WFCPerformanceTestCommand
 {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher)
 	{
 		dispatcher.register(Commands.literal("wfc_performance_test").requires(source -> source.hasPermission(Commands.LEVEL_OWNERS))
+				.then(Commands.argument("times", IntegerArgumentType.integer(1))
+						.executes(context -> {
+							run(IntegerArgumentType.getInteger(context, "times"), context.getSource().getLevel().getStructureManager());
+							return 1;
+						}))
 				.executes(context -> {
 					run(context.getSource().getLevel().getStructureManager());
 					return 1;
 				}));
+	}
+	
+	private static void run(int times, StructureTemplateManager templateManager)
+	{
+		timesToRun = times;
+	}
+	
+	private static int timesToRun = 0;
+	
+	@SubscribeEvent
+	private static void onTick(ServerTickEvent.Post event)
+	{
+		if(timesToRun > 0 && event.hasTime() && event.getServer().getTickCount() % 40 == 0)
+		{
+			timesToRun--;
+			run(event.getServer().getStructureManager());
+		}
 	}
 	
 	private static final StructurePieceAccessor DUMMY_PIECE_ACCESSOR = new StructurePieceAccessor()
