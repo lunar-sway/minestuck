646e642350e114203fa1c84068a73d7c91b86fc7
kirderf1 <kirderf4@gmail.com>
2025-04-13 10:49:58 +0200

    Wave function collapse structure generation and demo dimension (#662)

--- src/main/java/com/mraof/minestuck/Minestuck.java
+++ src/main/java/com/mraof/minestuck/Minestuck.java
@@ -42,6 +42,7 @@ import com.mraof.minestuck.world.gen.MSWorldGenTypes;
 import com.mraof.minestuck.world.gen.feature.MSFeatures;
 import com.mraof.minestuck.world.gen.feature.MSStructureProcessorTypes;
 import com.mraof.minestuck.world.gen.structure.MSStructures;
+import com.mraof.minestuck.world.gen.structure.wfc.ProspitWFCDemoStructure;
 import com.mraof.minestuck.world.lands.LandTypes;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.bus.api.IEventBus;
@@ -124,6 +125,7 @@ public class Minestuck
 		SkaiaBlocks.init();
 		AspectTreeBlocks.init();
 		DreamerMoonBlocks.init();
+		ProspitWFCDemoStructure.init();
 	}
 	
 	/**
--- src/main/java/com/mraof/minestuck/world/biome/MSBiomes.java
+++ src/main/java/com/mraof/minestuck/world/biome/MSBiomes.java
@@ -6,12 +6,13 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 
-public class MSBiomes
+public final class MSBiomes
 {
 	public static final ResourceKey<Biome> SKAIA = ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(Minestuck.MOD_ID, "skaia"));
 	public static final ResourceKey<Biome> PROSPIT = ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(Minestuck.MOD_ID, "prospit"));
 	public static final ResourceKey<Biome> DERSE = ResourceKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(Minestuck.MOD_ID, "derse"));
 	public static final ResourceKey<Biome> VEIL = ResourceKey.create(Registries.BIOME, Minestuck.id("veil"));
+	public static final ResourceKey<Biome> PROSPIT_WFC_DEMO = ResourceKey.create(Registries.BIOME, Minestuck.id("prospit_wfc_demo"));
 	
 	public static final LandBiomeSetType DEFAULT_LAND = new LandBiomeSetType(Minestuck.MOD_ID, "default", true, 0.7F, 0.5F);
 	public static final LandBiomeSetType HIGH_HUMID_LAND = new LandBiomeSetType(Minestuck.MOD_ID, "high_humid", true, 1.0F, 0.9F);
