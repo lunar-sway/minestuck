b64e0d6add1ad11a2c08f2334050b8ddb9c27d6a
medsal15 <medsal16+github@gmail.com>
2025-10-14 15:50:42 +0200

    move deploy list loader to its own class unstatic records split DeployList.registerItem function parameters into methods

--- src/main/java/com/mraof/minestuck/computer/editmode/DeployList.java
+++ src/main/java/com/mraof/minestuck/computer/editmode/DeployList.java
@@ -1,17 +1,9 @@
 package com.mraof.minestuck.computer.editmode;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.JsonOps;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.MinestuckConfig;
 import com.mraof.minestuck.api.alchemy.GristSet;
 import com.mraof.minestuck.api.alchemy.GristTypes;
-import com.mraof.minestuck.api.alchemy.MutableGristSet;
-import com.mraof.minestuck.api.alchemy.recipe.GristCostRecipe;
 import com.mraof.minestuck.block.MSBlocks;
 import com.mraof.minestuck.item.CaptchaCardItem;
 import com.mraof.minestuck.item.MSItems;
@@ -24,12 +16,8 @@ import com.mraof.minestuck.world.storage.MSExtraData;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.packs.resources.ResourceManager;
-import net.minecraft.server.packs.resources.SimpleJsonResourceReloadListener;
 import net.minecraft.util.StringRepresentable;
-import net.minecraft.util.profiling.ProfilerFiller;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.Level;
@@ -37,23 +25,16 @@ import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.ModConfigSpec;
-import net.neoforged.neoforge.common.conditions.ICondition;
-import net.neoforged.neoforge.event.AddReloadListenerEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.neoforge.server.ServerLifecycleHooks;
 
 import javax.annotation.Nonnull;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Optional;
 import java.util.function.BiFunction;
 
 /**
@@ -471,125 +452,5 @@ public final class DeployList
 	public static List<ItemStack> getEditmodeTools()
 	{
 		return Collections.emptyList();
-	}
-	
-	@SubscribeEvent
-	private static void onResourceReload(AddReloadListenerEvent event)
-	{
-		event.addListener(new Loader());
-	}
-	
-	private static final class Loader extends SimpleJsonResourceReloadListener
-	{
-		private static final Logger LOGGER = LogManager.getLogger();
-		
-		private static List<String> added_with_datapack = new ArrayList<>();
-		
-		Loader()
-		{
-			super(new GsonBuilder().create(), "minestuck/deploy_list");
-		}
-		
-		@Override
-		protected void apply(Map<ResourceLocation, JsonElement> jsonEntries, ResourceManager resourceManager,
-							 ProfilerFiller profiler)
-		{
-			allList.removeIf(entry -> added_with_datapack.contains(entry.getName()));
-			deployList.removeIf(entry -> added_with_datapack.contains(entry.getName()));
-			atheneumList.removeIf(entry -> added_with_datapack.contains(entry.getName()));
-			added_with_datapack.clear();
-			
-			for(Map.Entry<ResourceLocation, JsonElement> entry : jsonEntries.entrySet())
-			{
-				// FIXME grist costs are only updated on rejoin
-				DeployDataEntry.LIST_CODEC.parse(JsonOps.INSTANCE, entry.getValue())
-						.resultOrPartial(message -> LOGGER.error("Couldn't entirely parse deploylist entry {}: {}", entry.getKey(), message))
-						.ifPresent(list -> {
-							for(int i = 0; i < list.size(); i++)
-							{
-								DeployDataEntry data = list.get(i);
-								String name = entry.getKey().toString() + "-" + i;
-								added_with_datapack.add(name);
-								
-								registerItem(name, data.tier, d -> {
-									if(data.cost.isEmpty()) return true;
-									for(GristCost cost : data.cost)
-									{
-										if(cost.test(this.getContext())) return true;
-									}
-									return false;
-								}, (d, l) -> {
-									ItemStack stack = data.stack.copy();
-									stack.setCount(1);
-									if(data.punched)
-									{
-										stack = CaptchaCardItem.createPunchedCard(stack.getItem());
-									}
-									return stack;
-								}, (primary, playerdata) -> {
-									MutableGristSet set = MutableGristSet.newDefault();
-									if(data.cost.size() == 0)
-									{
-										set = GristCostRecipe.findCostForItem(data.stack, null, false, ServerLifecycleHooks.getCurrentServer().overworld()).mutableCopy();
-									}
-									for(GristCost cost : data.cost)
-									{
-										if(cost.test(this.getContext()))
-										{
-											if(cost.grist.isPresent())
-											{
-												set.add(cost.grist.get());
-											}
-											if(cost.primary != 0)
-											{
-												set.add(playerdata.getBaseGrist(), cost.primary);
-											}
-											break;
-										}
-									}
-									return set.asImmutable();
-								}, data.category);
-							}
-						});
-			}
-		}
-	}
-	
-	public static record GristCost(Optional<GristSet.Immutable> grist, int primary, List<ICondition> conditions)
-	{
-		public static final Codec<GristCost> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-				GristSet.Codecs.MAP_CODEC.optionalFieldOf("grist").forGetter(GristCost::grist),
-				Codec.INT.optionalFieldOf("primary_grist", 0).forGetter(GristCost::primary),
-				ICondition.LIST_CODEC.optionalFieldOf("conditions", List.of()).forGetter(GristCost::conditions)
-		).apply(instance, GristCost::new));
-		public static final Codec<List<GristCost>> LIST_CODEC = CODEC.listOf();
-		
-		public boolean test(ICondition.IContext context)
-		{
-			for(ICondition condition : conditions)
-			{
-				if(!condition.test(context)) return false;
-			}
-			return true;
-		}
-	}
-	
-	public static record DeployDataEntry(ItemStack stack, int tier, DeployList.EntryLists category,
-										 List<GristCost> cost, boolean punched)
-	{
-		public static final Codec<DeployDataEntry> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-				ItemStack.CODEC.fieldOf("item").validate(stack -> {
-					if(!stack.isComponentsPatchEmpty())
-						return DataResult.error(() -> "Deploylist item does not support additional components");
-					return DataResult.success(stack);
-				}).forGetter(DeployDataEntry::stack),
-				Codec.INT.optionalFieldOf("tier", 0).forGetter(DeployDataEntry::tier),
-				StringRepresentable.fromValues(EntryLists::values).optionalFieldOf("category", DeployList.EntryLists.ATHENEUM)
-						.validate(entry -> entry == EntryLists.ALL ? DataResult.error(() -> "Cannot add a deploylist entry to all") : DataResult.success(entry))
-						.forGetter(DeployDataEntry::category),
-				GristCost.LIST_CODEC.optionalFieldOf("cost", List.of()).forGetter(DeployDataEntry::cost),
-				Codec.BOOL.optionalFieldOf("punched", false).forGetter(DeployDataEntry::punched)
-		).apply(instance, DeployDataEntry::new));
-		public static final Codec<List<DeployDataEntry>> LIST_CODEC = CODEC.listOf();
 	}
 }
