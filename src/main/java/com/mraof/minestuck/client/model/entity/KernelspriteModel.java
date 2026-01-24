aa8e77eb1977be78ece22d292a1f5f6455b91ad8
saviourom <61608662+saviourom@users.noreply.github.com>
2026-01-24 15:48:06 -0500

    the kernalsprite entity can now be summoned via commands but the texture still isnt working, unsure as to why will continue to work on it

--- src/main/java/com/mraof/minestuck/client/model/entity/KernelspriteModel.java
+++ src/main/java/com/mraof/minestuck/client/model/entity/KernelspriteModel.java
@@ -1,5 +1,35 @@
 package com.mraof.minestuck.client.model.entity;
 
-public class KernelspriteModel
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.EntityModel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.Entity;
+
+public class KernelspriteModel extends EntityModel
 {
+	private final ResourceLocation model;
+	private final String layer;
+	
+	public KernelspriteModel(ResourceLocation model, String layer) {
+		
+		this.model = model;
+		this.layer = layer;
+		
+		
+	}
+	
+	
+	@Override
+	public void setupAnim(Entity entity, float v, float v1, float v2, float v3, float v4)
+	{
+	
+	}
+	
+	@Override
+	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int i, int i1, int i2)
+	{
+	
+	}
 }
--- src/main/java/com/mraof/minestuck/client/renderer/entity/KernelspriteRenderer.java
+++ src/main/java/com/mraof/minestuck/client/renderer/entity/KernelspriteRenderer.java
@@ -1,21 +1,24 @@
 package com.mraof.minestuck.client.renderer.entity;
 
+import com.mraof.minestuck.Minestuck;
 import com.mraof.minestuck.client.model.entity.KernelspriteModel;
 import com.mraof.minestuck.entity.KernelspriteEntity;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
-import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
-public class KernelspriteRenderer extends MobRenderer<KernelspriteEntity, KernelspriteModel<KernelspriteEntity>>
+public class KernelspriteRenderer extends EntityRenderer<KernelspriteEntity>
 {
-	public KernelspriteRenderer(EntityRendererProvider.Context context, KernelspriteModel<KernelspriteEntity> model, float shadowRadius)
+	public KernelspriteRenderer(EntityRendererProvider.Context context)
 	{
-		super(context, model, shadowRadius);
+		super(context);
+		this.shadowRadius = 0.15F;
+		this.shadowStrength = .75F;
 	}
 	
 	@Override
-	public ResourceLocation getTextureLocation(KernelspriteEntity kernelspriteEntity)
+	public ResourceLocation getTextureLocation(KernelspriteEntity entity)
 	{
-		return null;
+		return ResourceLocation.fromNamespaceAndPath(Minestuck.MOD_ID,"kernelsprite/kernelsprite_back");
 	}
 }
