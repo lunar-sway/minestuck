bd6ef6bf3322c6467d937815964d3cbb99ba98da
Maxwell <73421069+Fl1s@users.noreply.github.com>
2026-05-11 20:11:00 +0700

    fix: dropConnection

--- src/main/java/com/mraof/minestuck/block/machine/LaptopBlock.java
+++ src/main/java/com/mraof/minestuck/block/machine/LaptopBlock.java
@@ -59,6 +59,8 @@ public class LaptopBlock extends ComputerBlock
 	{
 		if(!(level.getBlockEntity(pos) instanceof ComputerBlockEntity computer))
 			return;
+
+		computer.closeAll();
 		
 		CompoundTag beTag = computer.saveWithoutMetadata(level.registryAccess());
 		
